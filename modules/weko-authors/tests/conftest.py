030bb157baa38d0d0a3b5190768765a588b0326d
weko3-dev07 <weko3-hitachi-dev07@ml.itg.hitachi.co.jp>
2018-03-13 13:18:24 +0900

    refs #2993 add weko-authors

---
+++ modules/weko-authors/tests/conftest.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of WEKO3.
+# Copyright (C) 2017 National Institute of Informatics.
+#
+# WEKO3 is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# WEKO3 is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with WEKO3; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111-1307, USA.
+
+"""Pytest configuration."""
+
+import shutil
+import tempfile
+
+import pytest
+from flask import Flask
+from flask_babelex import Babel
+
+
+@pytest.yield_fixture()
+def instance_path():
+    """Temporary instance path."""
+    path = tempfile.mkdtemp()
+    yield path
+    shutil.rmtree(path)
+
+
+@pytest.fixture()
+def base_app(instance_path):
+    """Flask application fixture."""
+    app_ = Flask('testapp', instance_path=instance_path)
+    app_.config.update(
+        SECRET_KEY='SECRET_KEY',
+        TESTING=True,
+    )
+    Babel(app_)
+    return app_
+
+
+@pytest.yield_fixture()
+def app(base_app):
+    """Flask application fixture."""
+    with base_app.app_context():
+        yield base_app
