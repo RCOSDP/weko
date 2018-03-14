777443b945993cd0947c496c45f735c590687c27
weko3-dev07 <weko3-hitachi-dev07@ml.itg.hitachi.co.jp>
2018-03-14 17:04:45 +0900

    add index to script

--- modules/weko-schema-ui/setup.py
+++ modules/weko-schema-ui/setup.py
@@ -97,7 +97,7 @@ setup(
             'weko_schema_ui_js = weko_schema_ui.bundles:js',
         ],
         'invenio_search.mappings': [
-            'weko = weko_schema_ui.mappings',
+            'junii2 = weko_schema_ui.mappings',
         ],
     },
     extras_require=extras_require,
