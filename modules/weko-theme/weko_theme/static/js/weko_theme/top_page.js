5726cdbf3afb2fd34a5f1322d0fdde08b5bd9328
weko3-dev05 <weko3-hitachi-dev05@ml.itg.hitachi.co.jp>
2018-04-03 19:33:05 +0900

    #3791 search-query-url

--- modules/weko-theme/weko_theme/static/js/weko_theme/top_page.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/top_page.js
@@ -18,6 +18,7 @@ require([
     }
 
     function ArrangeSearch(){
+     //ラジオボタン
       var SearchType = GetUrlParam('search_type');
       if (SearchType == '0'){
         $('#search_type_fulltext').prop('checked', true);
@@ -56,6 +57,48 @@ require([
       }
     }
 
+    //フォーム提出
+    function SearchSubmit(){
+      $('#search-form').submit(function(event){
+        var que = ''
+        var query = ''
+        $('#search_detail_metadata :input:not(:checkbox), #q').each(function(){
+          if ($(this).val() !== ''){
+            query += $(this).serialize() + '&';
+          }
+        });
+        window.location.href = ('search?page=1&size=20&' + query).slice(0,-1);
+        // process the form
+//        $.ajax({
+//            type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+//            url         : '/search', // the url where we want to POST
+//            data        : query, // our data object
+////            dataType    : 'json', // what type of data do we expect back from the server
+////                        encode          : true,
+//            success     : function(data){
+//              alert(data);
+//            }
+//        })
+//            // using the done promise callback
+//            .done(function(data) {
+//              alert(data);
+//              // log data to the console so we can see
+//              console.log(data);
+//              window.location.reload(true);
+//              // here we will handle errors and validation messages
+//            })
+//            .fail(function(data){
+//              alert("fail");
+//              var myJSO = JSON.stringify(data);
+//              alert(myJSO);
+//              window.location.reload(true);
+//            });
+
+        // stop the form from submitting the normal way and refreshing the page
+        event.preventDefault();
+      })
+    }
+
     $(document).ready(function() {
       //入力あったら、入力値入って展開したまま
       ArrangeSearch();
@@ -71,8 +114,8 @@ require([
         $('#search_detail_metadata :input:not(:checkbox)').each(function(){
           $(this).val('');
           sessionStorage.removeItem($(this).attr('id'));
-//            sessionStorage.setItem($(this).attr('id'), '');
-        })
+        });
+        SearchSubmit();
       });
 
       //詳細検索の表示と隠すボタン
@@ -98,6 +141,7 @@ require([
             sessionStorage.removeItem($(this).attr('id'));
           }
         })
+        SearchSubmit();
       });
 
        //クリアボタン：入力内容を削除
@@ -107,11 +151,6 @@ require([
           sessionStorage.removeItem($(this).attr('id'));
 //            sessionStorage.setItem($(this).attr('id'), '');
         })
-      });
-
-      //ラジオボタン
-      $('#search_type_fulltext search_type_keyword').on('click', function(){
-        $(this).prop('checked', true);
       });
 
     });
