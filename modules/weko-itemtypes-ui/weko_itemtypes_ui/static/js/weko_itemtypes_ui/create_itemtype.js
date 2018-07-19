a67503910baca791ed8f4d55bb26a15494c70293
weko3-dev02 <weko3-hitachi-dev02@ml.itg.hitachi.co.jp>
2018-07-19 14:06:22 +0900

    change contents from

--- modules/weko-itemtypes-ui/weko_itemtypes_ui/static/js/weko_itemtypes_ui/create_itemtype.js
+++ modules/weko-itemtypes-ui/weko_itemtypes_ui/static/js/weko_itemtypes_ui/create_itemtype.js
@@ -173,6 +173,12 @@
               condition: "model.filemeta[arrayIndex].licensetype == 'license_free'"
             },
             {
+              title: '剽窃チェック',
+              title_i18n:{ja:"剽窃チェック",en:"Check Plagiarism"},
+              type: "template",
+              template: "<div class='text-center'><a class='btn btn-primary' href='http://ec2-13-230-57-67.ap-northeast-1.compute.amazonaws.com/ezas/pdf-detect-weko.html' target='_blank' role='button'>{{ form.title }}</a></div>"
+            },
+            {
               key: "filemeta[].accessrole",
               type: "radios",
               title: "アクセス",
