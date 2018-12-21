27d9f8ab88c363a04cefa3101411b06da205c1d8
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-12-21 10:47:34 +0900

    fixup bug

--- modules/weko-search-ui/weko_search_ui/api.py
+++ modules/weko-search-ui/weko_search_ui/api.py
@@ -68,9 +68,9 @@ class SearchSetting(object):
                 sort_str = res.default_dis_sort_index
         else:
             if search_type == current_app.config['WEKO_SEARCH_TYPE_KEYWORD']:
-                sort_str= current_app.config['WEKO_ADMIN_MANAGEMENT_OPTIONS'].get('dlt_keyword_sort_selected')
+                sort_str = ad_config.WEKO_ADMIN_MANAGEMENT_OPTIONS['dlt_keyword_sort_selected']
             else:
-                sort_str = current_app.config['WEKO_ADMIN_MANAGEMENT_OPTIONS'].get('dlt_index_sort_selected')
+                sort_str = ad_config.WEKO_ADMIN_MANAGEMENT_OPTIONS['dlt_index_sort_selected']
 
         sort_key = sort_str[0:sort_str.rfind('_', 1)]
 
