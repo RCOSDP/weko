64b1c8344a048af674b8d05ca083da3b69218f80
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2019-01-09 18:21:41 +0900

    workflow

--- modules/weko-search-ui/weko_search_ui/views.py
+++ modules/weko-search-ui/weko_search_ui/views.py
@@ -74,8 +74,10 @@ def search():
     current_app.logger.debug("CCCCCCC")
 
     from weko_workflow.api import WorkActivity
-    activity_detail,activity,item,steps,action_id,cur_step,temporary_comment,approval_record,step_item_login_url,histories,res_check,pid,ctx\
+    activity_detail, activity, item, steps, action_id, cur_step, temporary_comment, approval_record, step_item_login_url, histories, res_check, pid, ctx\
         = WorkActivity.get_activity_index_search(activity_id='A1547011369')
+
+    current_app.logger.debug(item)
 
     return render_template('weko_workflow/activity_detail.html',
                                        activity=activity_detail,
