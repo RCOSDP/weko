969c349d740f164dab86571af08af443e2e0cbf6
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2019-01-10 18:52:18 +0900

    workflow

--- modules/weko-workflow/weko_workflow/static/js/weko_workflow/workflow_detail.js
+++ modules/weko-workflow/weko_workflow/static/js/weko_workflow/workflow_detail.js
@@ -193,12 +193,33 @@ require([
 function searchResItemLinkCtrl($scope, $rootScope, $http, $location) {
    $scope.testData='AAAAA'
    $scope.link_item_list=[]
-
+   $scope.sele_options=[
+                        {id:'relateTo',content:'relateTo'},
+                        {id:'isVersionOf',content:'isVersionOf'},
+                        {id:'hasVersion',content:'hasVersion'},
+                        {id:'isReplaceBy',content:'isReplaceBy'},
+                        {id:'replaces',content:'replaces'},
+                        {id:'isRequiredBy',content:'isRequiredBy'},
+                        {id:'requires',content:'requires'},
+                        {id:'isPartOf',content:'isPartOf'},
+                        {id:'hasPart',content:'hasPart'},
+                        {id:'isReferencedBy',content:'isReferencedBy'},
+                        {id:'references',content:'references'},
+                        {id:'isFormatOf',content:'isFormatOf'},
+                        {id:'hasFormat',content:'hasFormat'},
+                        {id:'isSupplementTo',content:'isSupplementTo'},
+                        {id:'isSupplementBy',content:'isSupplementBy'},
+                        {id:'isIdenticalTo',content:'isIdenticalTo'},
+                        {id:'isDeriverdFrom',content:'isDeriverdFrom'},
+                        {id:'isSoruceOf',content:'isSoruceOf'},
+                       ]
 //   add button
    $rootScope.add_link=function(data, index){
-    var sub_data={seleOption:[],sele_id:'',item_data:""}
-    sub_data.item_data = data
-    $scope.link_item_list.push(sub_data)
+    var sub_data={seleOption:[],sele_id:'',item_data:""};
+    sub_data.seleOption = angular.copy($scope.sele_options);
+    sub_data.sele_id = 'relateTo';
+    sub_data.item_data = data;
+    $scope.link_item_list.push(sub_data);
    }
 //   delete button
    $scope.del_link=function(index){
