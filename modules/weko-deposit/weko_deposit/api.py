b179ed7229fea0fb800df12986cb1adbc3154e4d
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2019-01-16 15:56:33 +0900

    add itemLink

--- modules/weko-deposit/weko_deposit/api.py
+++ modules/weko-deposit/weko_deposit/api.py
@@ -135,7 +135,7 @@ class WekoIndexer(RecordIndexer):
         relation_type_val=[]
         # current_app.logger.debug(relation_info[0])
         for d in relation_info[0]:
-            sub_data=dict(item_links=d.item_data.links.self, item_title=d.item_title, value=d.sele_id)
+            sub_data=dict(item_links=d.get('item_data').get('links').get('self'), item_title=d.get('item_title'), value=d.get('sele_id'))
             relation_type_val.append(sub_data)
         body = {'doc': {relation: {relation_type:relation_type_val}}}
         return self.client.update(
