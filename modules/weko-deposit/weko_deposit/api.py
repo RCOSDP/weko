18ac6d49c2dcb65380ea2497983fa2adc7e5c427
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2019-01-16 15:53:24 +0900

    add itemLink

--- modules/weko-deposit/weko_deposit/api.py
+++ modules/weko-deposit/weko_deposit/api.py
@@ -133,19 +133,18 @@ class WekoIndexer(RecordIndexer):
         relation = 'relation'
         relation_type = 'relation_type'
         relation_type_val=[]
-        current_app.logger.debug(relation_info[0])
-        # for d in relation_info:
-        #     current_app.logger.debug(d)
-        #     sub_data=dict(item_links=d.item_data.links.self, item_title=d.item_title, value=d.sele_id)
-        #     relation_type_val.append(sub_data)
-        # body = {'doc': {relation: {relation_type:relation_type_val}}}
-        # return self.client.update(
-        #     index=self.es_index,
-        #     doc_type=self.es_doc_type,
-        #     id=str(record.id),
-        #     version=record.revision_id,
-        #     body=body
-        # )
+        # current_app.logger.debug(relation_info[0])
+        for d in relation_info[0]:
+            sub_data=dict(item_links=d.item_data.links.self, item_title=d.item_title, value=d.sele_id)
+            relation_type_val.append(sub_data)
+        body = {'doc': {relation: {relation_type:relation_type_val}}}
+        return self.client.update(
+            index=self.es_index,
+            doc_type=self.es_doc_type,
+            id=str(record.id),
+            version=record.revision_id,
+            body=body
+        )
 
     def update_path(self, record):
         self.get_es_index()
