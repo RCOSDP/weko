ce2b1650031b5c933804834afa56ff8ffd53c848
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2019-01-18 15:16:10 +0900

    add itemLink

--- modules/weko-records-ui/weko_records_ui/bundles.py
+++ modules/weko-records-ui/weko_records_ui/bundles.py
@@ -35,7 +35,7 @@ js = NpmBundle(
     'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
     'node_modules/angular/angular.js',
     'js/weko_records_ui/detail.js',
-
+    'js/weko_records_ui/app.js',
     filters='jsmin',
     output="gen/weko_records_ui.%(version)s.js",
     npm={
