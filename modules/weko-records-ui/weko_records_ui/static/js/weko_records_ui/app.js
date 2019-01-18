bd5e07a79a836452b0e450506175c614d0794769
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2019-01-18 16:12:51 +0900

    add itemLink

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/app.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/app.js
@@ -1,38 +1,38 @@
-angular.module('myApp', ['ui.bootstrap'])
- .controller("ItemController",
-  function($scope, $modal, $http, $window) {
-   $scope.openConfirm = function(message, url, rdt) {
-    var modalInstance = $modal.open({
-     templateUrl: "confirm-modal.html",
-     controller: "ConfirmController",
-     resolve: {
-      msg: function() {
-       return message;
-      }
-     }
-    });
-    modalInstance.result.then(function() {
-     $http.delete(url).then(
-      function(response) {
-       // success callback
-       $window.location.href = rdt;
-      },
-      function(response) {
-       // failure call back
-       console.log(response);
-      }
-     );
-    });
-   };
-  }
- ).controller('ConfirmController',
-  function($scope, $modalInstance, msg) {
-   $scope.message = msg;
-
-   $scope.ok = function() {
-    $modalInstance.close();
-   };
-   $scope.cancel = function() {
-    $modalInstance.dismiss();
-   };
-  });
+//angular.module('myApp', ['ui.bootstrap'])
+// .controller("ItemController",
+//  function($scope, $modal, $http, $window) {
+//   $scope.openConfirm = function(message, url, rdt) {
+//    var modalInstance = $modal.open({
+//     templateUrl: "confirm-modal.html",
+//     controller: "ConfirmController",
+//     resolve: {
+//      msg: function() {
+//       return message;
+//      }
+//     }
+//    });
+//    modalInstance.result.then(function() {
+//     $http.delete(url).then(
+//      function(response) {
+//       // success callback
+//       $window.location.href = rdt;
+//      },
+//      function(response) {
+//       // failure call back
+//       console.log(response);
+//      }
+//     );
+//    });
+//   };
+//  }
+// ).controller('ConfirmController',
+//  function($scope, $modalInstance, msg) {
+//   $scope.message = msg;
+//
+//   $scope.ok = function() {
+//    $modalInstance.close();
+//   };
+//   $scope.cancel = function() {
+//    $modalInstance.dismiss();
+//   };
+//  });
