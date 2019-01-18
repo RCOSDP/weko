8185f55933fb012264b92e8b2af52f50c30b60f3
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2019-01-18 13:31:35 +0900

    add itemLink

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/app.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/app.js
@@ -38,59 +38,59 @@
 //  });
 
 
-(function (angular) {
-    // Bootstrap it!
-    angular.element(document).ready(function () {
-        angular.module('item_detail', ['ui.bootstrap']);
-
-        function ConfirmContr($scope, $rootScope, $modal, $http, $location) {
-           $scope.openConfirm = function(message, url, rdt) {
-            var modalInstance = $modal.open({
-             templateUrl: "confirm-modal.html",
-             controller: "ConfirmController",
-             resolve: {
-              msg: function() {
-               return message;
-              }
-             }
-            });
-            modalInstance.result.then(function() {
-             $http.delete(url).then(
-              function(response) {
-               // success callback
-               $window.location.href = rdt;
-              },
-              function(response) {
-               // failure call back
-               console.log(response);
-              }
-             );
-            });
-          };
-        }
-        // Inject depedencies
-        ConfirmContr.$inject = [
-          '$scope',
-          '$rootScope',
-          '$modal',
-          '$http',
-          '$location'
-        ];
-        angular.module('item_detail')
-            .controller('ItemController', ConfirmContr);
-        angular.module('item_detail')
-            .controller('ConfirmController', function($scope, $modalInstance, msg) {
-             $scope.message = msg;
-
-             $scope.ok = function() {
-              $modalInstance.close();
-             };
-             $scope.cancel = function() {
-              $modalInstance.dismiss();
-             };
-            });
-
-        angular.bootstrap(
-            document.getElementById('item_btn_aear'), ['item_detail']);
-    });
-})(angular);
+//(function (angular) {
+//    // Bootstrap it!
+//    angular.element(document).ready(function () {
+//        angular.module('item_detail', ['ui.bootstrap']);
+//
+//        function ConfirmContr($scope, $rootScope, $modal, $http, $location) {
+//           $scope.openConfirm = function(message, url, rdt) {
+//            var modalInstance = $modal.open({
+//             templateUrl: "confirm-modal.html",
+//             controller: "ConfirmController",
+//             resolve: {
+//              msg: function() {
+//               return message;
+//              }
+//             }
+//            });
+//            modalInstance.result.then(function() {
+//             $http.delete(url).then(
+//              function(response) {
+//               // success callback
+//               $window.location.href = rdt;
+//              },
+//              function(response) {
+//               // failure call back
+//               console.log(response);
+//              }
+//             );
+//            });
+//          };
+//        }
+//        // Inject depedencies
+//        ConfirmContr.$inject = [
+//          '$scope',
+//          '$rootScope',
+//          '$modal',
+//          '$http',
+//          '$location'
+//        ];
+//        angular.module('item_detail')
+//            .controller('ItemController', ConfirmContr);
+//        angular.module('item_detail')
+//            .controller('ConfirmController', function($scope, $modalInstance, msg) {
+//             $scope.message = msg;
+//
+//             $scope.ok = function() {
+//              $modalInstance.close();
+//             };
+//             $scope.cancel = function() {
+//              $modalInstance.dismiss();
+//             };
+//            });
+//
+//        angular.bootstrap(
+//            document.getElementById('item_btn_aear'), ['item_detail']);
+//    });
+//})(angular);
