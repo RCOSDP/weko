4eb2213b93b28b2c01b60c65d9f04bc1e973b4e2
weko3-dev17 <weko3-hitachi-dev17@ml.itg.hitachi.co.jp>
2019-02-18 22:35:35 +0900

    Create module weko-items-autofill

---
+++ modules/weko-items-autofill/weko_items_autofill/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 National Institute of Informatics.
+#
+# WEKO-Items-Autofill is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Module of weko-items-autofill."""
+
+# TODO: This is an example file. Remove it if your package does not use any
+# extra configuration variables.
+
+WEKO_ITEMS_AUTOFILL_DEFAULT_VALUE = 'foobar'
+"""Default value for the application."""
+
+WEKO_ITEMS_AUTOFILL_BASE_TEMPLATE = 'weko_items_autofill/base.html'
+"""Default base template for the demo page."""
