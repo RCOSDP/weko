4814cf074d434a4b6677181de3784b3d70e3034b
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-02-19 11:43:58 +0700

    [sp3][journal] - add api, rest, model of journal.

--- modules/weko-indextree-journal/setup.py
+++ modules/weko-indextree-journal/setup.py
@@ -74,7 +74,7 @@ setup(
         ],
         'invenio_base.api_apps': [
             'invenio_i18n_rest = invenio_i18n:InvenioI18N',
-            'weko_indextree_journal_rest = weko_indextree_journal_rest:WekoIndextreeJournalREST',
+            'weko_indextree_journal_rest = weko_indextree_journal:WekoIndextreeJournalREST',
         ],
         'invenio_base.blueprints': [
             'weko_indextree_journal = weko_indextree_journal.views:blueprint',
