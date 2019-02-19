7e1a2da956698b72de47f3393d68d93a58e59027
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-02-19 10:14:18 +0700

    [sp3][journal] - add api, rest, model of journal.

--- modules/weko-indextree-journal/weko_indextree_journal/ext.py
+++ modules/weko-indextree-journal/weko_indextree_journal/ext.py
@@ -30,6 +30,7 @@ class WekoIndextreeJournal(object):
     def init_app(self, app):
         """Flask application initialization."""
         self.init_config(app)
+        app.register_blueprint(blueprint)
         app.extensions['weko-indextree-journal'] = self
 
     def init_config(self, app):
@@ -42,4 +43,40 @@ class WekoIndextreeJournal(object):
             )
         for k in dir(config):
             if k.startswith('WEKO_INDEXTREE_JOURNAL_'):
+                app.config.setdefault(k, getattr(config, k))
+
+class WekoIndexTreeJournalREST(object):
+    """
+      weko-indextree-journal Rest Obj
+    """
+
+    def __init__(self, app=None):
+        """Extension initialization.
+
+        :param app: An instance of :class:`flask.Flask`.
+        """
+        if app:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Flask application initialization.
+
+        Initialize the REST endpoints.  Connect all signals if
+        `DEPOSIT_REGISTER_SIGNALS` is True.
+
+        :param app: An instance of :class:`flask.Flask`.
+        """
+        self.init_config(app)
+        blueprint = create_blueprint(app,
+                                     app.config['WEKO_INDEXTREE_JOURNAL_REST_ENDPOINTS'])
+        app.register_blueprint(blueprint)
+        app.extensions['weko-indextree-journal-rest'] = self
+
+    def init_config(self, app):
+        """Initialize configuration.
+
+        :param app: An instance of :class:`flask.Flask`.
+        """
+        for k in dir(config):
+            if k.startswith('WEKO_INDEX_TREE_'):
                 app.config.setdefault(k, getattr(config, k))
