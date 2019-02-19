1e9cca87039684c6d2689f067946309be0670273
weko3-dev22 <weko3-hitachi-dev22@ml.itg.hitachi.co.jp>
2019-02-19 18:02:02 +0900

    Create new module: weko-bulkupdate

---
+++ modules/weko-bulkupdate/docs/examplesapp.rst
@@ -0,0 +1,13 @@
+..
+    Copyright (C) 2019 National Institute of Informatics.
+
+    WEKO-Bulkupdate is free software; you can redistribute it and/or modify
+    it under the terms of the MIT License; see LICENSE file for more details.
+
+
+Example application
+===================
+
+.. include:: ../examples/app.py
+   :start-after: SPHINX-START
+   :end-before: SPHINX-END
--- modules/weko-records-ui/weko_records_ui/views.py
+++ modules/weko-records-ui/weko_records_ui/views.py
@@ -19,17 +19,19 @@
 # MA 02111-1307, USA.
 
 """Blueprint for weko-records-ui."""
-
+from flask_login import login_required
 import six
 from flask import Blueprint, abort, current_app, render_template, \
-    make_response, redirect, request, url_for, flash
-
+    make_response, redirect, request, url_for, jsonify, jsonify
+from flask_login import current_user, login_required
 from invenio_records_ui.utils import obj_or_import_string
 from invenio_records_ui.signals import record_viewed
+from invenio_pidstore.models import PersistentIdentifier
 from weko_index_tree.models import IndexStyle
 from .permissions import check_created_id
 from weko_search_ui.api import get_search_detail_keyword
-from weko_deposit.api import WekoIndexer
+from weko_deposit.api import WekoIndexer, WekoRecord
+from weko_records.api import ItemsMetadata
 from .models import PDFCoverPageSettings
 import werkzeug
 
@@ -327,3 +329,58 @@ def set_pdfcoverpage_header():
         return redirect('/admin/pdfcoverpage')
 
     return redirect('/admin/pdfcoverpage')
+
+@blueprint.route("/item_management/bulk_update", methods=['GET'])
+@login_required
+def bulk_update():
+
+    """Render view."""
+    detail_condition = get_search_detail_keyword('')
+    return render_template(current_app.config['WEKO_ITEM_MANAGEMENT_TEMPLATE'],
+                           fields=current_app.config['WEKO_RECORDS_UI_BULK_UPDATE_FIELDS']['fields'],
+                           licences=current_app.config['WEKO_RECORDS_UI_BULK_UPDATE_FIELDS']['licences'],
+                           management_type='update',
+                           detail_condition=detail_condition)
+
+
+@blueprint.route('/bulk_update/items_metadata', methods=['GET'])
+@login_required
+def get_items_metadata():
+    """Get the metadata of items to bulk update."""
+
+    def get_file_data(meta):
+        file_data = {}
+        for key in meta:
+            if isinstance(meta.get(key), list):
+                for item in meta.get(key):
+                    if 'filename' in item:
+                        file_data[key] = meta.get(key)
+                        break
+        return file_data
+
+    pids = request.values.get('pids')
+    pid_list = []
+    if pids is not None:
+        pid_list = pids.split('/')
+
+    data = {}
+    for pid in pid_list:
+        record = WekoRecord.get_record_by_pid(pid)
+        indexes = []
+        if isinstance(record.get('path'), list):
+            for path in record.get('path'):
+                indexes.append(path.split('/')[-1])
+
+        pidObject = PersistentIdentifier.get('recid', pid)
+        meta = ItemsMetadata.get_record(pidObject.object_uuid)
+
+        action = 'private' if record.get('publish_status', '1') == '1' \
+            else 'publish'
+
+        if meta:
+            data[pid] = {}
+            data[pid]['meta'] = meta
+            data[pid]['index'] = {"index": indexes, 'actions': action}
+            data[pid]['contents'] = get_file_data(meta)
+
+    return jsonify(data)
