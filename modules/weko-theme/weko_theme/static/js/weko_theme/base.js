04798dfc25429854681505b02b0116dda19f65ea
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-02-25 19:25:00 +0900

    Debug for chunk design.

--- modules/weko-theme/weko_theme/static/js/weko_theme/base.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/base.js
@@ -11,7 +11,7 @@ require([
       });
 
       // Chunk Design
-      alert('Base!!');
+      alert($('#grid-info'));
 //      alert($('#grid-info').innerHTML);
 //      alert($('#grid-body-left').innerHTML);
 //      alert($('#grid-body-right').innerHTML);
