d84dc855e17cfd795b85b9c4f476bdd9fbe06567
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-02-26 10:27:06 +0700

    [sp4][journal]: Add export function.

--- modules/weko-indextree-journal/weko_indextree_journal/api.py
+++ modules/weko-indextree-journal/weko_indextree_journal/api.py
@@ -190,3 +190,9 @@ class Journals(object):
             return []
 
         return dict(obj)
+
+    @classmethod
+    def export_journals(cls)
+        # get data from sql
+        # save to file.
+        # complete.
