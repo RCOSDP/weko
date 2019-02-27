1d62d26286fa46937ac9a717f76ce70c4ccdf2cf
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-02-27 17:00:07 +0700

    [sp4][journal]: add export function.

--- modules/weko-indextree-journal/weko_indextree_journal/views.py
+++ modules/weko-indextree-journal/weko_indextree_journal/views.py
@@ -34,6 +34,37 @@ blueprint = Blueprint(
     url_prefix='/indextree/journal',
 )
 
+@blueprint.route("/")
+def index():
+    """Render a basic view."""
+    item_type_id = 19 # item tpye's journal = 21
+    lists = ItemTypes.get_latest()
+    if lists is None or len(lists) == 0:
+        return render_template(
+            current_app.config['WEKO_ITEMS_UI_ERROR_TEMPLATE']
+        )
+    item_type = ItemTypes.get_by_id(item_type_id)
+    if item_type is None:
+        return
+    json_schema = '/indextree/journal/jsonschema/{}'.format(item_type_id)
+    schema_form = '/indextree/journal/schemaform/{}'.format(item_type_id)
+    json_record = []
+
+    return render_template(
+        current_app.config['WEKO_INDEXTREE_JOURNAL_INDEX_TEMPLATE'],
+        get_tree_json=current_app.config['WEKO_INDEX_TREE_LIST_API'],
+        upt_tree_json='',
+        mod_tree_detail=current_app.config['WEKO_INDEX_TREE_API'],
+        mod_journal_detail=current_app.config['WEKO_INDEXTREE_JOURNAL_API'],
+        record=json_record,
+        jsonschema=json_schema,
+        schemaform=schema_form,
+        lists=lists,
+        links=None,
+        id=item_type_id,
+        files=None,
+        pid=None
+    )
 
 @blueprint.route("/<int:index_id>")
 def index(index_id = 0):
@@ -55,9 +86,9 @@ def index(index_id = 0):
         journal = Journals.get_journal_by_index_id(index_id)
 
     if journal is None:
-        journal = '{}'
+        journal = []
 
-    json_record = jsonify(journal)
+    json_record = journal
 
     return render_template(
         current_app.config['WEKO_INDEXTREE_JOURNAL_INDEX_TEMPLATE'],
@@ -72,7 +103,8 @@ def index(index_id = 0):
         links=None,
         id=item_type_id,
         files=None,
-        pid=None
+        pid=None,
+        index_id = index_id
     )
 
 @blueprint.route("/index/<int:index_id>")
