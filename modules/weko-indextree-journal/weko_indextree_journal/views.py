437444388cace135445eda0ca83a97efcf206984
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-02-27 18:18:37 +0700

    [sp4][journal]: add export function.

--- modules/weko-indextree-journal/weko_indextree_journal/views.py
+++ modules/weko-indextree-journal/weko_indextree_journal/views.py
@@ -98,7 +98,7 @@ def export_journals():
         journals = Journals.get_all_journals()
         results = [obj.__dict__ for obj in journals]
         data = numpy.asarray(results)
-        numpy.savetxt("journal.tsv", data, delimiter='\t')
+        numpy.savetxt("journal.tsv", data, delimiter=",")
         print("[Log]: results")
         print(results)
 
