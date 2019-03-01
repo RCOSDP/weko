1097dd496db2e8dc8a7d4637e1584c3d2469d500
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-01 14:38:30 +0700

    [sp4][journal]: add export function.

--- modules/weko-indextree-journal/weko_indextree_journal/views.py
+++ modules/weko-indextree-journal/weko_indextree_journal/views.py
@@ -57,7 +57,7 @@ def index(index_id = 0):
     journal_id = None
     if index_id > 0:
         journal = Journals.get_journal_by_index_id(index_id)
-        if journal is not None
+        if journal is not None:
             journal_id = journal.get("id")
 
     json_record = journal
