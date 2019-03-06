213d724d650f59b0d6db5fab43773fbb842fa005
weko3-dev22 <weko3-hitachi-dev22@ml.itg.hitachi.co.jp>
2019-03-06 16:39:18 +0700

    Add scripts/demo/sys_lang.sql Implement class AdminLangSettings

---
+++ scripts/demo/sys_lang.sql
@@ -0,0 +1,38 @@
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('chi', 'Chinese', 'true', 001, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('ind', 'Indonesian', 'true', 002, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('vie', 'Vietnamese', 'true', 003, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('may', 'Malay', 'true', 004, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('tgl', 'Tagalog', 'true', 005, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('tha', 'Thai', 'true', 006, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('hin', 'Hindi', 'true', 007, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('ara', 'Arabic', 'true', 008, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('jpn', 'Japanese', 'true', 009, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('eng', 'English', 'true', 010, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('ab1', 'ABC1', 'true', 011, 'false');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('ab2', 'ABC2', 'false', 012, 'true');
+
+INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
+VALUES ('ab3', 'ABC3', 'false', 013, 'false');
