91efe2abd3fc6e956273485f83604c99d62efa4f
weko3-dev22 <weko3-hitachi-dev22@ml.itg.hitachi.co.jp>
2019-03-11 18:03:56 +0700

    Change language registered and number order of language

--- scripts/demo/sys_lang.sql
+++ scripts/demo/sys_lang.sql
@@ -1,29 +1,29 @@
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('chi', 'Chinese', 'true', 001, 'true');
+VALUES ('chi', 'Chinese', 'true', 000, 'false');
 
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('ind', 'Indonesian', 'true', 002, 'true');
+VALUES ('ind', 'Indonesian', 'true', 000, 'false');
 
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('vie', 'Vietnamese', 'true', 003, 'true');
+VALUES ('vie', 'Vietnamese', 'true', 000, 'false');
 
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('may', 'Malay', 'true', 004, 'true');
+VALUES ('may', 'Malay', 'true', 000, 'false');
 
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('tgl', 'Tagalog', 'true', 005, 'true');
+VALUES ('tgl', 'Tagalog', 'true', 000, 'false');
 
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('tha', 'Thai', 'true', 006, 'true');
+VALUES ('tha', 'Thai', 'true', 000, 'false');
 
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('hin', 'Hindi', 'true', 007, 'true');
+VALUES ('hin', 'Hindi', 'true', 000, 'false');
 
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('ara', 'Arabic', 'true', 008, 'true');
+VALUES ('ara', 'Arabic', 'true', 000, 'false');
 
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('jpn', 'Japanese', 'true', 009, 'true');
+VALUES ('jpn', 'Japanese', 'true', 001, 'true');
 
 INSERT INTO admin_lang_settings (lang_code, lang_name, is_registered, sequence, is_active)
-VALUES ('eng', 'English', 'true', 010, 'true');
+VALUES ('eng', 'English', 'true', 002, 'true');
