3bb9ced4ba40b54d5c57fbd48a39c02e826c4fa7
weko3-dev26 <weko3-hitachi-dev26@ml.itg.hitachi.co.jp>
2019-03-12 15:11:11 +0700

    change source code from ['is_registered'], ['lang_name'], ['sequence'] to .get()

--- modules/weko-admin/weko_admin/utils.py
+++ modules/weko-admin/weko_admin/utils.py
@@ -112,9 +112,9 @@ def update_admin_lang_setting(admin_lang_settings):
     try:
         for admin_lang in admin_lang_settings:
             AdminLangSettings.update_lang(admin_lang.get('lang_code'),
-                                          admin_lang['lang_name'],
-                                          admin_lang['is_registered'],
-                                          admin_lang['sequence'])
+                                          admin_lang.get('lang_name'),
+                                          admin_lang.get('is_registered'),
+                                          admin_lang.get('sequence'))
     except Exception as e:
         return str(e)
     return 'success'
