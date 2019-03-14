3fd448c4af74d8f8fef46d5cd32991a7b6eb20c6
weko3-dev22 <weko3-hitachi-dev22@ml.itg.hitachi.co.jp>
2019-03-14 10:09:26 +0700

    Revert file to 11/5/2018 10:27:48 AM

--- modules/weko-theme/weko_theme/static/js/weko_theme/base.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/base.js
@@ -9,31 +9,5 @@ require([
       $('#btn_edit_stop').on('click', function(){
         window.location.href = '/';
       });
-
-      const urlLoad = '/api/admin/load_lang';
-      $.ajax({
-        url: urlLoad,
-        type: 'GET',
-        success: function (data) {
-          const results = data.results.filter(ele => ele.is_registered).sort((o1, o2) => {
-            return o1.sequence - o2.sequence;
-          });
-          let options = '';
-          for (let index = 0; index < results.length; index++) {
-            const element = results[index];
-            if (!index) {
-              options += `<option value="${element.lang_code}" selected>${element.lang_name}</option>`;
-              continue;
-            }
-            options += `<option value="${element.lang_code}">${element.lang_name}</option>`;
-        }
-          const select = $('select[id=\'lang-code\']');
-          select.children().remove();
-          select.append(options);
-        },
-        error: function (error) {
-          console.log(error);
-        }
-      });
     });
 });
