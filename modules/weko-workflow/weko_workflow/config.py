282eddb012870a88157dcf6eefc7152947daae3b
weko_dev17 <weko3-hitachi-dev17@ml.itg.hitachi.co.jp>
2019-03-18 16:51:53 +0700

    edit Identifier Grant

--- modules/weko-workflow/weko_workflow/config.py
+++ modules/weko-workflow/weko_workflow/config.py
@@ -33,10 +33,10 @@ WEKO_WORKFLOW_ACTION_ENDPOINTS = {
     }
 }
 
-IDENTIFIER_GRANT_LIST = [(0, 'Not Grant'),
-                         (1, 'JaLC DOI'),
-                         (2, 'JaLC CrossRef DOI'),
-                         (3, 'JaLC DataCite DOI'),
-                         (4, 'CNRI')
+IDENTIFIER_GRANT_LIST = [('0', 'Not Grant'),
+                         ('1', 'JaLC DOI'),
+                         ('2', 'JaLC CrossRef DOI'),
+                         ('3', 'JaLC DataCite DOI'),
+                         ('4', 'CNRI')
                         ]
 """Options list for Identifier Grant action."""
