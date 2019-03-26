aa1413da4820cd807ccb7fdd18042fdf3ecac34e
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-26 13:47:55 +0700

    [sp6][citation style language] - change port of weko_dev23.

--- modules/weko-records-ui/weko_records_ui/bundles.py
+++ modules/weko-records-ui/weko_records_ui/bundles.py
@@ -24,8 +24,17 @@ from flask_assets import Bundle
 from invenio_assets import NpmBundle
 
 style = Bundle(
+    'node_modules/angular-loading-bar/build/loading-bar.css',
+    'node_modules/typeahead.js-bootstrap-css/typeaheadjs.css',
+    'node_modules/bootstrap-switch/dist/css/bootstrap3',
     'css/weko_records_ui/style.css',
-    output="gen/weko_records_ui.%(version)s.css"
+    output="gen/weko_records_ui.%(version)s.css",
+    npm={
+        'bootstrap-sass': '~3.3.5',
+        'bootstrap-switch': '~3.0.2',
+        'font-awesome': '~4.4.0',
+        'typeahead.js-bootstrap-css': '~1.2.1',
+    }
 )
 
 js_dependencies = NpmBundle(
@@ -39,11 +48,22 @@ js_dependencies = NpmBundle(
 js = NpmBundle(
     'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
     'node_modules/angular/angular.js',
+    'node_modules/angular-loading-bar/build/*.js',
+    'node_modules/typeahead.js/dist/*.js',
+    'node_modules/invenio-csl-js/dist/*.js',
+    'node_modules/bootstrap-switch/dist/js/bootstrap-switch.js',
     'js/weko_records_ui/detail.js',
     'js/weko_records_ui/app.js',
     filters='jsmin',
     output="gen/weko_records_ui.%(version)s.js",
     npm={
         'angular-ui-bootstrap': '~0.13.2',
+        'almond': '~0.3.1',
+        'angular': '~1.4.9',
+        'angular-sanitize': '~1.4.9',
+        'angular-loading-bar': '~0.9.0',
+        'bootstrap-switch': '~3.0.2',
+        'invenio-csl-js': '~0.1.3',
+        'typeahead.js': '~0.11.1',
     },
 )
