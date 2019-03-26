be906479c77359cb891a5992dc75333ad8fac408
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-26 14:18:44 +0700

    [sp6][citation style language] - change port of weko_dev23.

--- modules/weko-records-ui/weko_records_ui/bundles.py
+++ modules/weko-records-ui/weko_records_ui/bundles.py
@@ -45,6 +45,12 @@ js = NpmBundle(
     'node_modules/invenio-csl-js/dist/*.js',
     'js/weko_records_ui/detail.js',
     'js/weko_records_ui/app.js',
+    depends=(
+        'node_modules/angular-loading-bar/build/*.js',
+        'node_modules/typeahead.js/dist/*.js',
+        'node_modules/invenio-csl-js/dist/*.js',
+        'node_modules/bootstrap-switch/dist/js/bootstrap-switch.js',
+    ),
     filters='jsmin',
     output="gen/weko_records_ui.%(version)s.js",
     npm={
