5b430c1a91a048abd0280255732d6d4f43b29acc
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-26 19:19:01 +0700

    [sp6][citation style language] - change port of weko_dev23.

--- modules/weko-records-ui/weko_records_ui/bundles.py
+++ modules/weko-records-ui/weko_records_ui/bundles.py
@@ -29,18 +29,37 @@ style = Bundle(
     output="gen/weko_records_ui.%(version)s.css"
 )
 
+js_dependencies_angular_loading_bar = NpmBundle(
+    'node_modules/angular-loading-bar/build/*.js',
+    npm={
+        'angular-loading-bar': '~0.9.0',
+    }
+)
+
+js_dependencies_invenio_csl_js = NpmBundle(
+    'node_modules/invenio-csl-js/dist/*.js',
+    npm={
+        'invenio-csl-js': '~0.1.3',
+    }
+)
+
+js_dependencies_typeahead_js = NpmBundle(
+    'node_modules/typeahead.js/dist/*.js',
+    npm={
+        'typeahead.js': '~0.11.1',
+    }
+)
+
 js_dependencies = NpmBundle(
     'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
     'node_modules/angular/angular.js',
-    'node_modules/angular-loading-bar/build/*.js',
-    'node_modules/typeahead.js/dist/*.js',
-    'node_modules/invenio-csl-js/dist/*.js',
     'node_modules/bootstrap-switch/dist/js/bootstrap-switch.js',
+    js_dependencies_angular_loading_bar,
+    js_dependencies_invenio_csl_js,
+    js_dependencies_typeahead_js,
     npm={
         'angular-ui-bootstrap': '~0.13.2',
         'bootstrap-switch': '~3.0.2',
-        'invenio-csl-js': '~0.1.3',
-        'typeahead.js': '~0.11.1',
         'typeahead.js-bootstrap-css': '~1.2.1',
     },
     filters='requirejs',
