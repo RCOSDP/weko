51153766b11330eb27c7b15a52e9990ef27a91fd
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-27 14:46:13 +0700

    [sp6][citation style language] - update js dependencies.

--- modules/weko-records-ui/weko_records_ui/bundles.py
+++ modules/weko-records-ui/weko_records_ui/bundles.py
@@ -38,14 +38,54 @@ js_dependecies = NpmBundle(
     output='gen/weko_records_ui.dependencies.%(version)s.js',
 )
 
+# js = NpmBundle(
+#     'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
+#     'node_modules/angular/angular.js',
+#     'js/weko_records_ui/detail.js',
+#     'js/weko_records_ui/app.js',
+#     filters='jsmin',
+#     output="gen/weko_records_ui.%(version)s.js",
+#     npm={
+#         'angular-ui-bootstrap': '~0.13.2',
+#     },
+# )
+
 js = NpmBundle(
-    'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
-    'node_modules/angular/angular.js',
-    'js/weko_records_ui/detail.js',
-    'js/weko_records_ui/app.js',
+    # Bundle(
+    #     'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
+    #     'node_modules/angular/angular.js',
+    #     'js/weko_records_ui/detail.js',
+    #     'js/weko_records_ui/app.js',
+    #     filters='jsmin',
+    #     npm={
+    #         'angular-ui-bootstrap': '~0.13.2',
+    #     },
+    # ),
+    Bundle(
+        'node_modules/almond/almond.js',
+        'js/modernizr-custom.js',
+        filters='uglifyjs',
+    ),
+    depends=(
+        'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
+        'node_modules/angular/angular.js',
+        'js/weko_records_ui/detail.js',
+        'js/weko_records_ui/app.js',
+        'node_modules/angular-loading-bar/build/*.js',
+        'node_modules/typeahead.js/dist/*.js',
+        'node_modules/invenio-csl-js/dist/*.js',
+        'node_modules/bootstrap-switch/dist/js/bootstrap-switch.js',
+    ),
     filters='jsmin',
     output="gen/weko_records_ui.%(version)s.js",
     npm={
+        'almond': '~0.3.1',
+        'angular': '~1.4.9',
+        'angular-sanitize': '~1.4.9',
+        'angular-loading-bar': '~0.9.0',
+        'bootstrap-switch': '~3.0.2',
+        'invenio-csl-js': '~0.1.3',
+        'typeahead.js': '~0.11.1',
         'angular-ui-bootstrap': '~0.13.2',
-    },
+    }
 )
