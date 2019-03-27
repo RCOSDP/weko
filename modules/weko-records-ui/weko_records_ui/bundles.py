631b8b06b54e5b002067a1847722f5c1b45655f1
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-27 16:39:11 +0700

    [sp6][citation style language] - update js dependencies.

--- modules/weko-records-ui/weko_records_ui/bundles.py
+++ modules/weko-records-ui/weko_records_ui/bundles.py
@@ -36,52 +36,13 @@ js_dependecies_csl = NpmBundle(
     }
 )
 
-js_dependecies = NpmBundle(
-    js_dependecies_csl,
-    filters='requirejs',
-    output='gen/weko_records_ui.dependencies.csl.js',
-)
-
-# js = NpmBundle(
-#     'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
-#     'node_modules/angular/angular.js',
-#     'js/weko_records_ui/detail.js',
-#     'js/weko_records_ui/app.js',
-#     filters='jsmin',
-#     output="gen/weko_records_ui.%(version)s.js",
-#     npm={
-#         'angular-ui-bootstrap': '~0.13.2',
-#     },
-# )
-
-js = NpmBundle(
-    # Bundle(
-    #     'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
-    #     'node_modules/angular/angular.js',
-    #     'js/weko_records_ui/detail.js',
-    #     'js/weko_records_ui/app.js',
-    #     filters='jsmin',
-    #     npm={
-    #         'angular-ui-bootstrap': '~0.13.2',
-    #     },
-    # ),
-    'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
-    'node_modules/angular/angular.js',
-    'js/weko_records_ui/detail.js',
-    'js/weko_records_ui/app.js',
-    Bundle(
-        'node_modules/almond/almond.js',
-        filters='uglifyjs',
-    ),
-    depends=(
-        'node_modules/angular-loading-bar/build/*.js',
-        'node_modules/typeahead.js/dist/*.js',
-        'node_modules/invenio-csl-js/dist/*.js',
-        'node_modules/bootstrap-switch/dist/js/bootstrap-switch.js',
-    ),
-    filters='jsmin',
-    output="gen/weko_records_ui.%(version)s.js",
+js_dependencies_csl = NpmBundle(
+    'node_modules/angular-loading-bar/build/*.js',
+    'node_modules/typeahead.js/dist/*.js',
+    'node_modules/invenio-csl-js/dist/*.js',
+    'node_modules/bootstrap-switch/dist/js/bootstrap-switch.js',
     npm={
+        'angular-ui-bootstrap': '~0.1.3',
         'almond': '~0.3.1',
         'angular': '~1.4.9',
         'angular-sanitize': '~1.4.9',
@@ -89,6 +50,62 @@ js = NpmBundle(
         'bootstrap-switch': '~3.0.2',
         'invenio-csl-js': '~0.1.3',
         'typeahead.js': '~0.11.1',
-        'angular-ui-bootstrap': '~0.13.2',
-    }
+    },
 )
+
+js_dependencies = NpmBundle(
+    js_dependencies_csl,
+    filters='jsmin',
+    output='gen/weko_records_ui.dependencies.%(version)s.js',
+)
+
+# js_dependecies = NpmBundle(
+#     js_dependecies_csl,
+#     filters='requirejs',
+#     output='gen/weko_records_ui_all_csl.dependencies.js',
+# )
+
+js = NpmBundle(
+    'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
+    'node_modules/angular/angular.js',
+    'js/weko_records_ui/detail.js',
+    'js/weko_records_ui/app.js',
+    filters='jsmin',
+    output="gen/weko_records_ui.%(version)s.js",
+    npm={
+        'angular-ui-bootstrap': '~0.13.2',
+    },
+)
+
+# js = NpmBundle(
+#     # Bundle(
+#     #     'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
+#     #     'node_modules/angular/angular.js',
+#     #     'js/weko_records_ui/detail.js',
+#     #     'js/weko_records_ui/app.js',
+#     #     filters='jsmin',
+#     #     npm={
+#     #         'angular-ui-bootstrap': '~0.13.2',
+#     #     },
+#     # ),
+#     'node_modules/angular-ui-bootstrap/ui-bootstrap-tpls.js',
+#     'node_modules/angular/angular.js',
+#     'js/weko_records_ui/detail.js',
+#     'js/weko_records_ui/app.js',
+#     Bundle(
+#         'node_modules/almond/almond.js',
+#         filters='uglifyjs',
+#     ),
+#     filters='jsmin',
+#     output="gen/weko_records_ui.%(version)s.js",
+#     npm={
+#         # 'almond': '~0.3.1',
+#         # 'angular': '~1.4.9',
+#         # 'angular-sanitize': '~1.4.9',
+#         # 'angular-loading-bar': '~0.9.0',
+#         # 'bootstrap-switch': '~3.0.2',
+#         # 'invenio-csl-js': '~0.1.3',
+#         # 'typeahead.js': '~0.11.1',
+#         'angular-ui-bootstrap': '~0.13.2',
+#     }
+# )
