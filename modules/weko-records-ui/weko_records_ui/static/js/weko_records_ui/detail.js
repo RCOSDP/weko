9078c25d14207d43fd9258bc6e684ba8220bf633
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-27 23:49:07 +0700

    [sp6][citation style language] - update js dependencies.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
@@ -1,13 +1,13 @@
 require([
   "jquery",
   "bootstrap",
-  "bloodhound",
-  "typeahead.js",
   "node_modules/angular/angular",
   "node_modules/angular-sanitize/angular-sanitize",
   "node_modules/angular-loading-bar/build/loading-bar",
   "node_modules/invenio-csl-js/dist/invenio-csl-js",
   "node_modules/bootstrap-switch/dist/js/bootstrap-switch",
+  "bloodhound",
+  "typeahead.js",
 ], function() {
   $('#btn_back').on('click', function(){
     window.history.back();
