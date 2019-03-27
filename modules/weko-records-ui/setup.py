3f4e53c60fb3927eed3c37b2b800faa2fb60bcf5
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-27 16:52:13 +0700

    [sp6][citation style language] - update js dependencies.

--- modules/weko-records-ui/setup.py
+++ modules/weko-records-ui/setup.py
@@ -102,6 +102,7 @@ setup(
         'invenio_assets.bundles': [
             'weko_records_ui_css = weko_records_ui.bundles:style',
             'weko_records_ui_js = weko_records_ui.bundles:js',
+            'weko_records_ui_js_csl = weko_records_ui.bundles:js_dependencies'
         ],
         'invenio_access.actions': [
             'detail_page_access'
