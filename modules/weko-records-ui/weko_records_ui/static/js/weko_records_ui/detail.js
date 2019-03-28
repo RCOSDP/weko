6962ddc5bcc6acbc0f2d5f4cc9b78e473dee2a5e
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-29 01:01:25 +0700

    [sp6][citation style language] - apply csl bundle.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
@@ -1,3 +1,29 @@
+require.config({
+  baseUrl: "/static/",
+  paths: {
+    jquery: "node_modules/jquery/jquery",
+    bootstrap: "node_modules/bootstrap-sass/assets/javascripts/bootstrap",
+    angular: "node_modules/angular/angular",
+    typeahead: 'node_modules/typeahead.js/dist/typeahead.jquery',
+    bloodhound: 'node_modules/typeahead.js/dist/bloodhound',
+    clipboard: 'node_modules/clipboard/dist/clipboard',
+  },
+  shim: {
+    angular: {
+      exports: 'angular'
+    },
+    jquery: {
+      exports: "$"
+    },
+    bootstrap: {
+      deps: ["jquery"]
+    },
+    clipboard: {
+      exports: "Clipboard"
+    }
+  }
+});
+
 require([
   "jquery",
   "bootstrap"
