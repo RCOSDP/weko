92bdeda5c36d8f3e54e7a89438be9e33453a3084
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-28 15:41:21 +0700

    [sp6][citation style language] - build bundle of weko_records_ui.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/csl.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/csl.js
@@ -1,17 +1,25 @@
+// require([
+//     "jquery",
+//     "bootstrap",
+//     // "typeahead.js",
+//     // "bloodhound",
+//     // "node_modules/angular/angular",
+//     // "node_modules/invenio-csl-js/dist/invenio-csl-js",
+//     // "node_modules/invenio-search-js/dist/invenio-search-js",
+// ], function() {
+//     console.log("Hello World");
+//     // angular.element(document).ready(function() {
+//     //     angular.bootstrap(document.getElementById("invenio-csl"), [
+//     //         'invenioCsl',
+//     //         ]
+//     //     );
+//     // });  
+// });
+
 require([
-    "jquery",
-    "bootstrap",
-    // "typeahead.js",
-    // "bloodhound",
-    // "node_modules/angular/angular",
-    // "node_modules/invenio-csl-js/dist/invenio-csl-js",
-    // "node_modules/invenio-search-js/dist/invenio-search-js",
+"jquery",
+"bootstrap"
 ], function() {
     console.log("Hello World");
-    // angular.element(document).ready(function() {
-    //     angular.bootstrap(document.getElementById("invenio-csl"), [
-    //         'invenioCsl',
-    //         ]
-    //     );
-    // });  
 });
+  
