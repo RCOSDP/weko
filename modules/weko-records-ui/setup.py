371a40d23bae2698a6d726e2beb43d5a57b9b4eb
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-28 16:14:36 +0700

    [sp6][citation style language] - apply csl bundle.

--- modules/weko-records-ui/setup.py
+++ modules/weko-records-ui/setup.py
@@ -101,9 +101,8 @@ setup(
         ],
         'invenio_assets.bundles': [
             'weko_records_ui_css = weko_records_ui.bundles:style',
-            'weko_records_ui_cites_js = weko_records_ui.bundles:js_dependencies_cites',
-            'weko_records_ui_js = weko_records_ui.bundles:js',
             'weko_records_ui_csl_js = weko_records_ui.bundles:js_csl',
+            'weko_records_ui_js = weko_records_ui.bundles:js',
         ],
         'invenio_access.actions': [
             'detail_page_access'
