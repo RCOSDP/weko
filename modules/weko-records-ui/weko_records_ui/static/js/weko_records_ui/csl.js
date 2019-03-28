a9b33e5c05b158186a3913c64c9e7cf0c8de2054
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-28 17:16:53 +0700

    [sp6][citation style language] - apply csl bundle.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/csl.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/csl.js
@@ -1,11 +1,10 @@
 require([
-    "jquery",
-    "bootstrap",
+    // "jquery",
+    // "bootstrap",
     "typeahead.js",
     "bloodhound",
     "node_modules/angular/angular",
     "node_modules/invenio-csl-js/dist/invenio-csl-js",
-    "node_modules/invenio-search-js/dist/invenio-search-js",
 ], function() {
     console.log("Hello World");
     angular.element(document).ready(function() {
