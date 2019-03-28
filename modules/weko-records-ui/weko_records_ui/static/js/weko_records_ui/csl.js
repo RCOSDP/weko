bc6585c926050d4d11fd38f02d7d36220c25255a
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-28 15:30:20 +0700

    [sp6][citation style language] - build bundle of weko_records_ui.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/csl.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/csl.js
@@ -1,12 +1,12 @@
 require([
-    // "jquery",
-    // "bootstrap",
+    "jquery",
+    "bootstrap",
     // "typeahead.js",
     // "bloodhound",
     // "node_modules/angular/angular",
     // "node_modules/invenio-csl-js/dist/invenio-csl-js",
     // "node_modules/invenio-search-js/dist/invenio-search-js",
-], function(angular) {
+], function() {
     angular.element(document).ready(function() {
         angular.bootstrap(document.getElementById("invenio-csl"), [
             'invenioCsl',
