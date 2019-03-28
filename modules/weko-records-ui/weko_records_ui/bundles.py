74bd439d7d0841bf9dfa2031560194d4f5c0347d
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-03-28 15:56:08 +0700

    [sp6][citation style language] - build bundle of weko_records_ui.

--- modules/weko-records-ui/weko_records_ui/bundles.py
+++ modules/weko-records-ui/weko_records_ui/bundles.py
@@ -41,11 +41,11 @@ style = Bundle(
 #    },
 #)
 
-js_dependencies_cites = NpmBundle(
-    js_dependencies_jquery,
-    #filters='uglifyjs',
-    #output='gen/weko_records_ui.deposit.%(version)s.js',
-)
+# js_dependencies_cites = NpmBundle(
+#     js_dependencies_jquery,
+#     #filters='uglifyjs',
+#     #output='gen/weko_records_ui.deposit.%(version)s.js',
+# )
 
 # js_dependencies_cites = NpmBundle(
 #     js_dependencies_jquery,
