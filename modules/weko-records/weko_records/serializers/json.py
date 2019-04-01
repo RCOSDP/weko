9cc0e98f736cfe29c6e722503d5e5027c88986ad
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-04-01 17:04:25 +0700

    [sp6][citation style language] - Fix upload file.

--- modules/weko-records/weko_records/serializers/json.py
+++ modules/weko-records/weko_records/serializers/json.py
@@ -106,6 +106,7 @@ class WekoJSONSerializer(JSONSerializer):
 
     def serialize(self, pid, record, links_factory=None, **kwargs):
         print("[Log]: WekoJSONSerializer >> serialize")
+        print(**kwargs)
         result = super(WekoJSONSerializer, self).serialize(
             pid, record, links_factory=links_factory, **kwargs
         )
