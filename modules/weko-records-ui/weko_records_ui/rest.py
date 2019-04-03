e8cfe461837199e20fadb820a4eefd3c98081436
Hieu Xuan Dinh <hieuxd@hitachiconsulting.com>
2019-04-04 03:56:53 +0700

    [sp6][citation style language] - up serializer config.

--- modules/weko-records-ui/weko_records_ui/rest.py
+++ modules/weko-records-ui/weko_records_ui/rest.py
@@ -94,7 +94,7 @@ def create_blueprint(endpoints):
         cites = WekoRecordsCitesResource.as_view(
             WekoRecordsCitesResource.view_name.format(endpoint),
             serializers=serializers,
-            # pid_type=options['pid_type'],
+            pid_type=options['pid_type'],
             ctx=ctx,
             default_media_type=options.get('default_media_type'),
         )
