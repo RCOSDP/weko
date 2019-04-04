67c30fbc816db23ad35c667c8f9f6588e34677a1
weko3-dev02 <weko3-hitachi-dev02@ml.itg.hitachi.co.jp>
2019-04-04 13:03:41 +0900

    call web api with ajax.

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/file_stats.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/file_stats.js
@@ -1,5 +1,3 @@
-let response_data = null;
-
 $(document).ready(function () {
     let bucket_id = document.getElementById("bucket_id").innerText;
     let file_key = document.getElementById("file_key").innerText;
@@ -11,15 +9,14 @@ $(document).ready(function () {
         contentType: 'application/json; charset=UTF-8',
         success: function(data) {
             if (data != null && 'download_total' in data && 'preview_total' in data) {
-                response_data = data;
                 // total
                 document.getElementById("file_download_num").innerHTML = data.download_total;
                 document.getElementById("file_preview_num").innerHTML = data.preview_total;
                 // period
                 let ddl = document.getElementById("file_period");
                 let ddlHtml = ddl.innerHTML;
-                for (let key in data.period) {
-                    ddlHtml += '<option value=' + key + '>' + key + '</option>';
+                for (let i in data.period) {
+                    ddlHtml += '<option value=' + data.period[i] + '>' + data.period[i] + '</option>';
                 }
                 ddl.innerHTML = ddlHtml;
                 // domain
@@ -34,35 +31,49 @@ $(document).ready(function () {
             } else {
                 document.getElementById("file_download_num").innerHTML = 0;
                 document.getElementById("file_preview_num").innerHTML = 0;
+                document.getElementById("file_domain").innerHTML = '';
             }
         },
         error: function() {
             document.getElementById("file_download_num").innerHTML = 0;
             document.getElementById("file_preview_num").innerHTML = 0;
+            document.getElementById("file_domain").innerHTML = '';
         }
     });
 });
 
 function period_change(control) {
     date = control.value;
-    if (response_data != null) {
-        data = null
-        if (date == 'total') {
-            data = response_data;
-        } else {
-            data = response_data.period[date];
-        }
-        if (data != null) {
-            document.getElementById("file_download_num").innerHTML = data.download_total;
-            document.getElementById("file_preview_num").innerHTML = data.preview_total;
-            let tableHtml = '';
-            for (let i in data.domain_list) {
-                d = data.domain_list[i]
-                tableHtml += '<tr><td>' + d.domain + '</td><td>' +
-                    d.download_counts + '</td><td>' +
-                    d.preview_counts + '</td></tr>';
+    let bucket_id = document.getElementById("bucket_id").innerText;
+    let file_key = document.getElementById("file_key").innerText;
+    let statsurl = '/api/stats/' + bucket_id + '/' + file_key;
+    $.ajax({
+        url: statsurl,
+        type: 'POST',
+        data: '{"date": "' + date + '"}',
+        contentType: 'application/json; charset=UTF-8',
+        success: function(data) {
+            if (data != null && 'download_total' in data && 'preview_total' in data) {
+                document.getElementById("file_download_num").innerHTML = data.download_total;
+                document.getElementById("file_preview_num").innerHTML = data.preview_total;
+                let tableHtml = '';
+                for (let i in data.domain_list) {
+                    d = data.domain_list[i]
+                    tableHtml += '<tr><td>' + d.domain + '</td><td>' +
+                        d.download_counts + '</td><td>' +
+                        d.preview_counts + '</td></tr>';
+                }
+                document.getElementById("file_domain").innerHTML = tableHtml;
+            } else {
+                document.getElementById("file_download_num").innerHTML = 0;
+                document.getElementById("file_preview_num").innerHTML = 0;
+                document.getElementById("file_domain").innerHTML = '';
             }
-            document.getElementById("file_domain").innerHTML = tableHtml;
+        },
+        error: function() {
+            document.getElementById("file_download_num").innerHTML = 0;
+            document.getElementById("file_preview_num").innerHTML = 0;
+            document.getElementById("file_domain").innerHTML = '';
         }
-    }
+    });
 }
--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/record_view_stats.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/record_view_stats.js
@@ -1,7 +1,6 @@
 
 $(document).ready(function () {  
     let record_id = document.getElementById("record_id").innerText;
-
     let statsurl = '/api/stats/' + record_id
     $.ajax({
         url: statsurl,
@@ -26,10 +25,12 @@ $(document).ready(function () {
                 document.getElementById("record_view_domain").innerHTML = tableHtml;
             } else {
                 document.getElementById("record_view_num").innerHTML = 0;
+                document.getElementById("record_view_domain").innerHTML = '';
             }
         },
         error: function() {
             document.getElementById("record_view_num").innerHTML = 0;
+            document.getElementById("record_view_domain").innerHTML = '';
         }
     });
 });
@@ -38,43 +39,24 @@ function period_change (control) {
     date = control.value;
     let record_id = document.getElementById("record_id").innerText;
     let statsurl = '/api/stats/' + record_id;
-    if (date == 'total') {
-        $.ajax({
-            url: statsurl,
-            type: 'GET',
-            contentType: 'application/json; charset=UTF-8',
-            success: function(data) {
-                if (data != null && 'total' in data) {
-                    data_binding(data)
+    $.ajax({
+        url: statsurl,
+        type: 'POST',
+        data: '{"date": "' + date + '"}',
+        contentType: 'application/json; charset=UTF-8',
+        success: function(data) {
+            if (data != null && 'total' in data) {
+                document.getElementById("record_view_num").innerHTML = data.total;
+                let tableHtml = '';
+                for (let key in data.domain) {
+                    tableHtml += '<tr><td>' + key + '</td><td>' + data.domain[key] + '</td></tr>';
                 }
-            },
-            error: function(data) {
-                document.getElementById("record_view_num").innerHTML = 0;
+                document.getElementById("record_view_domain").innerHTML = tableHtml;
             }
-        });
-    } else {
-        $.ajax({
-            url: statsurl,
-            type: 'POST',
-            data: '{"date": ' + date + '}',
-            contentType: 'application/json; charset=UTF-8',
-            success: function(data) {
-                if (data != null && 'total' in data) {
-                    data_binding(data)
-                }
-            },
-            error: function(data) {
-                document.getElementById("record_view_num").innerHTML = 0;
-            }
-        });
-    }
-}
-
-function data_binding (data) {
-    document.getElementById("record_view_num").innerHTML = data.total;
-    let tableHtml = '';
-    for (let key in data.domain) {
-        tableHtml += '<tr><td>' + key + '</td><td>' + data.domain[key] + '</td></tr>';
-    }
-    document.getElementById("record_view_domain").innerHTML = tableHtml;
+        },
+        error: function(data) {
+            document.getElementById("record_view_num").innerHTML = 0;
+            document.getElementById("record_view_domain").innerHTML = '';
+        }
+    });
 }
