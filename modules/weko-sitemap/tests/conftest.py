28865e72d4e681bb0677f13a907c170229bf323b
weko3-dev02 <weko3-hitachi-dev02@ml.itg.hitachi.co.jp>
2019-04-04 15:41:26 +0900

    sitemap xml visible

---
+++ modules/weko-sitemap/tests/conftest.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 National Institute of Informatics.
+#
+# weko-sitemap is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pytest configuration.
+
+See https://pytest-invenio.readthedocs.io/ for documentation on which test
+fixtures are available.
+"""
+
+from __future__ import absolute_import, print_function
+
+import shutil
+import tempfile
+
+import pytest
+from flask import Flask
+from flask_babelex import Babel
+
+from weko_sitemap import WekoSitemap
+from weko_sitemap.views import blueprint
+
+
+@pytest.fixture(scope='module')
+def celery_config():
+    """Override pytest-invenio fixture.
+
+    TODO: Remove this fixture if you add Celery support.
+    """
+    return {}
+
+
+@pytest.fixture(scope='module')
+def create_app(instance_path):
+    """Application factory fixture."""
+    def factory(**config):
+        app = Flask('testapp', instance_path=instance_path)
+        app.config.update(**config)
+        Babel(app)
+        WekoSitemap(app)
+        app.register_blueprint(blueprint)
+        return app
+    return factory
