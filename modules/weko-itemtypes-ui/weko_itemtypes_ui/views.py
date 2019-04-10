758ef687b4de59fd9f945f067ddb5f94b5616dab
weko_dev17 <weko3-hitachi-dev17@ml.itg.hitachi.co.jp>
2019-04-10 16:43:06 +0700

    create API

--- modules/weko-itemtypes-ui/weko_itemtypes_ui/views.py
+++ modules/weko-itemtypes-ui/weko_itemtypes_ui/views.py
@@ -341,22 +341,12 @@ def mapping_register():
 @login_required
 @item_type_permission.require(http_exception=403)
 def delete_itemtype(item_type_id=0):
-    """Delete an item type."""
+    """Soft-delete an item type."""
     if item_type_id > 0:
-        result = ItemTypes.get_by_id(id_=item_type_id)
-        if result is not None:
-            data = result.render
+        record = ItemTypes.get_by_id(id_=item_type_id)
+        if record is not None:
             try:
-                record = ItemTypes.update(
-                    id_=item_type_id,
-                    name=data.get('table_row_map').get('name'),
-                    schema=data.get('table_row_map').get('schema'),
-                    form=data.get('table_row_map').get('form'),
-                    render=data,
-                    deleted_flg=1
-                )
-                Mapping.create(item_type_id=record.model.id,
-                               mapping=data.get('table_row_map').get('mapping'))
+                record.delete();
                 db.session.commit()
             except BaseException:
                 db.session.rollback()
