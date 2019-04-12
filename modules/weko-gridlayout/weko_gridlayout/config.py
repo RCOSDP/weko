3278d8510f72e918d10aa7f94988eb20ca52a744
weko3-dev21 <weko3-hitachi-dev21@ml.itg.hitachi.co.jp>
2019-04-12 19:38:40 +0900

    Create module

---
+++ modules/weko-gridlayout/weko_gridlayout/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 National Institute of Informatics.
+#
+# weko-gridlayout is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Module of weko-gridlayout."""
+
+# TODO: This is an example file. Remove it if your package does not use any
+# extra configuration variables.
+
+WEKO_GRIDLAYOUT_DEFAULT_VALUE = 'foobar'
+"""Default value for the application."""
+
+WEKO_GRIDLAYOUT_BASE_TEMPLATE = 'weko_gridlayout/base.html'
+"""Default base template for the demo page."""
