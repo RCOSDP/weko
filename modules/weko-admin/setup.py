12a25fb8fa393e112aa215cc9ad1e2849c0618f1
weko3-dev20 <weko3-hitachi-dev20@ml.itg.hitachi.co.jp>
2019-04-16 00:09:47 +0700

    Update widget design setting screen

--- modules/weko-admin/setup.py
+++ modules/weko-admin/setup.py
@@ -108,7 +108,7 @@ setup(
             'weko_admin_language = weko_admin.admin:language_adminview',
             'weko_admin_chunk = weko_admin.admin:chunk_adminview',
             'weko_admin_web_api_account = weko_admin.admin:web_api_account_adminview',
-            'weko_admin_chunk_design = weko_admin.admin:chunk_design_adminview',
+            'weko_admin_widget_design = weko_admin.admin:widget_design_adminview',
         ],
         'invenio_access.actions': [
             'page_style_access = weko_admin.permissions:action_admin_access',
