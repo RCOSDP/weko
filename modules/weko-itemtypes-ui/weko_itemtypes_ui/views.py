29807dd00ad7e5b82d3ac221f924fcedc7b1de27
weko3-dev21 <weko3-hitachi-dev21@ml.itg.hitachi.co.jp>
2019-04-17 22:17:45 +0700

    log get_record of ItemTypeMapping

--- modules/weko-itemtypes-ui/weko_itemtypes_ui/views.py
+++ modules/weko-itemtypes-ui/weko_itemtypes_ui/views.py
@@ -121,6 +121,7 @@ def register(item_type_id=0):
                        mapping=data.get('table_row_map').get('mapping'))
         print('Mapping get record ', Mapping.get_record(record.model.id))
         print('Item type record ', ItemTypes.get_record(record.model.id))
+        print('Item type all record ', ItemTypes.get_records())
         db.session.commit()
     except BaseException:
         db.session.rollback()
