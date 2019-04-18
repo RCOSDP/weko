11ebb5a53a7ff6b77f89491001e00ef7eb149681
weko3-dev21 <weko3-hitachi-dev21@ml.itg.hitachi.co.jp>
2019-04-18 16:13:17 +0700

    remove print log.

--- modules/weko-itemtypes-ui/weko_itemtypes_ui/views.py
+++ modules/weko-itemtypes-ui/weko_itemtypes_ui/views.py
@@ -113,16 +113,8 @@ def register(item_type_id=0):
                                       'schema'),
                                   form=data.get('table_row_map').get('form'),
                                   render=data)
-        print('Impact feature mapping.create: item_type_mapping')
-        print('Impact feature item_type_id: ', item_type_id)
-        print('-------------------record.model: ', record.model)
-        print('-------------------record.model.id: ', record.model.id)
         Mapping.create(item_type_id=record.model.id,
                        mapping=data.get('table_row_map').get('mapping'))
-        print('Mapping get record ', Mapping.get_record(record.model.id))
-        print('Item type record ', ItemTypes.get_record(record.model.id))
-
-        print('All record Item type ', ItemTypes.get_records([]))
         db.session.commit()
     except BaseException:
         db.session.rollback()
