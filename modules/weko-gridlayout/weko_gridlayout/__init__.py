2ebdf4887e0ec631805b13d261542b7ab2691f80
weko3-dev20 <weko3-hitachi-dev20@ml.itg.hitachi.co.jp>
2019-04-18 14:16:14 +0700

    Update import

--- modules/weko-gridlayout/weko_gridlayout/__init__.py
+++ modules/weko-gridlayout/weko_gridlayout/__init__.py
@@ -9,7 +9,7 @@
 
 from __future__ import absolute_import, print_function
 
-from .ext import wekogridlayout
+from .ext import WekogridLayout
 from .version import __version__
 
 __all__ = ('__version__', 'WekogridLayout')
