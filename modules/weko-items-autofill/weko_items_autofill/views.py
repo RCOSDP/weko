4de10d8391e4804ac179acc608316a11ae7c5de9
weko3-dev20 <weko3-hitachi-dev20@ml.itg.hitachi.co.jp>
2019-04-24 10:05:01 +0700

    Fixbug: In item registration  screen, Next button occurs error.

--- modules/weko-items-autofill/weko_items_autofill/views.py
+++ modules/weko-items-autofill/weko_items_autofill/views.py
@@ -16,9 +16,8 @@ from weko_admin.utils import get_current_api_certification
 
 from .permissions import auto_fill_permission
 from .utils import get_cinii_data, get_crossref_data, get_item_id, \
-    parse_cinii_json_response, parse_crossref_json_response, \
-    get_title_pubdate_path
-from weko_admin.utils import get_current_api_certification
+    get_title_pubdate_path, parse_cinii_json_response, \
+    parse_crossref_json_response
 
 blueprint = Blueprint(
     "weko_items_autofill",
@@ -103,7 +102,13 @@ def get_selection_option():
     return jsonify(result)
 
 
-@blueprint_api.route('/get_title_pubdate_id/<int:item_type_id>', methods=['GET'])
+@blueprint_api.route('/get_title_pubdate_id/<int:item_type_id>',
+                     methods=['GET'])
 def get_title_pubdate_id(item_type_id=0):
+    """Get title and pubdate id.
+
+    :param item_type_id:
+    :return: result json
+    """
     result = get_title_pubdate_path(item_type_id)
     return jsonify(result)
