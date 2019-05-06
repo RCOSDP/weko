177ecac43c2b6b5f3df25578e0234b2fcb08bfee
weko_dev17 <weko3-hitachi-dev17@ml.itg.hitachi.co.jp>
2019-05-06 15:36:13 +0700

    edit GUI

---
+++ modules/weko-workflow/weko_workflow/static/js/weko_workflow/quit_confirmation.js
@@ -0,0 +1,18 @@
+require([
+  "jquery",
+  "bootstrap"
+], function() {
+  $("#btn_quit").click(function () {
+    console.log('#btn_quit');
+    $("#action_quit_confirmation").modal("show");
+  });
+  $('#btn_cancel').on('click', function(){
+    console.log('#btn-cancel');
+    if ($rootScope === undefined) {
+      console.log('root undefined');
+    } else {
+      console.log('root defined');
+    }
+  });
+  }
+});
