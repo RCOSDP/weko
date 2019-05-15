169fd8972249ea712dbe2c0f041cbc1002490ffc
weko3-dev21 <weko3-hitachi-dev21@ml.itg.hitachi.co.jp>
2019-05-15 14:08:29 +0700

    Edit style for widget item

--- modules/weko-theme/weko_theme/static/js/weko_theme/widget.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/widget.js
@@ -53,8 +53,7 @@ let PageBodyGrid = function () {
     let template =
       '<div class="grid-stack-item">' +
       ' <div class="grid-stack-item-content panel panel-default widget" style="background-color: ' + backgroundColor + '; border-color: ' + frameBorderColor + ';">' +
-      '   <div class="panel-heading widget-header"><strong>' + node.type + '</strong></div>' +
-      '   <div class="panel-body" style="color: ' + labelColor + ';">' + node.name + '</div>' +
+      '   <div class="panel-heading widget-header" style="color: ' + labelColor + ';"><strong>' + node.type + '</strong></div>' +
       ' </div>' +
       '</div>';
     return template;
