f6b1b5ef4022c2697e2cb627ffaffe25fc6478d0
weko3-dev18 <weko3-hitachi-dev18@ml.itg.hitachi.co.jp>
2019-05-20 15:49:57 +0700

    Correct display year in item-create report

--- modules/weko-admin/weko_admin/static/js/weko_admin/custom_report.js
+++ modules/weko-admin/weko_admin/static/js/weko_admin/custom_report.js
@@ -168,11 +168,11 @@ class ComponentTableResult extends React.Component {
       rows.push(<tr>{cell}</tr>);
       for (var i = 0; i < data.length; i++) {
         cell = [];
-        let year = ''
+        let year = '';
         if (data[i].year) {
-          year = data[i].year
+          year = data[i].year;
         } else {
-          year.push({data[i].start_date}{" - "}{data[i].end_date})
+          year.push({data[i].start_date}{" - "}{data[i].end_date});
         }
         cell.push(<td style={this.styleTable}>{year}</td>);
         cell.push(<td style={this.styleTable}>{data[i].count}</td>);
