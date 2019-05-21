dd6b09e19fdd3562643b6ab62dedbe5abbf8788e
weko3-dev21 <weko3-hitachi-dev21@ml.itg.hitachi.co.jp>
2019-05-21 13:57:24 +0700

    Fix style of widget item

--- modules/weko-theme/weko_theme/static/js/weko_theme/widget.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/widget.js
@@ -50,18 +50,20 @@ let PageBodyGrid = function () {
     let frameBorderColor = ((node.frame_border) ? node.frame_border_color : "");
     let backgroundColor = node.background_color;
     let description = "";
+    let heightHeading = "";
     let heightDescription = "";
 
     if (node.type == "Free description" || node.type == "Notice") {
       description = node.description;
-      heightDescription = "94%";
+      heightHeading = (node.height/3) * 100;
+      heightDescription = 100 - heightHeading;
     }
 
     let template =
       '<div class="grid-stack-item">' +
       ' <div class="grid-stack-item-content panel panel-default widget" style="background-color: ' + backgroundColor + '; border-color: ' + frameBorderColor + '; overflow-y: hidden !important;">' +
-      '   <div style="height: 100%;">' +
-      '     <div class="panel-heading widget-header" style="color: ' + labelColor + ';"><strong>' + node.name + '</strong></div>' +
+      '   <div style="height: 100%;" data-gs-height="'+ node.height +'">' +
+      '     <div class="panel-heading widget-header" style="color: ' + labelColor + '; height: '+ heightHeading + ';"><strong>' + node.name + '</strong></div>' +
       '     <div class="panel-body ql-editor" style="overflow-y: auto;height: '+ heightDescription + ';">' + description + '</div>' +
       '   </div>' +
       ' </div>' +
