1692d369abfa401f1bafac02353fb6b4fcb75021
weko3-dev21 <weko3-hitachi-dev21@ml.itg.hitachi.co.jp>
2019-05-24 15:50:48 +0700

    Implement widget notice.

--- modules/weko-theme/weko_theme/static/js/weko_theme/widget.js
+++ modules/weko-theme/weko_theme/static/js/weko_theme/widget.js
@@ -64,7 +64,7 @@ let PageBodyGrid = function () {
 
     if (node.type == "Notice") {
       let rssFeedTemplate = "";
-      let rssSites = 'http://ir.acc.senshu-u.ac.jp/index.php?action=whatsnew_view_main_rss&page_id=30&block_id=54&display_number=5&_header=0';
+      let rssSites = "'http://ir.acc.senshu-u.ac.jp/index.php?action=whatsnew_view_main_rss&page_id=30&block_id=54&display_number=5&_header=0'";
       let moreDescription = "";
       let templateWriteMoreNotice = '<div id="moreDescription">' + moreDescription +'</div>';
 
