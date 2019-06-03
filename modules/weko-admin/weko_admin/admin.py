bade0113b3c734e7290e9286b1efce7678fb7bff
weko3-dev02 <weko3-hitachi-dev02@ml.itg.hitachi.co.jp>
2019-06-03 14:39:02 +0900

    merge feature/sp11-add-stats-display-setting-admin

--- modules/weko-admin/weko_admin/admin.py
+++ modules/weko-admin/weko_admin/admin.py
@@ -29,8 +29,8 @@ import zipfile
 from datetime import datetime
 from io import BytesIO, StringIO
 
-from flask import abort, current_app, flash, jsonify, make_response, redirect, \
-    request
+from flask import abort, current_app, flash, jsonify, make_response, \
+    redirect, request
 from flask_admin import BaseView, expose
 from flask_babelex import gettext as _
 from flask_login import current_user
@@ -245,9 +245,9 @@ class ReportView(BaseView):
             _('Title'), _('Registered Index Name'), _('View Count'),
             _('Non-logged-in User')],
         'file_using_per_user': [_('Mail address'),
-            _('Username'),
-            _('File download count'),
-            _('File playing count')],
+                                _('Username'),
+                                _('File download count'),
+                                _('File playing count')],
         'search_count': [_('Search Keyword'), _('Number Of Searches')]
     }
 
@@ -381,7 +381,8 @@ class ReportView(BaseView):
                                      record['login'], record['site_license'],
                                      record['admin'], record['reg']])
                 elif file_type == 'index_access':
-                    writer.writerow([record['index_name'], record['view_count']])
+                    writer.writerow(
+                        [record['index_name'], record['view_count']])
                 elif file_type == 'search_count':
                     writer.writerow([record['search_key'], record['count']])
                 elif file_type == 'detail_view':
@@ -450,6 +451,7 @@ class StatsSettingsView(BaseView):
             current_app.config["WEKO_ADMIN_STATS_SETTINGS_TEMPLATE"],
             display_stats=current_app.config["WEKO_ADMIN_DISPLAY_FILE_STATS"]
         )
+
 
 style_adminview = {
     'view_class': StyleSettingView,
