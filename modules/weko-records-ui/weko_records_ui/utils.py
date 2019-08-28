e7d989bc072ed8fdfe702920ff8016a1f5bfd560
weko3-dev02 <weko3-hitachi-dev02@ml.itg.hitachi.co.jp>
2019-08-28 18:25:52 +0900

    Let soft_delete/restore functions can accept uuid.

--- modules/weko-records-ui/weko_records_ui/utils.py
+++ modules/weko-records-ui/weko_records_ui/utils.py
@@ -165,9 +165,11 @@ def soft_delete(recid):
     try:
         pid = PersistentIdentifier.query.filter_by(
             pid_type='recid', pid_value=recid).first()
+        if not pid:
+            pid = PersistentIdentifier.query.filter_by(
+                pid_type='recid', object_uuid=recid).first()
         if pid.status == PIDStatus.DELETED:
             return
-        pid.status = PIDStatus.DELETED
         depid = PersistentIdentifier.query.filter_by(
             pid_type='depid', object_uuid=pid.object_uuid).first()
         if depid:
@@ -175,7 +177,9 @@ def soft_delete(recid):
             dep = WekoDeposit(rec.json, rec)
             dep['path'] = []
             dep.indexer.update_path(dep, update_revision=False)
-            depid.status = PIDStatus.DELETED
+        pids = PersistentIdentifier.query.filter_by(object_uuid=pid.object_uuid)
+        for p in pids:
+            p.status = PIDStatus.DELETED
         db.session.commit()
     except Exception as ex:
         db.session.rollback()
@@ -186,6 +190,9 @@ def restore(recid):
     try:
         pid = PersistentIdentifier.query.filter_by(
             pid_type='recid', pid_value=recid).first()
+        if not pid:
+            pid = PersistentIdentifier.query.filter_by(
+                pid_type='recid', object_uuid=recid).first()
         if pid.status != PIDStatus.DELETED:
             return
         pid.status = PIDStatus.REGISTERED
@@ -196,6 +203,9 @@ def restore(recid):
             rec = RecordMetadata.query.filter_by(id=pid.object_uuid).first()
             dep = WekoDeposit(rec.json, rec)
             dep.indexer.update_path(dep, update_revision=False)
+        pids = PersistentIdentifier.query.filter_by(object_uuid=pid.object_uuid)
+        for p in pids:
+            p.status = PIDStatus.REGISTERED
         db.session.commit()
     except Exception as ex:
         db.session.rollback()
