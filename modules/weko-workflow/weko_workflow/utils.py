d5831df7cc6a2afc5e328b1681c3c9f36f3d3e17
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2019-09-24 20:17:02 +0700

    Fix get_pidstore with pid_type v3

--- modules/weko-workflow/weko_workflow/utils.py
+++ modules/weko-workflow/weko_workflow/utils.py
@@ -549,7 +549,7 @@ class IdentifierHandle(object):
                         child.parent.pid_value.split('/')[1])
                     doi_pidstore = self.get_pistore(pid_type, parent.object_uuid)
 
-            if pid_type == 'doi' and len(doi_pidstore):
+            if pid_type == 'doi' and isinstance(doi_pidstore, array):
                 doi_pidstore = doi_pidstore[0]
             return doi_pidstore
 
