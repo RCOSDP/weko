291f7c9a03789546134895301c0b09c0059ac1fd
weko3-dev02 <weko3-hitachi-dev02@ml.itg.hitachi.co.jp>
2019-10-10 14:34:45 +0900

    fix pep8

--- modules/weko-index-tree/weko_index_tree/utils.py
+++ modules/weko-index-tree/weko_index_tree/utils.py
@@ -193,7 +193,7 @@ def get_user_groups():
 def check_roles(user_role, roles):
     """Check roles."""
     is_can = True
-    if type(roles) == type(""):
+    if isinstance(roles, ""):
         roles = roles.split(',')
     if not user_role[0]:
         if current_user.is_authenticated:
