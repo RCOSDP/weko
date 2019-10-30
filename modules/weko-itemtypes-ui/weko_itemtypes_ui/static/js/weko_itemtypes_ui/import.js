19daaf8bb75028eb80117c216e17d6c0915f324c
weko3-dev36 <weko3-hitachi-dev36@ml.itg.hitachi.co.jp>
2019-10-30 16:58:43 +0700

    Update messager

--- modules/weko-itemtypes-ui/weko_itemtypes_ui/static/js/weko_itemtypes_ui/import.js
+++ modules/weko-itemtypes-ui/weko_itemtypes_ui/static/js/weko_itemtypes_ui/import.js
@@ -1,34 +1,36 @@
-//const urlLoadLang = '/api/admin/load_lang';
-//const urlLoadSiteInfo = '/api/admin/get_site_info';
-//const default_favicon = 'https://community.repo.nii.ac.jp/images/common/favicon.ico'
-//const default_favicon_name = 'JAIRO Cloud icon'
-//const site_name_label = document.getElementById("site_name_label").value;
-//const favicon_label = document.getElementById("favicon_label").value;
-//const copyright_label = document.getElementById("copyright_label").value;
-//const description_label = document.getElementById("description_label").value;
-//const keyword_label = document.getElementById("keyword_label").value;
-//const selected_icon_label = document.getElementById("selected_icon_label").value;
-//const add_site_name_label = document.getElementById("add_site_name_label").value;
-//const site_name_not_set_label = document.getElementById("site_name_not_set_label").value;
-//const language_label = document.getElementById("language_label").value;
-//const save_label = document.getElementById("save_label").value;
-//const must_set_at_least_1_site_name_label = document.getElementById("must_set_at_least_1_site_name_label").value;
-//const please_input_site_infomation_for_empty_field_label = document.getElementById("please_input_site_infomation_for_empty_field_label").value;
-//const the_same_language_is_set_for_many_site_names_label = document.getElementById("the_same_language_is_set_for_many_site_names_label").value;
-//const error_when_get_languages_label = document.getElementById("error_when_get_languages_label").value;
-//const error_when_get_site_infomation_label = document.getElementById("error_when_get_site_infomation_label").value;
-//const language_not_match_label = document.getElementById("language_not_match_label").value;
-//const delete_label = document.getElementById("delete_label").value;
-//const select_icon_file_label = document.getElementById("select_icon_file_label").value;
-//const success_mess = document.getElementById("success_mess").value;
-//const lang_code_ja = 'ja'
+const import = document.getElementById("import").value;
+const list = document.getElementById("list").value;
+const import_file = document.getElementById("import_file").value;
+const import_index = document.getElementById("import_index").value;
+const work_flow = document.getElementById("work_flow").value;
+const select_file = document.getElementById("select_file").value;
+const select_index = document.getElementById("select_index").value;
+const select_work_flow = document.getElementById("select_work_flow").value;
+const selected_file_name = document.getElementById("selected_file_name").value;
+const selected_index = document.getElementById("selected_index").value;
+const selected_work_flow = document.getElementById("selected_work_flow").value;
+const index_tree = document.getElementById("index_tree").value;
+const designate_index = document.getElementById("designate_index").value;
+const work_flow_2 = document.getElementById("work_flow_2").value;
+const item_type = document.getElementById("item_type").value;
+const flow = document.getElementById("flow").value;
+const select = document.getElementById("select").value;
+const cancel = document.getElementById("cancel").value;
+
 class MainLayout extends React.Component {
 
     componentDidMount() {
       console.log("import is work")
+      console.log("cancel", cancel)
     }
 
-
+    render() {
+      return(
+        <div>
+          hello
+        <div>
+      )
+    }
 }
 
 $(function () {
