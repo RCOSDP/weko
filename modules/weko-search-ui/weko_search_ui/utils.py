547ae72ff1d5f36a38065fc5779fc7ca3d93f963
weko3-dev36 <weko3-hitachi-dev36@ml.itg.hitachi.co.jp>
2019-12-02 13:35:38 +0700

    mirror Update

--- modules/weko-search-ui/weko_search_ui/utils.py
+++ modules/weko-search-ui/weko_search_ui/utils.py
@@ -388,14 +388,13 @@ def read_stats_tsv(tsv_file_path: str) -> dict:
                     item_path,
                     data_row)
                 )
-
-                item_type_name = check_item_type[
-                    'name'] if check_item_type else ''
-                item_type_id = check_item_type[
-                    'item_type_id'] if check_item_type else ''
+                if isinstance(check_item_type, dict):
+                    item_type_name = check_item_type.get(
+                        'name')
+                    item_type_id = check_item_type.get('item_type_id')
                 tsv_item = dict(**json_data_parse, **data_parse_metadata, **{
-                    'item_type_name': item_type_name,
-                    'item_type_id': item_type_id,
+                    'item_type_name': item_type_name or '',
+                    'item_type_id': item_type_id or '',
                     '$schema': schema if schema else ''
                 })
                 tsv_data.append(tsv_item)
