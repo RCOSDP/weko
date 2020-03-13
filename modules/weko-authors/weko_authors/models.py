30728d5071ebf1a21e6fef11dc916ca13651835b
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2020-03-13 14:48:28 +0700

    Back-End: fix bug did not trim scheme

--- modules/weko-authors/weko_authors/models.py
+++ modules/weko-authors/weko_authors/models.py
@@ -147,7 +147,7 @@ class AuthorsPrefixSettings(db.Model, Timestamp):
             with db.session.begin_nested():
                 data.name = name
                 data.url = url
-                data.scheme = scheme
+                data.scheme = scheme.strip()
                 db.session.add(data)
             db.session.commit()
         except BaseException as ex:
@@ -164,7 +164,7 @@ class AuthorsPrefixSettings(db.Model, Timestamp):
                 data = cls.query.filter_by(id=id).first()
                 data.name = name
                 data.url = url
-                data.scheme = scheme
+                data.scheme = scheme.strip()
                 db.session.merge(data)
             db.session.commit()
         except BaseException as ex:
