018113e9952eff5ab14bf092fd5f0905a67f21c3
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2020-03-15 15:17:32 +0700

    Fix html text format in shib_login template

--- modules/weko-accounts/setup.py
+++ modules/weko-accounts/setup.py
@@ -99,7 +99,7 @@ setup(
         ],
         'invenio_assets.bundles': [
             'weko_accounts_embedded_ds_P1_js = weko_accounts.bundles:embedded_ds_P1_js',
-            'weko_accounts_embedded_ds_P2_js = weko_accounts.bundles:embedded_ds_P1_js',
+            'weko_accounts_embedded_ds_P2_js = weko_accounts.bundles:embedded_ds_P2_js',
             'weko_accounts_embedded_ds_multi_lang_js = weko_accounts.bundles:embedded_ds_multi_language_js',
             'weko_accounts_css = weko_accounts.bundles:embedded_wayf_custom',
             'weko_accounts_suggest_js = weko_accounts.bundles:suggest_js',
