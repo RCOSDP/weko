1ea65edb1b87b1ac656fc3bcf90ce98499db0751
hide24 <hidetoshi.yoshimoto@gmail.com>
2020-03-26 20:21:49 +0900

    update copyright.

--- modules/invenio-previewer/invenio_previewer/extensions/iiif_presentation.py
+++ modules/invenio-previewer/invenio_previewer/extensions/iiif_presentation.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of Invenio.
-# Copyright (C) 2016 CERN.
+# This file is part of WEKO3.
+# Copyright (C) 2020 National Institute of Informatics.
 #
-# Invenio is free software; you can redistribute it
+# WEKO3 is free software; you can redistribute it
 # and/or modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 2 of the
 # License, or (at your option) any later version.
