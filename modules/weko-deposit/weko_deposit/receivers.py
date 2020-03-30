d99e8afef840f9de3db03d49f6b4234420409653
weko3-dev02 <weko3-hitachi-dev02@ml.itg.hitachi.co.jp>
2020-03-30 12:26:02 +0900

    Fix _item_metadata.

--- modules/weko-deposit/weko_deposit/receivers.py
+++ modules/weko-deposit/weko_deposit/receivers.py
@@ -2,23 +2,21 @@
 #
 # """Deposit module receivers."""
 
-from __future__ import absolute_import, print_function
-from flask import current_app
 from .api import WekoDeposit
 from .pidstore import get_record_without_version
-from invenio_pidstore.models import PersistentIdentifier
-from invenio_records.models import RecordMetadata
-from weko_records.api import ItemsMetadata
 
 def append_file_content(sender, json=None, record=None, index=None, **kwargs):
     """Append file content to ES record."""
     dep = WekoDeposit.get_record(record.id)
     pid = get_record_without_version(dep.pid)
-    json['_item_metadata'] = ItemsMetadata.get_record(record.id)
-    json['_item_metadata']['pubdate'] = dep['pubdate']
+    im = dep.copy()
+    im.pop('_deposit')
+    im.pop('_buckets')
+    json['_item_metadata'] = im
+    json['_oai'] = im['_oai']
+    json['control_number'] = dep.pid.pid_value
     json['relation_version_is_last'] = True \
         if pid == get_record_without_version(pid) else False
-    json['control_number'] = dep.pid.pid_value
     files = [f for f in dep.files]
     contents = []
     for f in files:
