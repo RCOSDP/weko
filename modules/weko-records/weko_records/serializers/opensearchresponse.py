53eaa402b72c6b5b228e2f4b2baf0d8b1e054a37
weko3-dev20 <weko3-hitachi-dev20@ml.itg.hitachi.co.jp>
2020-04-07 17:25:36 +0700

    Modify license notation for Weko-Records module

--- modules/weko-records/weko_records/serializers/opensearchresponse.py
+++ modules/weko-records/weko_records/serializers/opensearchresponse.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 #
-# This file is part of Invenio.
-# Copyright (C) 2016-2018 CERN.
+# Copyright (C) 2020 National Institute of Informatics.
 #
-# Invenio is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
+# WEKO3 is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
 
 """Serialization response factories.
 
