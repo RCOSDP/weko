64192f334e7643fcee0ab69e4a163456b5746d2a
Masaharu Hayashi <masaharu.hayashi3@gmail.com>
2020-04-21 08:20:56 +0900

    add batch

--- handle/handle.sh
+++ handle/handle.sh
@@ -6,6 +6,8 @@ HANDLE_SVR="/var/handle/svr"
 # Build and configure the server
 python3 /home/handle/build.py $HANDLE_BIN $HANDLE_SVR
 
+exec "$HANDLE_BIN/hdl-genericbatch" "$HANDLE_SVR/addadmin.batch"
+
 # Start the handle server
 exec "$HANDLE_BIN/hdl-server" $HANDLE_SVR 2>&1
 
