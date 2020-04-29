d491a053baf1e29065237510ed2c520394c5ebac
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2020-04-29 15:35:31 +0700

    Fix isort

--- modules/weko-deposit/weko_deposit/api.py
+++ modules/weko-deposit/weko_deposit/api.py
@@ -57,8 +57,8 @@ from weko_user_profiles.models import UserProfile
 
 from .config import DEPOSIT_RECORDS_UI_CREATOR, MAGAZINE_INFORMATION, \
     MAGAZINE_INFORMATION_KEY
-from .pidstore import get_latest_version_id, weko_deposit_fetcher, \
-    weko_deposit_minter
+from .pidstore import get_latest_version_id, get_record_without_version, \
+    weko_deposit_fetcher, weko_deposit_minter
 from .signals import item_created
 
 PRESERVE_FIELDS = (
@@ -1328,8 +1328,6 @@ class WekoRecord(Record):
 
     def _get_pid(self, pid_type):
         """Return pid_value from persistent identifier."""
-        from .pidstore import get_record_without_version
-
         pid_without_ver = get_record_without_version(self.pid)
         if not pid_without_ver:
             return None
