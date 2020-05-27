704e40ec24ed0adbeef7bada42f6afc9f1ab79a2
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2020-05-27 17:06:55 +0700

    Fix GUI Version Controller

--- modules/weko-items-ui/weko_items_ui/static/js/weko_items_ui/version_controller.js
+++ modules/weko-items-ui/weko_items_ui/static/js/weko_items_ui/version_controller.js
@@ -1,34 +1,44 @@
+const VERSION_CONTROLLER_LABEL = document.getElementById(
+  "component-version-label"
+).value;
+const UPGRADE_RADIO_BUTTON_LABEL = document.getElementById(
+  "upgrade-radiobutton-label"
+).value;
+const EDIT_RADIO_BUTTON_LABEL = document.getElementById(
+  "edit-radiobutton-label"
+).value;
+
 class MainLayout extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+  }
 
-    }
-    setVersion(event) {
-        console.log(event.target.value);
-    }
-
-    render() {
-        const label = $("#component-version-label").val();
-        const keepOption = $("#radio-version-option-2").val();
-        const updateOption = $("#radio-version-option-1").val();
-        return (
-            <div className="row">
-                <div class="form-group">
-                    <label class="control-label col-sm-3 text-right">{label}</label>
-                    <div class="col-sm-9" onChange={this.setVersion.bind(this)}>
-                        <label><input type="radio" name="radioVersionSelect" value="update" />{updateOption}</label>
-                        <label><input type="radio" name="radioVersionSelect" value="keep" />{keepOption}</label>
-                    </div>
-                </div>
-            </div>
-
-        );
-    }
+  render() {
+    return (
+      <div className="row">
+        <div class="form-group">
+          <label class="control-label col-sm-3 text-right">
+            {VERSION_CONTROLLER_LABEL}
+          </label>
+          <div class="col-sm-9">
+            <label class="col-sm-3">
+              <input type="radio" name="radioVersionSelect" value="update" />{" "}
+              {UPGRADE_RADIO_BUTTON_LABEL}
+            </label>
+            <label class="col-sm-3">
+              <input type="radio" name="radioVersionSelect" value="keep" />{" "}
+              {EDIT_RADIO_BUTTON_LABEL}
+            </label>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 $(function () {
-    ReactDOM.render(
-        <MainLayout />,
-        document.getElementById('react-component-version')
-    )
+  ReactDOM.render(
+    <MainLayout />,
+    document.getElementById("react-component-version")
+  );
 });
