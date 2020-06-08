d48798bce70eb6d1a98a385b250940edf310ccf6
Masaharu Hayashi <masaharu.hayashi3@gmail.com>
2020-06-08 13:54:57 +0900

    modifiy scripts/provision-web.sh

--- scripts/provision-web.sh
+++ scripts/provision-web.sh
@@ -84,12 +84,7 @@ provision_web_common_ubuntu14 () {
          libxml2-dev \
          libxslt-dev \
          libzip-dev \
-         libpng-dev \
-         zlib1g-dev \ 
-         libopenjp2-7-dev \
          libjpeg-dev \
-         imagemagick \
-         libmagickwand-dev \
          nodejs \
          python-dev \
          python-pip
