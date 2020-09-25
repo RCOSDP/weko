e1f6042c8b10aaf377b99f3b49fda81658d5c099
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2020-09-25 10:20:03 +0700

    Weko#14238-Fixed Index Name in ResourceSync

--- modules/invenio-resourcesyncserver/invenio_resourcesyncserver/static/js/invenio-resourcesyncserver/change_list.js
+++ modules/invenio-resourcesyncserver/invenio_resourcesyncserver/static/js/invenio-resourcesyncserver/change_list.js
@@ -436,7 +436,7 @@ class CreateResourceComponent extends React.Component {
               <option value="" disabled></option>
               <option value="0">Root Index</option>
               {state.tree_list.map(item => {
-                return <option value={item.id}>{item.value}</option>;
+                return <option value={item.id} dangerouslySetInnerHTML={{ __html: item.value }}></option>;
               })}
             </select>
           </div>
@@ -768,7 +768,7 @@ class EditResourceComponent extends React.Component {
             >
               <option value="0">Root Index</option>
               {state.tree_list.map(item => {
-                return <option value={item.id}>{item.value}</option>;
+                return <option value={item.id} dangerouslySetInnerHTML={{ __html: item.value }}></option>;
               })}
             </select>
           </div>
