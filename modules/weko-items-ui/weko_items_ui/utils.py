310a02b310c306ba6b5cd2e00f391c45ceeaa332
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2020-11-19 17:54:50 +0700

    Weko#22522-Fixed pep

--- modules/weko-items-ui/weko_items_ui/utils.py
+++ modules/weko-items-ui/weko_items_ui/utils.py
@@ -1381,6 +1381,21 @@ def _export_item(record_id,
                  tmp_path=None,
                  records_data=None):
     """Exports files for record according to view permissions."""
+    def del_hide_sub_metadata(keys, metadata):
+        """Delete hide metadata."""
+        if isinstance(metadata, dict):
+            data = metadata.get(keys[0])
+            if data:
+                if len(keys) > 1:
+                    del_hide_sub_metadata(keys[1:], data)
+                else:
+                    del metadata[keys[0]]
+        elif isinstance(metadata, list):
+            count = len(metadata)
+            for index in range(count):
+                del_hide_sub_metadata(keys[1:] if len(
+                    keys) > 1 else keys, metadata[index])
+
     exported_item = {}
     record = WekoRecord.get_record_by_pid(record_id)
     list_item_role = {}
@@ -2329,22 +2344,6 @@ def hide_thumbnail(schema_form):
         if isinstance(data_items, list) and is_thumbnail(data_items):
             form_data['condition'] = 1
             break
-
-
-def del_hide_sub_metadata(keys, metadata):
-    """Delete hide metadata."""
-    if isinstance(metadata, dict):
-        data = metadata.get(keys[0])
-        if data:
-            if len(keys) > 1:
-                del_hide_sub_metadata(keys[1:], data)
-            else:
-                del metadata[keys[0]]
-    elif isinstance(metadata, list):
-        count = len(metadata)
-        for index in range(count):
-            del_hide_sub_metadata(keys[1:] if len(
-                keys) > 1 else keys, metadata[index])
 
 
 def get_ignore_item(_item_type_id):
