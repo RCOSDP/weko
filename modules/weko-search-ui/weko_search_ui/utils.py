9ab79c874d2c155e931d9ee6a15b3348a3b6cfce
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2020-12-01 16:30:28 +0700

    Weko#22724-Implement DefaultOrderedDict instead of DefaultDict

--- modules/weko-search-ui/weko_search_ui/utils.py
+++ modules/weko-search-ui/weko_search_ui/utils.py
@@ -30,7 +30,7 @@ import sys
 import tempfile
 import traceback
 import uuid
-from collections import defaultdict
+from collections import Callable, OrderedDict
 from datetime import datetime
 from functools import reduce
 from io import StringIO
@@ -80,6 +80,67 @@ from .query import feedback_email_search_factory, item_path_search_factory
 
 err_msg_suffix = 'Suffix of {} can only be used with half-width' \
     + ' alphanumeric characters and half-width symbols "_-.; () /".'
+
+
+class DefaultOrderedDict(OrderedDict):
+    """Default Dictionary that remembers insertion order."""
+
+    def __init__(self, default_factory=None, *a, **kw):
+        """Initialize an default ordered dictionary.
+        
+        The signature
+        is the same as regular dictionaries.  Keyword argument order
+        is preserved.
+        """
+        if (default_factory is not None and
+           not isinstance(default_factory, Callable)):
+            raise TypeError('first argument must be callable')
+        OrderedDict.__init__(self, *a, **kw)
+        self.default_factory = default_factory
+
+    def __getitem__(self, key):
+        """Modify inherited dict provides __getitem__."""
+        try:
+            return OrderedDict.__getitem__(self, key)
+        except KeyError:
+            return self.__missing__(key)
+
+    def __missing__(self, key):
+        """Needed so that self[missing_item] does not raise KeyError."""
+        if self.default_factory is None:
+            raise KeyError(key)
+        self[key] = value = self.default_factory()
+        return value
+
+    def __reduce__(self):
+        """Return state information for pickling."""
+        if self.default_factory is None:
+            args = tuple()
+        else:
+            args = self.default_factory,
+        return type(self), args, None, None, self.items()
+
+    def copy(self):
+        """Modify inherited dict provides copy.
+
+        od.copy() -> a shallow copy of od.
+        """
+        return self.__copy__()
+
+    def __copy__(self):
+        """Modify inherited dict provides __copy__."""
+        return type(self)(self.default_factory, self)
+
+    def __deepcopy__(self, memo):
+        """Modify inherited dict provides __deepcopy__."""
+        import copy
+        return type(self)(self.default_factory,
+                          copy.deepcopy(self.items()))
+
+    def __repr__(self):
+        """Return a nicely formatted representation string."""
+        return 'OrderedDefaultDict(%s, %s)' % (self.default_factory,
+                                               OrderedDict.__repr__(self))
 
 
 def get_tree_items(index_tree_id):
@@ -259,7 +320,7 @@ def define_default_dict():
     :return
        return       -- {dict}.
     """
-    return defaultdict(define_default_dict)
+    return DefaultOrderedDict(define_default_dict)
 
 
 def defaultify(d: dict) -> dict:
@@ -273,7 +334,7 @@ def defaultify(d: dict) -> dict:
     """
     if not isinstance(d, dict):
         return d
-    return defaultdict(
+    return DefaultOrderedDict(
         define_default_dict,
         {k: defaultify(v) for k, v in d.items()}
     )
