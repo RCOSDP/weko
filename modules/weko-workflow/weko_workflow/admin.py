86c40894662f4e3c6bd25e735b1e31f01561399e
weko3-dev38 <weko3-hitachi-dev38@ml.itg.hitachi.co.jp>
2020-12-18 17:46:58 +0700

    weko#22928: Fix error when id is incorrect

--- modules/weko-workflow/weko_workflow/admin.py
+++ modules/weko-workflow/weko_workflow/admin.py
@@ -21,6 +21,7 @@
 """WEKO3 module docstring."""
 
 import uuid
+import re
 
 from flask import abort, jsonify, request, url_for
 from flask_admin import BaseView, expose
@@ -69,10 +70,12 @@ class FlowSettingView(BaseView):
                 actions=None,
                 action_list=actions
             )
+        UUID_PATTERN = re.compile(r'^[\da-f]{8}-([\da-f]{4}-){3}[\da-f]{12}$',
+                                  re.IGNORECASE)
+        if not UUID_PATTERN.match(flow_id):
+            abort(404)
         workflow = Flow()
         flow = workflow.get_flow_detail(flow_id)
-        if not flow:
-            abort(400)
         return self.render(
             'weko_workflow/admin/flow_detail.html',
             flow_id=flow_id,
