cebc7e102cb730de377760086c71f8eb61a63899
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2021-01-25 14:30:30 +0700

    Weko#22260: Add relationship between shibboleth_user and role.

--- modules/weko-accounts/weko_accounts/models.py
+++ modules/weko-accounts/weko_accounts/models.py
@@ -24,6 +24,16 @@ from invenio_accounts.models import User
 from invenio_db import db
 
 
+shibuserole = db.Table(
+    'shibboleth_userrole',
+    db.Column('shib_user_id', db.Integer(), db.ForeignKey(
+        'shibboleth_user.id', name='fk_shibboleth_userrole_user_id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey(
+        'accounts_role.id', name='fk_shibboleth_userrole_role_id')),
+)
+"""Relationship between shibboleth users and roles."""
+
+
 class ShibbolethUser(db.Model):
     """Shibboleth User Model."""
 
@@ -62,6 +72,9 @@ class ShibbolethUser(db.Model):
     shib_ip_range_flag = db.Column(db.String(255), nullable=True)
     """SHIB_ATTR_SITE_USER_WITHIN_IP_RANGE_FLAG"""
 
+    shib_roles = db.relationship('Role', secondary=shibuserole,
+                            backref=db.backref('shib_users', lazy='dynamic'))
+
     @classmethod
     def create(cls, weko_user, **kwargs):
         """Create shibboleth user.
@@ -88,13 +101,3 @@ class ShibbolethUser(db.Model):
         return cls.query.filter_by(
             shib_eppn=email,
         ).one_or_none()
-
-
-ShibUserRole = db.Table(
-    'shibboleth_userrole',
-    db.Column('shib_user_id', db.Integer(), db.ForeignKey(
-        'shibboleth_user.id', name='fk_shibboleth_userrole_user_id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey(
-        'accounts_role.id', name='fk_shibboleth_userrole_role_id')),
-)
-"""Relationship between shibboleth users and roles."""
