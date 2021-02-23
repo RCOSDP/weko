46a5442d1d5066c19a869c3e4e43ee0e8fd710d3
weko3-dev41 <weko3-hitachi-dev41@ml.itg.hitachi.co.jp>
2021-02-23 15:00:34 +0700

    Weko#24265: Added class Exporter

--- modules/weko-items-ui/weko_items_ui/api.py
+++ modules/weko-items-ui/weko_items_ui/api.py
@@ -18,14 +18,26 @@
 import os
 
 import redis
-from flask import current_app, json, session, url_for
-from flask_login import login_required
+from flask import current_app, json, session, url_for, flash, redirect, request, send_file
+from flask_login import login_required, current_user
 from simplekv.memory.redisstore import RedisStore
 from weko_records.api import ItemTypes
 from weko_records.utils import find_items
 
 from .permissions import item_permission
-from .utils import is_schema_include_key
+from .utils import is_schema_include_key, _get_max_export_items, write_tsv_files, \
+    write_bibtex_files, get_ignore_item_from_mapping, hide_meta_data_for_role
+from weko_records_ui.permissions import check_file_download_permission
+import tempfile
+from datetime import date, datetime, timedelta
+import traceback
+import sys
+from flask_babelex import gettext as _
+import re
+import shutil
+import bagit
+from celery import shared_task
+from weko_deposit.api import WekoRecord
 
 
 @login_required
@@ -92,3 +104,207 @@ def item_login(item_type_id=0):
         record, json_schema, schema_form, \
         item_save_uri, files, endpoints, need_thumbnail, files_thumbnail, \
         allow_multi_thumbnail
+
+class Exporter():
+    """ Class Exporter """
+    @classmethod
+    def __init__(self):
+        """ Init Data """
+        self._include_contents = None   #Check Export File Content
+        self._export_format = None      #Check Export Format: JSON or BIBTEX
+        self._record_ids = None         #List item need to export
+        self._invalid_record_ids = None #List item not need to export
+        self._record_metadata = None    #Metadata
+        self._temp_path = None          #TempFile is Store in Server
+
+    @classmethod
+    def check_item_type_name(self, name):
+        """Check a list of allowed characters in filenames."""
+        new_name = re.sub(r'[\/:*"<>|\s]', '_', name)
+        return new_name
+
+    @shared_task
+    def export_all_items(self):
+        """Export items for Export Admin Screen."""
+
+        # self._include_contents = False
+        # self._export_format = 'JSON'
+        # self._invalid_record_ids = []
+        # self._record_ids = {}
+        # self._record_metadata = {}
+        # self._temp_path = tempfile.TemporaryDirectory()  
+    @classmethod
+    def export_select_items(self, post_data):
+        """Export items for Export Screen."""
+
+        # Get data
+        self._include_contents = True if post_data.get('export_file_contents_radio') == 'True' else False
+        self._export_format = post_data['export_format_radio']
+        self._record_ids = json.loads(post_data['record_ids'])
+        self._invalid_record_ids = json.loads(post_data['invalid_record_ids'])
+        self._invalid_record_ids = [int(i) for i in self._invalid_record_ids]
+        # Remove all invalid records
+        self._record_ids = set(self._record_ids) - set(self._invalid_record_ids)
+        self._record_metadata = json.loads(post_data['record_metadata'])
+        self._temp_path = tempfile.TemporaryDirectory()
+
+        try:
+            if len(self._record_ids ) > _get_max_export_items():
+                return abort(400)
+            elif len(self._record_ids) == 0:
+                return '', 204
+            # Get Path File to Download
+            export_path = self.export_items()
+        except Exception:
+            current_app.logger.error('-' * 60)
+            traceback.print_exc(file=sys.stdout)
+            current_app.logger.error('-' * 60)
+            flash(_('Error occurred during item export.'), 'error')
+            return redirect(url_for('weko_items_ui.export'))
+        return send_file(
+            export_path + '.zip',
+            as_attachment=True,
+            attachment_filename='export.zip'
+        )
+            
+
+    @classmethod
+    def export_items(self):
+        """Gather all the item data and export and return as a JSON or BIBTEX.
+
+        :return: JSON, BIBTEX
+        """
+        include_contents = self._include_contents
+        export_format = self._export_format
+        record_ids = self._record_ids
+        invalid_record_ids = self._invalid_record_ids
+        record_metadata = self._record_metadata
+        result = {'items': []}
+        temp_path = self._temp_path 
+        item_types_data = {}
+
+        # Set export folder
+        export_path = temp_path.name + '/' + \
+            datetime.utcnow().strftime("%Y%m%d%H%M%S")
+        # Double check for limits
+        for record_id in record_ids:
+            record_path = export_path + '/recid_' + str(record_id)
+            os.makedirs(record_path, exist_ok=True)
+            exported_item, list_item_role = self._export_item(
+                record_id,
+                export_format,
+                include_contents,
+                record_path,
+                record_metadata.get(str(record_id))
+            )
+
+            result['items'].append(exported_item)
+
+            item_type_id = exported_item.get('item_type_id')
+            item_type = ItemTypes.get_by_id(item_type_id)
+            if not item_types_data.get(item_type_id):
+                item_type_name = self.check_item_type_name(
+                    item_type.item_type_name.name)
+                item_types_data[item_type_id] = {
+                    'item_type_id': item_type_id,
+                    'name': '{}({})'.format(
+                        item_type_name,
+                        item_type_id),
+                    'root_url': request.url_root,
+                    'jsonschema': 'items/jsonschema/' + item_type_id,
+                    'keys': [],
+                    'labels': [],
+                    'recids': [],
+                    'data': {},
+                }
+            item_types_data[item_type_id]['recids'].append(record_id)
+
+        # Create export info file
+        if export_format == 'BIBTEX':
+            write_bibtex_files(item_types_data, export_path)
+        else:
+            write_tsv_files(item_types_data, export_path, list_item_role)
+
+        # Create bag
+        bagit.make_bag(export_path)
+        # Create download file
+        shutil.make_archive(export_path, 'zip', export_path)
+
+        return export_path 
+
+    @classmethod
+    def _export_item(self, record_id,
+                    export_format,
+                    include_contents,
+                    tmp_path=None,
+                    records_data=None):
+        """Exports files for record according to view permissions."""
+        def del_hide_sub_metadata(keys, metadata):
+            """Delete hide metadata."""
+            if isinstance(metadata, dict):
+                data = metadata.get(keys[0])
+                if data:
+                    if len(keys) > 1:
+                        del_hide_sub_metadata(keys[1:], data)
+                    else:
+                        del metadata[keys[0]]
+            elif isinstance(metadata, list):
+                count = len(metadata)
+                for index in range(count):
+                    del_hide_sub_metadata(keys[1:] if len(
+                        keys) > 1 else keys, metadata[index])
+
+        exported_item = {}
+        record = WekoRecord.get_record_by_pid(record_id)
+        list_item_role = {}
+        if record:
+            exported_item['record_id'] = record.id
+            exported_item['name'] = 'recid_{}'.format(record_id)
+            exported_item['files'] = []
+            exported_item['path'] = 'recid_' + str(record_id)
+            exported_item['item_type_id'] = record.get('item_type_id')
+            if not records_data:
+                records_data = record
+            if exported_item['item_type_id']:
+                list_hidden = get_ignore_item_from_mapping(
+                    exported_item['item_type_id'])
+                if records_data.get('metadata'):
+                    meta_data = records_data.get('metadata')
+                    record_role_ids = {
+                        'weko_creator_id': meta_data.get('weko_creator_id'),
+                        'weko_shared_id': meta_data.get('weko_shared_id')
+                    }
+                    list_item_role.update(
+                        {exported_item['item_type_id']: record_role_ids})
+                    if hide_meta_data_for_role(record_role_ids):
+                        for hide_key in list_hidden:
+                            if isinstance(hide_key, str) \
+                                    and meta_data.get(hide_key):
+                                del records_data['metadata'][hide_key]
+                            elif isinstance(hide_key, list):
+                                del_hide_sub_metadata(
+                                    hide_key, records_data['metadata'])
+
+            # Create metadata file.
+            with open('{}/{}_metadata.json'.format(tmp_path,
+                                                exported_item['name']),
+                    'w',
+                    encoding='utf8') as output_file:
+                json.dump(records_data, output_file, indent=2,
+                        sort_keys=True, ensure_ascii=False)
+            # First get all of the files, checking for permissions while doing so
+            if include_contents:
+                # Get files
+                for file in record.files:  # TODO: Temporary processing
+                    if check_file_download_permission(record, file.info()):
+                        if file.info().get('accessrole') != 'open_restricted':
+                            exported_item['files'].append(file.info())
+                            # TODO: Then convert the item into the desired format
+                            if file:
+                                file_buffered = file.obj.file.storage().open()
+                                temp_file = open(
+                                    tmp_path + '/' + file.obj.basename, 'wb')
+                                temp_file.write(file_buffered.read())
+                                temp_file.close()
+
+        return exported_item, list_item_role
