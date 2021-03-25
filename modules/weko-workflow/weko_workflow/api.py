e993c85a5b8beed6ef1087666bc38984c7965d3c
weko3-dev40 <weko3-hitachi-dev40@ml.itg.hitachi.co.jp>
2021-03-25 19:50:33 +0700

    weko#25204 - refactor gen activity_id when init an ativity

--- modules/weko-workflow/weko_workflow/api.py
+++ modules/weko-workflow/weko_workflow/api.py
@@ -608,11 +608,46 @@ class WorkActivity(object):
                     if flow_actions and len(flow_actions) >= 2:
                         next_action_id = flow_actions[1].action_id
 
+            # Calculate activity_id based on id
+            current_date_start = utc_now.strftime("%Y-%m-%d 00:00:00")
+            from datetime import timedelta
+            next_date_start = (utc_now + timedelta(1)).\
+                strftime("%Y-%m-%d 00:00:00")
+
+            from sqlalchemy import func
+            min_id = db.session.query(func.min(_Activity.id)).filter(
+                _Activity.created >= '{}'.format(current_date_start),
+                _Activity.created < '{}'.format(next_date_start),
+            ).scalar()
+            max_id = db.session.query(func.max(_Activity.id)).filter(
+                _Activity.created >= '{}'.format(current_date_start),
+                _Activity.created < '{}'.format(next_date_start),
+            ).scalar()
+
+            if min_id and max_id:
+                # Calculate aid
+                number = max_id - min_id + 2
+                if number > 99999:
+                    raise IndexError('The number is out of range \
+                        (maximum is 99999, current is {}'.format(number))
+            else:
+                # The default activity Id of the current day
+                number = 1
+
+            # Activity Id's format
+            activity_id_format = current_app.\
+                config['WEKO_WORKFLOW_ACTIVITY_ID_FORMAT']
+
+            # A-YYYYMMDD-NNNNN (NNNNN starts from 00001)
+            datetime_str = utc_now.strftime("%Y%m%d")
+
+            # Define activity Id of day
+            activity_id = activity_id_format.format(
+                datetime_str,
+                '{inc:05d}'.format(inc=number))
+
             db_activity = _Activity(
-                # Dummy activity ID, the real one will be updated
-                #   after this activity is created
-                activity_id='A' + str(
-                    datetime.utcnow().timestamp()).split('.')[0],
+                activity_id=activity_id,
                 item_id=item_id,
                 workflow_id=activity.get('workflow_id'),
                 flow_id=activity.get('flow_id'),
@@ -633,48 +668,13 @@ class WorkActivity(object):
             db.session.commit()
 
             try:
-                # Calculate activity_id based on id
-                current_date_start = utc_now.strftime("%Y-%m-%d 00:00:00")
-                from datetime import timedelta
-                next_date_start = (utc_now + timedelta(1)).\
-                    strftime("%Y-%m-%d 00:00:00")
-
-                from sqlalchemy import func
-                min_id = db.session.query(func.min(_Activity.id)).filter(
-                    _Activity.created >= '{}'.format(current_date_start),
-                    _Activity.created < '{}'.format(next_date_start),
-                ).scalar()
-
-                if min_id:
-                    # Calculate aid
-                    number = db_activity.id - min_id + 1
-                    if number > 99999:
-                        raise IndexError('The number is out of range \
-                            (maximum is 99999, current is {}'.format(number))
-                else:
-                    # The default activity Id of the current day
-                    number = 1
-
-                # Activity Id's format
-                activity_id_format = current_app.\
-                    config['WEKO_WORKFLOW_ACTIVITY_ID_FORMAT']
-
-                # A-YYYYMMDD-NNNNN (NNNNN starts from 00001)
-                datetime_str = utc_now.strftime("%Y%m%d")
-
-                # Define activity Id of day
-                activity_id = activity_id_format.format(
-                    datetime_str,
-                    '{inc:05d}'.format(inc=number))
-
                 # Update the activity with calculated activity_id
-                action_pid = PersistentIdentifier.create(
+                PersistentIdentifier.create(
                     pid_type='actid',
                     pid_value=str(activity_id),
                     status=PIDStatus.REGISTERED
                     # object_type='act', #Workflow Activity Object Type
                 )
-                db_activity.activity_id = action_pid.pid_value
 
                 # Add history and flow_action
                 db_history = ActivityHistory(
