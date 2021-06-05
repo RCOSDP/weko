f564cdfa02c54124072630bbf4e26a3a8a3ed832
Masaharu Hayashi <masaharu.hayashi3@gmail.com>
2021-06-05 15:40:49 +0900

    Fix DB request problem in /ping access

--- build_testenv.sh
+++ build_testenv.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+docker cp test/postgresql/del_index.sql $(docker-compose ps -q postgresql):/tmp/
+docker-compose exec postgresql psql -U invenio -d invenio -f /tmp/del_index.sql
+
+#docker cp test/postgresql/index2.sql $(docker-compose ps -q postgresql):/tmp/
+#docker-compose exec postgresql psql -U invenio -d invenio -f /tmp/index2.sql
+
 docker cp test/postgresql/index.sql $(docker-compose ps -q postgresql):/tmp/
 docker-compose exec postgresql psql -U invenio -d invenio -f /tmp/index.sql
