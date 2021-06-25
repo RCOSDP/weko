30c803c41c80f32012f613bf535d9e1562f5311c
weko3-dev23 <weko3-hitachi-dev23@ml.itg.hitachi.co.jp>
2021-06-25 15:07:23 +0700

    Weko#26601: Added condition for handle DOI granted in Approval.

--- modules/weko-workflow/weko_workflow/views.py
+++ modules/weko-workflow/weko_workflow/views.py
@@ -929,6 +929,7 @@ def next_action(activity_id='0', action_id=0):
             action_id=identifier_actionid)
 
         if not post_json.get('temporary_save') \
+            and last_idt_setting.get('action_identifier_select') \
                 and last_idt_setting.get('action_identifier_select') > 0:
             _pid = current_pid.pid_value
             if pid_without_ver:
