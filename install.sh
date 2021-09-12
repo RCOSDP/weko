a245192e803bca441578535e66a1dc17c30d66ca
Masaharu Hayashi <masaharu.hayashi3@gmail.com>
2021-09-12 16:12:30 +0900

    add test/scripts/userroles.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ docker-compose down -v
 docker-compose build
 docker-compose up -d
 docker-compose exec web ./scripts/populate-instance.sh
-docker cp scripts/demo/item_type.sql $(docker-compose ps -q postgresql):/tmp/item_type.sql
+docker cp scripts/demo/item_type2.sql $(docker-compose ps -q postgresql):/tmp/item_type.sql
 docker-compose exec postgresql psql -U invenio -d invenio -f /tmp/item_type.sql
 docker-compose exec web invenio workflow init action_status,Action
+docker cp scripts/demo/resticted_access.sql $(docker-compose ps -q postgresql):/tmp/resticted_access.sql
+docker-compose exec postgresql psql -U invenio -d invenio -f /tmp/resticted_access.sql
+
--- test/postgresql/README.md
+++ test/postgresql/README.md
@@ -1,7 +1,16 @@
 # index.sql
 
-インデックスの公開・非公開・エンバーゴ公開、
-OAI-PMHの公開・非公開の全6パターンの組合せの
+1032 インデックス
+
+6種類のインデックス、3階層 x 4
+
+- インデックス公開
+- インデックスエンバーゴ期間内
+- インデックス非公開
+- インデックス公開・OAI-PMH公開
+- インデックスエンバーゴ期間内・OAI-PMH公開
+- インデックス非公開・OAI-PMH公開
+
 
 ```
 docker-compose exec postgresql pg_dump -U invenio -c -T alembic_version -t index --column-inserts > index.sql
