fcb30b8d06574d77dc4778e9804331a14c54dfad
Masaharu Hayashi <masaharu.hayashi3@gmail.com>
2021-09-21 08:58:54 +0900

    update test data generator

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ docker-compose exec web ./scripts/populate-instance.sh
 docker cp scripts/demo/item_type2.sql $(docker-compose ps -q postgresql):/tmp/item_type.sql
 docker-compose exec postgresql psql -U invenio -d invenio -f /tmp/item_type.sql
 docker-compose exec web invenio workflow init action_status,Action
-#docker cp scripts/demo/resticted_access.sql $(docker-compose ps -q postgresql):/tmp/resticted_access.sql
-#docker-compose exec postgresql psql -U invenio -d invenio -f /tmp/resticted_access.sql
+docker cp scripts/demo/resticted_access.sql $(docker-compose ps -q postgresql):/tmp/resticted_access.sql
+docker-compose exec postgresql psql -U invenio -d invenio -f /tmp/resticted_access.sql
 
