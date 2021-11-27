07890cbfce3bcb1fe98a0f59ddf93a364b9b2028
Masaharu Hayashi <masaharu.hayashi3@gmail.com>
2021-11-28 01:51:34 +0900

    fix some issues for jgss

--- modules/weko-deposit/weko_deposit/api.py
+++ modules/weko-deposit/weko_deposit/api.py
@@ -1640,10 +1640,7 @@ class WekoRecord(Record):
             is_ok = True
         # Check super users
         else:
-            super_users = current_app.config[
-                'WEKO_PERMISSION_SUPER_ROLE_USER'] +
-            current_app.config[
-                'WEKO_PERMISSION_ROLE_COMMUNITY']
+            super_users = current_app.config['WEKO_PERMISSION_SUPER_ROLE_USER'] + current_app.config['WEKO_PERMISSION_ROLE_COMMUNITY']
             for role in list(current_user.roles or []):
                 if role.name in super_users:
                     is_ok = True
