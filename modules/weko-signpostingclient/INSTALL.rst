6ba0f0b026751e105763233742888b44e40b0385
ivis-miyachi <kyosuke.miyachi@ivis.co.jp>
2022-02-10 13:34:28 +0900

    add weko-signpostingclient

---
+++ modules/weko-signpostingclient/INSTALL.rst
@@ -0,0 +1,8 @@
+Installation
+============
+
+WEKO-Signpostingclient is on PyPI so all you need is:
+
+.. code-block:: console
+
+   $ pip install weko-signpostingclient
---
+++ modules/weko-signpostingclient/weko_signpostingclient/api.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022 National Institute of Informatics.
+#
+# WEKO-Signpostingclient is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+import requests
+import re
+
+def request_signposting(uri):
+    r = requests.head(uri)
+    link = r.headers['Link']
+    
+    data = create_data_from_signposting(link)
+    
+    return data
+
+def create_data_from_signposting(link):
+    
+    lines = link.split(',')
+    data = list()
+    for line in lines:
+        d=dict()
+        for l in line.split(';'):
+            if re.search(r'<(.*)>',l):
+                d['url'] = re.search(r'<(.*)>',l).group(1).strip()
+            else:
+                d[l.split('=')[0].strip()]=l.split('=')[1].strip(' "')
+        data.append(d)
+    return data
