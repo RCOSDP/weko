9a6dcfa143fa8d99ddd28cf02b0f8d54ba2e4b04
mhaya <mhaya@nii.ac.jp>
2022-02-20 17:41:45 +0000

    add alembic setting

---
+++ modules/invenio-resourcesyncclient/invenio_resourcesyncclient/alembic/d758395731f2_initial.py
@@ -0,0 +1,28 @@
+#
+# This file is part of Invenio.
+# Copyright (C) 2016-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""initial"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd758395731f2'
+down_revision = None
+branch_labels = ('invenio_resourcesyncclient',)
+depends_on = None
+
+
+def upgrade():
+    """Upgrade database."""
+    pass
+
+
+def downgrade():
+    """Downgrade database."""
+    pass
