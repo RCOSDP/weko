7dd4d0d1a5c7d501fa3bc7efad0cd6decb975441
ivis-miyachi <kyosuke.miyachi@ivis.co.jp>
2022-03-01 11:25:26 +0900

    code formatting

--- modules/weko-signpostingserver/weko_signpostingserver/api.py
+++ modules/weko-signpostingserver/weko_signpostingserver/api.py
@@ -5,7 +5,7 @@
 # WEKO-Signpostingserver is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
-from flask import Blueprint, Response, request, current_app,redirect
+from flask import Blueprint, Response, request, current_app, redirect
 
 from invenio_records.api import Record
 from weko_inbox_sender.utils import get_record_permalink, get_records_pid
@@ -14,41 +14,49 @@ blueprint_signposting_api = Blueprint(
     __name__
 )
 
+
 # TODO: recordが存在しない場合の処理
-@blueprint_signposting_api.route('/records/<recid>/signposting', methods = ['HEAD'])
+@blueprint_signposting_api.route('/records/<recid>/signposting',
+                                 methods=['HEAD']
+                                 )
 def requested_signposting(recid):
     resp = Response()
     host_url = outside_url(request.host_url)
     record = Record.get_record(get_records_pid(str(recid)))
     link = list()
-    
+
     permalink = outside_url(get_record_permalink(recid))
     if not permalink:
         permalink = '{hosturl}records/{recid}'.\
                     format(hosturl=host_url, recid=recid)
-    link.append('<{url}> ; rel="cite-as"'.\
-                format(url = permalink))
-    link.append('<{url}> ; rel="describedby" ; type="application/json"'.\
-                format(url = '{hosturl}records/{recid}/export/json'.\
-                        format(hosturl = host_url, recid = recid)))
-    link.append('<{url}> ; rel="describedby" ; type="application/x-bibtex"'.\
-                format(url = '{hosturl}records/{recid}/export/bibtex'.\
-                        format(hosturl = host_url, recid = recid)))
-    
-    oad = current_app.config.get('OAISERVER_METADATA_FORMATS',{})
-    
-    for _format,_object in oad.items():
-        url = '{hosturl}oai?verb=GetRecord&metadataPrefix={formats}&identifier={recid}'.\
-            format(hosturl=host_url,formats=_format,recid=record['_oai']['id'])
-        link.append('<{url}> ; rel="describedby" ; type="application/xml" ; formats={formats}'.\
-                    format(url = url,
-                           formats = _object['namespace']))
+    link.append('<{url}> ; rel="cite-as"'.
+                format(url=permalink))
+    link.append('<{url}> ; rel="describedby" ; type="application/json"'.
+                format(url='{hosturl}records/{recid}/export/json'.
+                           format(hosturl=host_url, recid=recid)))
+    link.append('<{url}> ; rel="describedby" ; type="application/x-bibtex"'.
+                format(url='{hosturl}records/{recid}/export/bibtex'.
+                           format(hosturl=host_url, recid=recid)))
+
+    oad = current_app.config.get('OAISERVER_METADATA_FORMATS', {})
+
+    for _format, _object in oad.items():
+        url = '{hosturl}oai?verb=GetRecord&metadataPrefix='\
+              '{formats}&identifier={recid}'.\
+              format(hosturl=host_url,
+                     formats=_format,
+                     recid=record['_oai']['id']
+                     )
+        link.append('<{url}> ; rel="describedby" ; '
+                    'type="application/xml" ; formats="{formats}"'.
+                    format(url=url,
+                           formats=_object['namespace']))
     resp.headers['Link'] = ','.join(link)
     return resp
 
 
 def outside_url(url):
     if 'nginx' in url:
-        return url.replace('nginx',current_app.config['WEB_HOST'])
+        return url.replace('nginx', current_app.config['WEB_HOST'])
     else:
         return url
--- modules/weko-signpostingserver/weko_signpostingserver/ext.py
+++ modules/weko-signpostingserver/weko_signpostingserver/ext.py
@@ -15,6 +15,7 @@ from flask_babelex import gettext as _
 from . import config
 from .api import blueprint_signposting_api
 
+
 class WekoSignpostingserver(object):
     """WEKO-Signpostingserver extension."""
 
@@ -30,7 +31,7 @@ class WekoSignpostingserver(object):
     def init_app(self, app):
         """Flask application initialization."""
         self.init_config(app)
-        app.register_blueprint(blueprint_signposting_api,url_prefix='/')
+        app.register_blueprint(blueprint_signposting_api, url_prefix='/')
         app.extensions['weko-signpostingserver'] = self
 
     def init_config(self, app):
