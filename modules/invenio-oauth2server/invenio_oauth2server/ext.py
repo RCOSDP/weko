5acf81d8a23f397cd4a52d1aa469786c8a069e36
YusukeIsokawa <73814248+YusukeIsokawa@users.noreply.github.com>
2022-03-09 10:37:50 +0900

    Revert "Revert "Revert "oauth"""
    This reverts commit 3d470d5381451bba4424e495e03dd3c4dd35be5b.

--- modules/invenio-oauth2server/invenio_oauth2server/ext.py
+++ modules/invenio-oauth2server/invenio_oauth2server/ext.py
@@ -39,12 +39,14 @@ class _OAuth2ServerState(object):
         oauth2.init_app(app)
 
         # Flask-OAuthlib does not support CACHE_REDIS_URL
-        if app.config['OAUTH2_CACHE_TYPE'] == 'redis' and app.config.get(
-                'CACHE_REDIS_URL'):
-            from redis import from_url as redis_from_url
+        if app.config['OAUTH2_CACHE_TYPE'] == 'redissentinel' and app.config.get(
+                'CACHE_REDIS_SENTINELS'):
+            from redis import sentinel
+            master = sentinel.Sentinel(app.config['CACHE_REDIS_SENTINELS'],decode_responses=False)
             app.config.setdefault(
                 'OAUTH2_CACHE_REDIS_HOST',
-                redis_from_url(app.config['CACHE_REDIS_URL'])
+                master.master_for(
+                app.config['CACHE_REDIS_SENTINEL_MASTER'],db=app.config['CACHE_REDIS_DB_NO'])
             )
 
         # Configures an OAuth2Provider instance to use configured caching
