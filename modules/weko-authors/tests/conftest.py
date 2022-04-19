284bfa836110d044879344eeb1ecc1205a569db0
Yuriko Oshima <yuriko.oshima@ivis.co.jp>
2022-04-19 16:57:02 +0900

    update api control tests

--- modules/weko-authors/tests/conftest.py
+++ modules/weko-authors/tests/conftest.py
@@ -27,8 +27,9 @@ import json
 import pytest
 from flask import Flask, request, url_for
 from flask_babelex import Babel
-from invenio_db import InvenioDB, db
+from invenio_db import InvenioDB, db as db_
 from invenio_accounts import InvenioAccounts
+from invenio_accounts.models import User, Role
 from invenio_accounts.testutils import create_test_user, login_user_via_session
 from invenio_access import InvenioAccess
 from invenio_access.models import ActionUsers
@@ -40,8 +41,8 @@ from sqlalchemy_utils.functions import create_database, database_exists, \
     drop_database
 
 from weko_authors.views import blueprint_api
-from weko_authors.models import Authors
 from weko_authors import WekoAuthors
+from weko_authors.models import Authors, AuthorsPrefixSettings
 from weko_search_ui import WekoSearchUI
 
 
@@ -54,7 +55,7 @@ def instance_path():
 
 
 @pytest.fixture()
-def base_app(instance_path, request):
+def base_app(instance_path):
     """Flask application fixture."""
     app_ = Flask('testapp', instance_path=instance_path)
     app_.config.update(
@@ -75,7 +76,6 @@ def base_app(instance_path, request):
     InvenioSearch(app_)
     WekoAuthors(app_)
     WekoSearchUI(app_)
-    _database_setup(app_, request)
 
     # app_.register_blueprint(blueprint)
     app_.register_blueprint(blueprint_api, url_prefix='/api/authors')
@@ -89,24 +89,15 @@ def app(base_app):
         yield base_app
 
 
-def _database_setup(app, request):
-    """Set up the database."""
-    with app.app_context():
-        if not database_exists(str(db.engine.url)):
-            create_database(str(db.engine.url))
-        db.create_all()
-
-    def teardown():
-        with app.app_context():
-            if database_exists(str(db.engine.url)):
-                drop_database(str(db.engine.url))
-            # Delete sessions in kvsession store
-            if hasattr(app, 'kvsession_store') and \
-                    isinstance(app.kvsession_store, RedisStore):
-                app.kvsession_store.redis.flushall()
-
-    request.addfinalizer(teardown)
-    return app
+@pytest.yield_fixture()
+def db(app):
+    """Database fixture."""
+    if not database_exists(str(db_.engine.url)):
+        create_database(str(db_.engine.url))
+    db_.create_all()
+    yield db_
+    db_.session.remove()
+    # drop_database(str(db_.engine.url))
 
 
 @pytest.yield_fixture()
@@ -117,22 +108,40 @@ def client(app):
 
 
 @pytest.fixture()
-def users(app):
+def users(app, db):
     """Create users."""
     ds = app.extensions['invenio-accounts'].datastore
-    user = create_test_user(email='test@test.org')
-    contributor = create_test_user(email='test2@test.org')
-    comadmin = create_test_user(email='test3@test.org')
-    repoadmin = create_test_user(email='test4@test.org')
-    sysadmin = create_test_user(email='test5@test.org')
+    user_count = User.query.filter_by(email='test@test.org').count()
+    if user_count != 1:
+        user = create_test_user(email='test@test.org')
+        contributor = create_test_user(email='test2@test.org')
+        comadmin = create_test_user(email='test3@test.org')
+        repoadmin = create_test_user(email='test4@test.org')
+        sysadmin = create_test_user(email='test5@test.org')
 
-    r1 = ds.create_role(name='System Administrator')
+    else:
+        user = User.query.filter_by(email='test@test.org').first()
+        contributor = User.query.filter_by(email='test2@test.org').first()
+        comadmin = User.query.filter_by(email='test3@test.org').first()
+        repoadmin = User.query.filter_by(email='test4@test.org').first()
+        sysadmin = User.query.filter_by(email='test5@test.org').first()
+
+    role_count = Role.query.filter_by(name='System Administrator').count()
+    if role_count != 1:
+        r1 = ds.create_role(name='System Administrator')
+        r2 = ds.create_role(name='Repository Administrator')
+        r3 = ds.create_role(name='Contributor')
+        r4 = ds.create_role(name='Community Administrator')
+
+    else:
+        r1 = Role.query.filter_by(name='System Administrator').first()
+        r2 = Role.query.filter_by(name='Repository Administrator').first()
+        r3 = Role.query.filter_by(name='Contributor').first()
+        r4 = Role.query.filter_by(name='Community Administrator').first()
+
     ds.add_role_to_user(sysadmin, r1)
-    r2 = ds.create_role(name='Repository Administrator')
     ds.add_role_to_user(repoadmin, r2)
-    r3 = ds.create_role(name='Contributor')
     ds.add_role_to_user(contributor, r3)
-    r4 = ds.create_role(name='Community Administrator')
     ds.add_role_to_user(comadmin, r4)
 
     # Assign author-access to contributor, comadmin, repoadmin, sysadmin.
@@ -147,15 +156,15 @@ def users(app):
 
     return [
         {'email': user.email, 'id': user.id,
-         'password': user.password_plaintext, 'obj': user},
+         'obj': user},
         {'email': contributor.email, 'id': contributor.id,
-         'password': contributor.password_plaintext, 'obj': contributor},
+         'obj': contributor},
         {'email': comadmin.email, 'id': comadmin.id,
-         'password': comadmin.password_plaintext, 'obj': comadmin},
+         'obj': comadmin},
         {'email': repoadmin.email, 'id': repoadmin.id,
-         'password': repoadmin.password_plaintext, 'obj': repoadmin},
+         'obj': repoadmin},
         {'email': sysadmin.email, 'id': sysadmin.id,
-         'password': sysadmin.password_plaintext, 'obj': sysadmin},
+         'obj': sysadmin},
     ]
 
 
@@ -164,10 +173,27 @@ def id_prefix(client, users):
     """Create test prefix."""
     # login for create prefix
     login_user_via_session(client=client, email=users[4]['email'])
-
     input = {'name': 'testprefix', 'scheme': 'testprefix',
              'url': 'https://testprefix/##'}
     client.put('/api/authors/add_prefix',
                data=json.dumps(input),
                content_type='application/json')
     client.get(url_for('security.logout'))
+    authors_prefix = AuthorsPrefixSettings.query.filter_by(
+                        name='testprefix').first()
+    return authors_prefix.id
+
+
+@pytest.fixture()
+def create_author(db):
+    def _create_author(data):
+        with db.session.begin_nested():
+            new_id = Authors.get_sequence(db.session)
+            data["id"] = str(new_id)
+            data["pk_id"] = str(new_id)
+            author = Authors(id=new_id, json=data)
+            db.session.add(author)
+        return new_id
+
+    # Return new author's id
+    return _create_author
