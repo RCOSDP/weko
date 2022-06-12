084663d5489cbee8e5b9a737b9785a5a7d86b783
Masaharu Hayashi <mhaya@nii.ac.jp>
2022-06-12 07:37:56 +0000

    update elasticsearchTool

--- tools/elasticsearchTool.py
+++ tools/elasticsearchTool.py
@@ -15,35 +15,57 @@ from weko_records.models import ItemMetadata, ItemReference
 from elasticsearch import Elasticsearch, helpers
 
 
-def deleteUntrackableItemsFromElasticsearch(dryrun=True):
-    """Delete untrackable items from item index in Elasticsearch
+def deleteUntrackableItems(dryrun=True):
+    """
+    deleteUntrackableItems Delete untrackable items from item index in Elasticsearch
+
+    _extended_summary_
+
+    Args:
+        dryrun (bool, optional): True is dryrun mode. Defaults to True.
     """
     start = time.time()
     es = Elasticsearch(
-        "http://"+os.environ.get('INVENIO_ELASTICSEARCH_HOST', 'localhost')+":9200")
+        "http://" + os.environ.get("INVENIO_ELASTICSEARCH_HOST", "localhost") + ":9200"
+    )
     _query = '{"query": {"match_all" : {}}}'
-    results = helpers.scan(es, index=os.environ.get('SEARCH_INDEX_PREFIX',
-                                                    'tenant1')+"-weko-item-v1.0.0", preserve_order=True, query=_query)
+    results = helpers.scan(
+        es,
+        index=os.environ.get("SEARCH_INDEX_PREFIX", "tenant1") + "-weko-item-v1.0.0",
+        preserve_order=True,
+        query=_query,
+    )
     idList = []
-    for item in results:
-        idList.append(item['_id'])
 
-    current_app.logger.info('documents: {0}'.format(len(idList)))
+    for item in results:
+        idList.append(item["_id"])
+
+    current_app.logger.info("documents: {0}".format(len(idList)))
 
     delList = []
     for item in idList:
-        pid = PersistentIdentifier.query.filter_by(
-            pid_type='parent', object_uuid=item).first()
-        if pid is not None and pid.is_deleted():
-            delList.append(item)
+        current_app.logger.debug("checking item: {0}".format(item))
+        try:
+            pid = PersistentIdentifier.query.filter_by(
+                pid_type="parent", object_uuid=str(item)
+            ).first()
+            if pid is not None and pid.is_deleted():
+                delList.append(item)
+        except ValueError as e:
+            current_app.logger.error(e)
+        except Exception as e:
+            current_app.logger.error(e)
 
     current_app.logger.info("deleting items: {0}".format(len(delList)))
 
     for item in delList:
         if not dryrun:
-            es.delete(index=os.environ.get('SEARCH_INDEX_PREFIX',
-                                           'tenant1')+"-weko-item-v1.0.0", doc_type="item-v1.0.0", id=item)
+            es.delete(
+                index=os.environ.get("SEARCH_INDEX_PREFIX", "tenant1")
+                + "-weko-item-v1.0.0",
+                doc_type="item-v1.0.0",
+                id=item,
+            )
         current_app.logger.info("delete item : {0}".format(item))
 
-    current_app.logger.info(
-        "elapsed time: {0}".format(time.time()-start))
+    current_app.logger.info("elapsed time: {0}".format(time.time() - start))
