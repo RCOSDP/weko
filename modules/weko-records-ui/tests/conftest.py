a2539ad626384dc3a208813cc5b57b8f734914fb
Masaharu Hayashi <mhaya@nii.ac.jp>
2022-07-16 17:42:14 +0000

    update test code

--- modules/weko-records-ui/tests/conftest.py
+++ modules/weko-records-ui/tests/conftest.py
@@ -20,17 +20,61 @@
 
 """Pytest configuration."""
 
+import os
 import shutil
 import tempfile
+import copy
+from unittest.mock import patch
 
 import pytest
 from flask import Flask
+from flask_menu import Menu
 from flask_babelex import Babel
+from invenio_accounts import InvenioAccounts
+from invenio_accounts.models import User, Role
+from invenio_accounts.testutils import create_test_user
+from invenio_access import InvenioAccess
+from invenio_access.models import ActionUsers
+from invenio_admin import InvenioAdmin
+from invenio_assets import InvenioAssets
+from invenio_db import InvenioDB, db as db_
+from invenio_cache import InvenioCache
+from invenio_deposit import InvenioDeposit
+from invenio_files_rest import InvenioFilesREST
+from invenio_files_rest.views import blueprint as invenio_files_rest_blueprint
+from invenio_files_rest.models import Bucket, Location, ObjectVersion
+from invenio_i18n import InvenioI18N
+from invenio_indexer import InvenioIndexer
+from invenio_jsonschemas import InvenioJSONSchemas
+from invenio_pidrelations import InvenioPIDRelations
+from invenio_pidstore import InvenioPIDStore
+from invenio_records import InvenioRecords
+from invenio_records_rest.utils import PIDConverter
+from invenio_search import InvenioSearch
+from invenio_search_ui import InvenioSearchUI
+from invenio_stats.config import SEARCH_INDEX_PREFIX as index_prefix
+from six import BytesIO
+from simplekv.memory.redisstore import RedisStore
+from sqlalchemy_utils.functions import create_database, database_exists, \
+    drop_database
+from weko_admin import WekoAdmin
+from weko_items_ui import WekoItemsUI
+from weko_records import WekoRecords
+# from weko_records_ui import WekoRecordsUI
+from weko_search_ui import WekoSearchUI
+from weko_search_ui.config import WEKO_SEARCH_MAX_RESULT
+from weko_theme import WekoTheme
+from weko_groups import WekoGroups
 
+from weko_deposit import WekoDeposit, WekoDepositREST
+from weko_deposit.api import WekoRecord
+from weko_deposit.views import blueprint
+from weko_deposit.api import WekoDeposit as aWekoDeposit
+from weko_deposit.config import WEKO_BUCKET_QUOTA_SIZE, \
+    WEKO_DEPOSIT_REST_ENDPOINTS, _PID, DEPOSIT_REST_ENDPOINTS
 
 @pytest.yield_fixture()
 def instance_path():
-    """Temporary instance path."""
     path = tempfile.mkdtemp()
     yield path
     shutil.rmtree(path)
@@ -39,12 +83,105 @@ def instance_path():
 @pytest.fixture()
 def base_app(instance_path):
     """Flask application fixture."""
+
     app_ = Flask('testapp', instance_path=instance_path)
+    app_.url_map.converters['pid'] = PIDConverter
+    # initialize InvenioDeposit first in order to detect any invalid dependency
+    WEKO_DEPOSIT_REST_ENDPOINTS = copy.deepcopy(DEPOSIT_REST_ENDPOINTS)
+    WEKO_DEPOSIT_REST_ENDPOINTS['depid']['rdc_route'] = '/deposits/redirect/<{0}:pid_value>'.format(_PID)
+    WEKO_DEPOSIT_REST_ENDPOINTS['depid']['pub_route'] = '/deposits/publish/<{0}:pid_value>'.format(_PID)
+
     app_.config.update(
-        SECRET_KEY='SECRET_KEY',
+        CELERY_ALWAYS_EAGER=True,
+        CELERY_CACHE_BACKEND='memory',
+        CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+        CELERY_RESULT_BACKEND='cache',
+        CACHE_REDIS_URL='redis://redis:6379/0',
+        CACHE_REDIS_DB=0,
+        CACHE_REDIS_HOST="redis",
+        REDIS_PORT='6379',
+        JSONSCHEMAS_URL_SCHEME='http',
+        SECRET_KEY='CHANGE_ME',
+        SECURITY_PASSWORD_SALT='CHANGE_ME_ALSO',
+        # SQLALCHEMY_DATABASE_URI=os.environ.get(
+        #     'SQLALCHEMY_DATABASE_URI',
+        #     'postgresql+psycopg2://invenio:dbpass123@postgresql:5432/invenio'),
+        SQLALCHEMY_DATABASE_URI=os.environ.get(
+            'SQLALCHEMY_DATABASE_URI', 'sqlite:///test.db'),
+        SEARCH_ELASTIC_HOSTS=os.environ.get(
+            'SEARCH_ELASTIC_HOSTS', None),
+        SQLALCHEMY_TRACK_MODIFICATIONS=True,
+        SQLALCHEMY_ECHO=False,
         TESTING=True,
+        WTF_CSRF_ENABLED=False,
+        DEPOSIT_SEARCH_API='/api/search',
+        SECURITY_PASSWORD_HASH='plaintext',
+        SECURITY_PASSWORD_SCHEMES=['plaintext'],
+        SECURITY_DEPRECATED_PASSWORD_SCHEMES=[],
+        OAUTHLIB_INSECURE_TRANSPORT=True,
+        OAUTH2_CACHE_TYPE='simple',
+        ACCOUNTS_JWT_ENABLE=False,
+        INDEXER_DEFAULT_INDEX='{}-weko-item-v1.0.0'.format(
+            index_prefix),
+        INDEXER_DEFAULT_DOCTYPE='item-v1.0.0',
+        INDEXER_DEFAULT_DOC_TYPE='item-v1.0.0',
+        INDEXER_FILE_DOC_TYPE='content',
+        WEKO_BUCKET_QUOTA_SIZE=WEKO_BUCKET_QUOTA_SIZE,
+        WEKO_MAX_FILE_SIZE=WEKO_BUCKET_QUOTA_SIZE,
+        INDEX_IMG='indextree/36466818-image.jpg',
+        WEKO_SEARCH_MAX_RESULT=WEKO_SEARCH_MAX_RESULT,
+        WEKO_DEPOSIT_REST_ENDPOINTS=WEKO_DEPOSIT_REST_ENDPOINTS,
+        WEKO_INDEX_TREE_STYLE_OPTIONS={
+            'id': 'weko',
+            'widths': ['1', '2', '3', '4', '5', '6', '7', '8',
+                       '9', '10', '11']
+        },
+        WEKO_INDEX_TREE_UPATED=True,
+        I18N_LANGUAGE=[("ja", "Japanese"), ("en", "English")],
+        SERVER_NAME = 'TEST_SERVER',
+        WEKO_PERMISSION_ROLE_USER = ['System Administrator',
+                             'Repository Administrator',
+                             'Contributor',
+                             'General',
+                             'Community Administrator'],
+
+WEKO_PERMISSION_SUPER_ROLE_USER = ['System Administrator',
+                                   'Repository Administrator']
     )
+    # with ESTestServer(timeout=30) as server:
     Babel(app_)
+    InvenioAccounts(app_)
+    InvenioAccess(app_)
+    InvenioAdmin(app_)
+    InvenioAssets(app_)
+    InvenioCache(app_)
+    InvenioDB(app_)
+    InvenioDeposit(app_)
+    InvenioFilesREST(app_)
+    InvenioJSONSchemas(app_)
+    InvenioIndexer(app_)
+    InvenioPIDStore(app_)
+    InvenioRecords(app_)
+    # client = Elasticsearch(['localhost:%s'%server.port])
+    # InvenioSearch(app_, client=client)
+    InvenioSearch(app_)
+    InvenioSearchUI(app_)
+    InvenioPIDRelations(app_)
+    InvenioI18N(app_)
+    WekoRecords(app_)
+    WekoItemsUI(app_)
+    # WekoRecordsUI(app_)
+    WekoAdmin(app_)
+    WekoSearchUI(app_)
+    WekoTheme(app_)
+    WekoGroups(app_)
+    Menu(app_)
+    # app_.register_blueprint(blueprint)
+    app_.register_blueprint(invenio_files_rest_blueprint) # invenio_files_rest
+    # rest_blueprint = create_blueprint(app_, WEKO_DEPOSIT_REST_ENDPOINTS)
+    # app_.register_blueprint(rest_blueprint)
+    WekoDeposit(app_)
+    WekoDepositREST(app_)
     return app_
 
 
@@ -53,3 +190,76 @@ def app(base_app):
     """Flask application fixture."""
     with base_app.app_context():
         yield base_app
+
+
+@pytest.yield_fixture()
+def db(app):
+    """Database fixture."""
+    if not database_exists(str(db_.engine.url)):
+        create_database(str(db_.engine.url))
+    db_.create_all()
+    yield db_
+    db_.session.remove()
+    db_.drop_all()
+    # drop_database(str(db_.engine.url))
+
+@pytest.yield_fixture()
+def client(app):
+    """Get test client."""
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture()
+def users(app, db):
+    """Create users."""
+    ds = app.extensions['invenio-accounts'].datastore
+    user_count = User.query.filter_by(email='test@test.org').count()
+    if user_count != 1:
+        user = create_test_user(email='test@test.org')
+        contributor = create_test_user(email='test2@test.org')
+        comadmin = create_test_user(email='test3@test.org')
+        repoadmin = create_test_user(email='test4@test.org')
+        sysadmin = create_test_user(email='test5@test.org')
+
+    else:
+        user = User.query.filter_by(email='test@test.org').first()
+        contributor = User.query.filter_by(email='test2@test.org').first()
+        comadmin = User.query.filter_by(email='test3@test.org').first()
+        repoadmin = User.query.filter_by(email='test4@test.org').first()
+        sysadmin = User.query.filter_by(email='test5@test.org').first()
+
+    role_count = Role.query.filter_by(name='System Administrator').count()
+    if role_count != 1:
+        r1 = ds.create_role(name='System Administrator')
+        r2 = ds.create_role(name='Repository Administrator')
+        r3 = ds.create_role(name='Contributor')
+        r4 = ds.create_role(name='Community Administrator')
+
+    else:
+        r1 = Role.query.filter_by(name='System Administrator').first()
+        r2 = Role.query.filter_by(name='Repository Administrator').first()
+        r3 = Role.query.filter_by(name='Contributor').first()
+        r4 = Role.query.filter_by(name='Community Administrator').first()
+
+    ds.add_role_to_user(sysadmin, r1)
+    ds.add_role_to_user(repoadmin, r2)
+    ds.add_role_to_user(contributor, r3)
+    ds.add_role_to_user(comadmin, r4)
+
+    # Assign access authorization
+    with db.session.begin_nested():
+        action_users = [
+            ActionUsers(action='superuser-access', user=sysadmin)
+        ]
+        db.session.add_all(action_users)
+
+    return [
+        {'email': user.email, 'id': user.id, 'obj': user},
+        {'email': contributor.email, 'id': contributor.id, 'obj': contributor},
+        {'email': comadmin.email, 'id': comadmin.id, 'obj': comadmin},
+        {'email': repoadmin.email, 'id': repoadmin.id, 'obj': repoadmin},
+        {'email': sysadmin.email, 'id': sysadmin.id, 'obj': sysadmin},
+    ]
+
+
+
--- modules/weko-records-ui/tests/test_permissions.py
+++ modules/weko-records-ui/tests/test_permissions.py
@@ -19,10 +19,14 @@ from weko_records_ui.permissions import check_created_id
 #def check_publish_status(record):
 #def check_created_id(record):
 
-def test_check_created_id(app):
+
+
+def test_check_created_id(users,app):
+
+
     record =  {'_oai': {'id': 'oai:weko3.example.org:00000001', 'sets': ['1657555088462']}, 'path': ['1657555088462'], 'owner': '1', 'recid': '1', 'title': ['a'], 'pubdate': {'attribute_name': 'PubDate', 'attribute_value': '2022-07-12'}, '_buckets': {'deposit': '35004d51-8938-4e77-87d7-0c9e176b8e7b'}, '_deposit': {'id': '1', 'pid': {'type': 'depid', 'value': '1', 'revision_id': 0}, 'owner': '1', 'owners': [1], 'status': 'published', 'created_by': 1, 'owners_ext': {'email': 'wekosoftware@nii.ac.jp', 'username': '', 'displayname': ''}}, 'item_title': 'a', 'author_link': [], 'item_type_id': '15', 'publish_date': '2022-07-12', 'publish_status': '0', 'weko_shared_id': -1, 'item_1617186331708': {'attribute_name': 'Title', 'attribute_value_mlt': [{'subitem_1551255647225': 'a', 'subitem_1551255648112': 'ja'}]}, 'item_1617258105262': {'attribute_name': 'Resource Type', 'attribute_value_mlt': [{'resourceuri': 'http://purl.org/coar/resource_type/c_5794', 'resourcetype': 'conference paper'}]}, 'relation_version_is_last': True}
     with app.test_request_context():
-        assert check_created_id(record) == True
+        assert check_created_id(record) == False
 
 #def check_usage_report_in_permission(permission):
 #def check_create_usage_report(record, file_json):
