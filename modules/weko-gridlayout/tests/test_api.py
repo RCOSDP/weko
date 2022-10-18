14f7f097be5031c7da3b1c2a24a49e3332ff505c
JericFederer <pianocoderodyssey@gmail.com>
2022-10-18 17:03:49 +0900

    20221018 test code updates

--- modules/weko-gridlayout/tests/test_api.py
+++ modules/weko-gridlayout/tests/test_api.py
@@ -142,22 +142,74 @@ class test_WidgetItems():
         db.session.commit()
 
         # Doesn't return any value
+        # ERROR ~ delete function doesn't exist in WidgetItem class
         assert not WidgetItems.delete(1)
 
 
     # def get_all_widget_items(cls):
+    def test_get_all_widget_items(i18n_app, widget_item):
+        assert WidgetItems.get_all_widget_items()
 
 
     # def validate_exist_multi_language(cls, item, data):
+    def test_validate_exist_multi_language(i18n_app):
+        item = {
+            "multiLangSetting": {"k": {"label": "label"}}
+        }
+        data = {
+            "k": {"label": "label"}
+        }
+        assert WidgetItems.validate_exist_multi_language(item, data)
+
+        item = {}
+        assert not WidgetItems.validate_exist_multi_language(item, data)
 
 
     # def is_existed(cls, widget_items, widget_item_id):
+    def test_is_existed(i18n_app, widget_item):
+        assert not WidgetItems.is_existed(widget_item, "1")
+
+        # ERROR ~ get_by_repo_and_type function doesn't exist in WidgetItem class
+        with patch("weko_gridlayout.api.WidgetItem.get_by_repo_and_type", return_value=""):
+            widget_item = {
+                "repository": "repository",
+                "widget_type": "widget_type",
+            }
+            assert WidgetItems.is_existed(widget_item, "1")
 
 
     # def get_account_role(cls):
+    def test_get_account_role(i18n_app, users):
+        assert WidgetItems.get_account_role()
+
+
+    def test_get_account_role_2(i18n_app):
+        # For exception coverage
+        assert WidgetItems.get_account_role()
 
 
     # def parse_result(cls, in_result):
+    def test_parse_result(i18n_app):
+        in_result = MagicMock()
+        in_result.id = 1
+        in_result.repository_id = 1
+        in_result.widget_type = "test"
+        in_result.label = "test"
+        in_result.language = "test"
+        in_result.browsing_role = "test"
+        in_result.edit_role = "test"
+        in_result.is_enabled = "test"
+        in_result.settings = json.dumps({
+            "label_color": "test",
+            "has_frame_border": "test",
+            "frame_border_color": "test",
+            "text_color": "test",
+            "background_color": "test",
+            "multiLangSetting": "test", 
+        })
+        
+        assert WidgetItems.parse_result(in_result)
+
 
 test = test_WidgetItems()
 
@@ -174,4 +226,16 @@ def test_update():
 def test_update_by_id():
     test.test_update_by_id()
 def test_delete(db):
-    test.test_delete(db)    
+    test.test_delete(db)
+def test_get_all_widget_items(widget_item):
+    test.test_get_all_widget_items(widget_item)
+def test_validate_exist_multi_language():
+    test.test_validate_exist_multi_language()   
+def test_is_existed(widget_item):
+    test.test_is_existed(widget_item)
+def test_get_account_role(users):
+    test.test_get_account_role(users)
+def test_get_account_role_2():
+    test.test_get_account_role_2()
+def test_parse_result():
+    test.test_parse_result()
--- modules/weko-gridlayout/tests/test_utils.py
+++ modules/weko-gridlayout/tests/test_utils.py
@@ -7,8 +7,174 @@
 
 """Module tests."""
 import pytest
+from mock import patch, MagicMock, Mock
 from datetime import datetime
-from weko_gridlayout.utils import convert_data_to_edit_pack, main_design_has_main_widget
+from weko_gridlayout.utils import (
+    get_widget_type_list,
+    delete_item_in_preview_widget_item,
+    convert_popular_data,
+    update_general_item,
+    update_menu_item,
+    update_access_counter_item,
+    update_new_arrivals_item,
+    get_default_language,
+    get_unregister_language,
+    get_register_language,
+    get_system_language,
+    build_data,
+    _escape_html_multi_lang_setting,
+    build_data_setting,
+    _build_access_counter_setting_data,
+    _build_new_arrivals_setting_data,
+    _build_notice_setting_data,
+    _build_header_setting_data,
+    build_multi_lang_data,
+    convert_widget_data_to_dict,
+    convert_widget_multi_lang_to_dict,
+    convert_data_to_design_pack,
+    convert_data_to_edit_pack
+)
+
+
+
+# def get_widget_type_list():
+def test_get_widget_type_list(i18n_app, db_register):
+    assert get_widget_type_list()
+
+
+# def delete_item_in_preview_widget_item(data_id, json_data):
+def test_delete_item_in_preview_widget_item(i18n_app):
+    data_id = {
+        "label": "test",
+        "widget_type": "test"
+    }
+    json_data = [{
+        "name": "test",
+        "type": "test"
+    }]
+    assert delete_item_in_preview_widget_item(data_id, json_data)
+
+
+# def convert_popular_data(source_data, des_data):
+def test_convert_popular_data(i18n_app):
+    source_data = {
+        "background_color": "test",
+        "label_enable": "test",
+        "theme": "test",
+        "frame_border_color": "test",
+        "border_style": "test",
+        "label_text_color": "test",
+        "label_color": "test",
+    }
+    des_data = {}
+    # Doesn't return any value
+    assert not convert_popular_data(source_data, des_data)
+
+
+# def update_general_item(item, data_result):
+def test_update_general_item(i18n_app):
+    WEKO_GRIDLAYOUT_ACCESS_COUNTER_TYPE = "Access counter"
+    WEKO_GRIDLAYOUT_NEW_ARRIVALS_TYPE = "New arrivals"
+    WEKO_GRIDLAYOUT_MENU_WIDGET_TYPE = 'Menu'
+    WEKO_GRIDLAYOUT_HEADER_WIDGET_TYPE = 'Header'
+    item = {
+        "name": "test",
+        "type": "test",
+        "multiLangSetting": "test",
+    }
+    data_result = {
+        "label": "test",
+        "widget_type": "test",
+        "multiLangSetting": "test",
+        "settings": {},
+        "widget_type": WEKO_GRIDLAYOUT_ACCESS_COUNTER_TYPE
+    }
+    
+    # Doesn't return any value
+    with patch("weko_gridlayout.utils.convert_popular_data", return_value=""):
+        with patch("weko_gridlayout.utils.update_access_counter_item", return_value=""):
+            assert not update_general_item(item, data_result)
+        with patch("weko_gridlayout.utils.update_new_arrivals_item", return_value=""):
+            data_result["widget_type"] = WEKO_GRIDLAYOUT_NEW_ARRIVALS_TYPE
+            assert not update_general_item(item, data_result)
+        with patch("weko_gridlayout.utils.update_menu_item", return_value=""):
+            data_result["widget_type"] = WEKO_GRIDLAYOUT_MENU_WIDGET_TYPE
+            assert not update_general_item(item, data_result)
+        with patch("weko_gridlayout.utils._build_header_setting_data", return_value=""):
+            data_result["widget_type"] = WEKO_GRIDLAYOUT_HEADER_WIDGET_TYPE
+            assert not update_general_item(item, data_result)
+
+
+# def update_menu_item(item, data_result):
+def test_update_menu_item(i18n_app):
+    item = MagicMock()
+    data_result = MagicMock()
+
+    # Doesn't return any value
+    assert not update_menu_item(item, data_result)
+
+
+# def update_access_counter_item(item, data_result):
+def test_update_access_counter_item(i18n_app):
+    item = MagicMock()
+    data_result = MagicMock()
+
+    # Doesn't return any value
+    assert not update_access_counter_item(item, data_result)
+
+
+# def update_new_arrivals_item(item, data_result):
+def test_update_new_arrivals_item(i18n_app):
+    item = MagicMock()
+    data_result = MagicMock()
+
+    # Doesn't return any value
+    assert not update_new_arrivals_item(item, data_result)
+
+
+# def get_default_language():
+def test_get_default_language(i18n_app, admin_lang_settings):
+    assert get_default_language()
+
+
+# def get_unregister_language():
+def test_get_unregister_language(i18n_app, admin_lang_settings):
+    assert get_unregister_language()
+
+
+# def get_register_language():
+def test_get_register_language(i18n_app, admin_lang_settings):
+    assert get_register_language()
+
+
+# def get_system_language():
+def test_get_system_language(i18n_app, admin_lang_settings):
+    assert get_system_language()
+def test_get_system_language_2(i18n_app):
+    # For exception coverage
+    assert get_system_language()
+
+
+# def build_data(data):
+def test_build_data(i18n_app):
+    data = MagicMock()
+    with patch("weko_gridlayout.utils.build_data_setting", return_value=""):
+        assert build_data(data)
+
+
+
+# def _escape_html_multi_lang_setting(multi_lang_setting: dict):
+# def build_data_setting(data):
+# def _build_access_counter_setting_data(result, setting):
+# def _build_new_arrivals_setting_data(result, setting):
+# def _build_notice_setting_data(result, setting):
+# def _build_header_setting_data(result, setting):
+# def build_multi_lang_data(widget_id, multi_lang_json):
+# def convert_widget_data_to_dict(widget_data):
+# def convert_widget_multi_lang_to_dict(multi_lang_data):
+# def convert_data_to_design_pack(widget_data, list_multi_lang_data):
+# def convert_data_to_edit_pack(data):
+
 
 @pytest.mark.parametrize("data, result",[({}, None),
     ({
@@ -177,39 +343,15 @@ def test_convert_data_to_edit_pack(data, result):
     assert res == result
 
 
-# .tox/c1/bin/pytest --cov=weko_gridlayout tests/test_utils.py::test_filter_condition -v --cov-branch --cov-report=term -s --basetemp=/code/modules/weko-gridlayout/.tox/c1/tmp -vv
-
-# def get_widget_type_list():
-# def delete_item_in_preview_widget_item(data_id, json_data):
-# def convert_popular_data(source_data, des_data):
-# def update_general_item(item, data_result):
-# def update_menu_item(item, data_result):
-# def update_access_counter_item(item, data_result):
-# def update_new_arrivals_item(item, data_result):
-# def get_default_language():
-# def get_unregister_language():
-# def get_register_language():
-# def get_system_language():
-# def build_data(data):
-# def _escape_html_multi_lang_setting(multi_lang_setting: dict):
-# def build_data_setting(data):
-# def _build_access_counter_setting_data(result, setting):
-# def _build_new_arrivals_setting_data(result, setting):
-# def _build_notice_setting_data(result, setting):
-# def _build_header_setting_data(result, setting):
-# def build_multi_lang_data(widget_id, multi_lang_json):
-# def convert_widget_data_to_dict(widget_data):
-# def convert_widget_multi_lang_to_dict(multi_lang_data):
-# def convert_data_to_design_pack(widget_data, list_multi_lang_data):
-# def convert_data_to_edit_pack(data):
 # def build_rss_xml(data=None, index_id=0, page=1, count=20, term=0, lang=''):
 # def find_rss_value(data, keyword):
 # def get_rss_data_source(source, keyword):
 # def get_elasticsearch_result_by_date(start_date, end_date):
 # def validate_main_widget_insertion(repository_id, new_settings, page_id=0):
 # def get_widget_design_page_with_main(repository_id):
-# def main_design_has_main_widget(repository_id):
 
+
+# def main_design_has_main_widget(repository_id):
 def test_main_design_has_main_widget(db_register):
     assert main_design_has_main_widget('Root Index')==True
     assert main_design_has_main_widget('test')==False
@@ -217,19 +359,7 @@ def test_main_design_has_main_widget(db_register):
 
 
 # def has_main_contents_widget(settings):
-
-#def test_has_main_contents_widget(db_register):
-
-
-
 # def get_widget_design_setting(repository_id, current_language, page_id=None):
-#     def validate_response():
-#     def get_widget_response(_page_id):
 # def compress_widget_response(response):
 # def delete_widget_cache(repository_id, page_id=None):
-#     def __init__(self):
-#     def initialize_widget_bucket(self):
-#     def __validate(self, file_stream, file_name, community_id="0", file_size=0):
-#     def save_file(self, file_stream, file_name: str, mimetype: str,
-#     def get_file(self, file_name, community_id=0):
 # def validate_upload_file(community_id: str):
