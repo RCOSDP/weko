30d6a4a82e8c421e6843a3a089d17c76ff736306
Masaharu Hayashi <mhaya@nii.ac.jp>
2022-12-20 11:51:39 +0000

    Fix

--- tools/update/addjpcoar_v1_mapping.py
+++ tools/update/addjpcoar_v1_mapping.py
@@ -1,4 +1,6 @@
 from weko_records.models import ItemTypeMapping
+from sqlalchemy.sql import func
+
 from invenio_db import db
 import pickle
 
@@ -6,12 +8,14 @@ def main():
     with db.session.begin_nested():
         res = db.session.query(ItemTypeMapping).all()
         for item_type in res:
-            mapping = pickle.loads(pickle.dumps(item_type.mapping,-1))
-            for key in mapping.keys():
-                prop=mapping[key]
-                if 'jpcoar_mapping' in prop:
+            try:
+                mapping = pickle.loads(pickle.dumps(item_type.mapping,-1))
+                for key in mapping.keys():
+                    prop=mapping[key]
                     prop['jpcoar_v1_mapping']=pickle.loads(pickle.dumps(prop['jpcoar_mapping'],-1))
-            item_type.mapping=mapping
+                item_type.mapping = mapping
+            except sqlalchemy.orm.exc.StaleDataError:
+                print(item_type.id)
     db.session.commit()
 
 if __name__ == '__main__':
