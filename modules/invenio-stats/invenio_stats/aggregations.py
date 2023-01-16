b1a031e2decda8190a47dcfbad36361cea8806f4
Masaharu Hayashi <mhaya@nii.ac.jp>
2023-01-16 14:18:58 +0000

    Fix issues35222

--- modules/invenio-stats/invenio_stats/aggregations.py
+++ modules/invenio-stats/invenio_stats/aggregations.py
@@ -272,21 +272,25 @@ class StatAggregator(object):
 
     def _get_oldest_event_timestamp(self):
         """Search for the oldest event timestamp."""
-        # Retrieve the oldest event in order to start aggregation
-        # from there
-        query_events = Search(
-            using=self.client,
-            index=self.event_index
-        )[0:1].sort(
-            {'timestamp': {'order': 'asc'}}
-        )
-        result = query_events.execute()
+        try:
+            # Retrieve the oldest event in order to start aggregation
+            # from there
+            query_events = Search(
+                using=self.client,
+                index=self.event_index
+            )[0:1].sort(
+                {'timestamp': {'order': 'asc'}}
+            )
+            result = query_events.execute()
 
-        # There might not be any events yet if the first event have been
-        # indexed but the indices have not been refreshed yet.
-        if len(result) == 0:
+            # There might not be any events yet if the first event have been
+            # indexed but the indices have not been refreshed yet.
+            if len(result) == 0:
+                return None
+            return parser.parse(result[0]['timestamp'])
+        except es_exceptions.RequestError as e:
+            # Returns None if the index does not exist
             return None
-        return parser.parse(result[0]['timestamp'])
 
     def agg_iter(self, lower_limit=None, upper_limit=None, manual=False):
         """Aggregate and return dictionary to be indexed in ES."""
