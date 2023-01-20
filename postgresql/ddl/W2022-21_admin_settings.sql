fe4bfaaa1a39b8461d0a93bb30d47a87c1e17ac7
YusukeIsokawa <73814248+YusukeIsokawa@users.noreply.github.com>
2023-01-20 13:50:24 +0900

    marge ddl

--- postgresql/ddl/W2022-21_admin_settings.sql
+++
@@ -1,3 +0,0 @@
-insert into public.admin_settings(id,name,settings) values 
- (5,'elastic_reindex_settings','{"has_errored": false}')
-;
--- postgresql/ddl/w2022-21-update_files_location.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE public.files_location ADD COLUMN s3_endpoint_url varchar(128);
-ALTER TABLE public.files_location ADD COLUMN s3_send_file_directly boolean NOT NULL DEFAULT true;
