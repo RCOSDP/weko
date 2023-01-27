0c0934c14274c607c450db98ecba59dfad4036e2
JericFederer <pianocoderodyssey@gmail.com>
2023-01-27 16:56:00 +0900

    test code updates 20230127

--- modules/weko-records-ui/tests/test_fd.py
+++ modules/weko-records-ui/tests/test_fd.py
@@ -25,6 +25,19 @@ def test_weko_view_method(app,records,itemtypes,users):
             with patch("flask.templating._render", return_value=""):
                 assert weko_view_method(recid,record,WEKO_RECORDS_UI_DETAIL_TEMPLATE)==""
 
+            data1 = MagicMock()
+            def dumps():
+                return "test/test/test/"
+            data1.dumps = dumps
+            app.config.WEKO_SEARCH_UI_BASE_PAGE_TEMPLATE = 'weko_records_ui/admin/pdfcoverpage.html'
+
+            with patch("weko_records_ui.fd.FilesMetadata.get_records", return_value=[data1]):
+                try:
+                    assert weko_view_method(recid,record,WEKO_RECORDS_UI_DETAIL_TEMPLATE)==""
+                except:
+                    pass
+
+
 # def prepare_response(pid_value, fd=True):
 # .tox/c1/bin/pytest --cov=weko_records_ui tests/test_fd.py::test_prepare_response -vv -s --cov-branch --cov-report=term --basetemp=/code/modules/weko-records-ui/.tox/c1/tmp
 def test_prepare_response(app,client,records,itemtypes,users):
@@ -73,6 +86,35 @@ def test_file_ui(app,records,itemtypes,users):
             with pytest.raises(NotFound):
                 res = file_ui(recid,record,_record_file_factory= lambda x,y,z: None )
     
+    data1 = MagicMock()
+    def cannot():
+        return False
+    data1.can = cannot
+    data2 = MagicMock()
+    def is_authenticated_func():
+        return False
+    data2.is_authenticated = is_authenticated_func
+    data3 = MagicMock()
+    def can():
+        return True
+    data3.can = can
+    data3.obj = 1
+
+    with app.test_request_context():
+    #     with patch("weko_records_ui.fd.file_permission_factory", return_value=data3):
+    #         with patch("weko_records_ui.fd.record_file_factory", return_value=data3):
+    #             # Exception coverage
+    #             try:
+    #                 file_ui(pid=1, record=2)
+    #             except:
+    #                 pass
+
+        with patch("weko_records_ui.fd.file_permission_factory", return_value=data1):
+            # abort(403) coverage
+            try:
+                file_ui(data2, data3)
+            except:
+                pass
 
 
 # def _download_file(file_obj, is_preview, lang, obj, pid, record):
@@ -88,6 +130,9 @@ def test__download_file(app,records,itemtypes,users):
             res = _download_file(fileobj,True,"en",obj,recid,record)
             assert res.status == '200 OK'
 
+        fileobj.mimetype = ["pdf", "msword"]
+        with patch("weko_records_ui.fd.check_original_pdf_download_permission", return_value="test"):
+            _download_file(fileobj,True,"en",obj,recid,record)
 
 # def add_signals_info(record, obj):
 # .tox/c1/bin/pytest --cov=weko_records_ui tests/test_fd.py::test_add_signals_info -vv -s --cov-branch --cov-report=term --basetemp=/code/modules/weko-records-ui/.tox/c1/tmp
@@ -101,6 +146,27 @@ def test_add_signals_info(app,records,itemtypes,users):
         with patch("flask_login.utils._get_user", return_value=users[1]["obj"]):
             add_signals_info(record,obj)
             assert obj.item_title=='ja_conference paperITEM00000009(public_open_access_open_access_simple)'
+
+        data1 = MagicMock()
+        def all_func():
+            return []
+        data1.roles = []
+        data1.all = all_func
+        data1.id = 1
+        data2 = MagicMock()
+        data2.id = 2
+
+        with patch("weko_records_ui.fd.Group.query_by_user", return_value=data1):
+            with patch("flask_login.utils._get_user", return_value=data1):
+                add_signals_info(record,obj)
+            
+            data1.roles = [data1, data2]
+
+            with patch("flask_login.utils._get_user", return_value=data1):
+                add_signals_info(record,obj)
+
+                with patch("weko_records_ui.fd.is_billing_item", return_value=True):
+                    add_signals_info(record,obj)
 
 
 # def file_download_onetime(pid, record, _record_file_factory=None, **kwargs):
@@ -116,4 +182,13 @@ def test_file_download_onetime(app, records, itemtypes, users, db_fileonetimedow
                 with patch("weko_records_ui.fd.get_onetime_download", return_value=db_fileonetimedownload):
                     _rv = (True, "")
                     with patch("weko_records_ui.fd.validate_onetime_download_token", return_value=_rv):
+                        assert file_download_onetime(recid,record,record_file_factory)==""
+
+                        with patch("weko_records_ui.fd.parse_one_time_download_token", return_value=(True, [1])):
+                            assert file_download_onetime(recid,record,record_file_factory)==""
+                        
+                    with patch("weko_records_ui.fd.validate_onetime_download_token", return_value=(False, [1])):
+                        assert file_download_onetime(recid,record,record_file_factory)==""
+
+                    with patch("weko_records_ui.fd.record_file_factory", return_value=False):
                         assert file_download_onetime(recid,record,record_file_factory)==""
