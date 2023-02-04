1883b513b9c4f8b381a09cdaf27b69dd8c7cbac2
ivis-miyachi <kyosuke.miyachi@ivis.co.jp>
2023-02-04 15:08:21 +0900

    fix tests

--- modules/weko-search-ui/tests/test_query.py
+++ modules/weko-search-ui/tests/test_query.py
@@ -43,11 +43,11 @@ class MockSearchPerm:
 def test_get_permission_filter(i18n_app, users, client_request_args, indices):
     with patch("flask_login.utils._get_user", return_value=users[3]['obj']):
         res = get_permission_filter(33)
-        assert res==([Match(publish_status='0'), Range(publish_date={'lte': 'now/d'}), Terms(path=[]), Bool(must=[Match(publish_status='0'), Match(relation_version_is_last='true')])], ['1'])
+        assert res==([Match(publish_status='0'), Range(publish_date={'lte': 'now/d','time_zone':'UTC'}), Terms(path=['33']), Bool(must=[Match(publish_status='0'), Match(relation_version_is_last='true')])], ['33','33/44'])
         mock_searchperm = MagicMock(side_effect=MockSearchPerm)
         with patch('weko_search_ui.query.search_permission', mock_searchperm):
             res = get_permission_filter()
-            assert res==([Bool(must=[Terms(path=['1'])], should=[Match(weko_creator_id='5'), Match(weko_shared_id='5'), Bool(must=[Match(publish_status='0'), Range(publish_date={'lte': 'now/d'})])]), Bool(must=[Match(relation_version_is_last='true')])], ['1'])
+            assert res==([Bool(must=[Terms(path=['33','44'])], should=[Match(weko_creator_id='5'), Match(weko_shared_id='5'), Bool(must=[Match(publish_status='0'), Range(publish_date={'lte': 'now/d','time_zone':'UTC'})])]), Bool(must=[Match(relation_version_is_last='true')])], ['33','33/44'])
 
 
 # def default_search_factory(self, search, query_parser=None, search_type=None):
@@ -66,46 +66,44 @@ def test_default_search_factory(app, users, communities):
         'fd_attr': 'Accepted',
         'text1': 'test_text'
     }
-    search = RecordsSearch()
-    app.config['WEKO_SEARCH_KEYWORDS_DICT'] = WEKO_SEARCH_KEYWORDS_DICT
-    app.config['WEKO_ADMIN_MANAGEMENT_OPTIONS'] = WEKO_ADMIN_MANAGEMENT_OPTIONS
-    with app.test_request_context(headers=[('Accept-Language','en')], data=_data):
-        app.extensions['invenio-oauth2server'] = 1
-        app.extensions['invenio-queues'] = 1
-        with patch("flask_login.utils._get_user", return_value=users[3]['obj']):
+    with app.test_client() as client:
+        login_user_via_session(client, email=users[3]["email"])
+        search = RecordsSearch()
+        app.config['WEKO_SEARCH_KEYWORDS_DICT'] = WEKO_SEARCH_KEYWORDS_DICT
+        app.config['WEKO_ADMIN_MANAGEMENT_OPTIONS'] = WEKO_ADMIN_MANAGEMENT_OPTIONS
+        with app.test_request_context(headers=[('Accept-Language','en')], data=_data):
+            app.extensions['invenio-oauth2server'] = 1
+            app.extensions['invenio-queues'] = 1
             mock_searchperm = MagicMock(side_effect=MockSearchPerm)
             with patch('weko_search_ui.query.search_permission', mock_searchperm):
                 _rv = (search, MultiDict([]))
                 with patch('invenio_records_rest.facets.default_facets_factory', return_value=_rv):
                     res = default_search_factory(self=None, search=search)
                     assert res
-    _data['community'] = 'comm1'
-    with app.test_request_context(headers=[('Accept-Language','en')], data=_data):
-        app.extensions['invenio-oauth2server'] = 1
-        app.extensions['invenio-queues'] = 1
-        with patch("flask_login.utils._get_user", return_value=users[3]['obj']):
+        _data['community'] = 'comm1'
+        with app.test_request_context(headers=[('Accept-Language','en')], data=_data):
+            app.extensions['invenio-oauth2server'] = 1
+            app.extensions['invenio-queues'] = 1
             mock_searchperm = MagicMock(side_effect=MockSearchPerm)
             with patch('weko_search_ui.query.search_permission', mock_searchperm):
                 _rv = (search, MultiDict([]))
                 with patch('invenio_records_rest.facets.default_facets_factory', return_value=_rv):
                     res = default_search_factory(self=None, search=search)
                     assert res
-    _data['search_type'] = '0'
-    with app.test_request_context(headers=[('Accept-Language','en')], data=_data):
-        app.extensions['invenio-oauth2server'] = 1
-        app.extensions['invenio-queues'] = 1
-        with patch("flask_login.utils._get_user", return_value=users[3]['obj']):
+        _data['search_type'] = '0'
+        with app.test_request_context(headers=[('Accept-Language','en')], data=_data):
+            app.extensions['invenio-oauth2server'] = 1
+            app.extensions['invenio-queues'] = 1
             mock_searchperm = MagicMock(side_effect=MockSearchPerm)
             with patch('weko_search_ui.query.search_permission', mock_searchperm):
                 _rv = (search, MultiDict([]))
                 with patch('invenio_records_rest.facets.default_facets_factory', return_value=_rv):
                     res = default_search_factory(self=None, search=search)
                     assert res
-    _data['community'] = None
-    with app.test_request_context(headers=[('Accept-Language','en')], data=_data):
-        app.extensions['invenio-oauth2server'] = 1
-        app.extensions['invenio-queues'] = 1
-        with patch("flask_login.utils._get_user", return_value=users[3]['obj']):
+        _data['community'] = None
+        with app.test_request_context(headers=[('Accept-Language','en')], data=_data):
+            app.extensions['invenio-oauth2server'] = 1
+            app.extensions['invenio-queues'] = 1
             mock_searchperm = MagicMock(side_effect=MockSearchPerm)
             with patch('weko_search_ui.query.search_permission', mock_searchperm):
                 _rv = (search, MultiDict([]))
@@ -143,8 +141,11 @@ def test_check_admin_user(i18n_app, users):
 
 # def opensearch_factory(self, search, query_parser=None):
 # .tox/c1/bin/pytest --cov=weko_search_ui tests/test_query.py::test_opensearch_factory -vv -s --cov-branch --cov-report=xml --basetemp=/code/modules/weko-search-ui/.tox/c1/tmp
-def test_opensearch_factory(i18n_app, users, indices):
+def test_opensearch_factory(i18n_app, users, indices, mocker):
     search = RecordsSearch()
+    mocker.patch("weko_search_ui.query.search_permission",side_effect=MockSearchPerm)
+    mocker.patch("weko_search_ui.permissions.search_permission",side_effect=MockSearchPerm)
+
     with patch("flask_login.utils._get_user", return_value=users[3]['obj']):
         res = opensearch_factory(self=None, search=search)
         assert res
