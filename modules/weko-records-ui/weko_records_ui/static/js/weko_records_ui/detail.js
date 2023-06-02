516d7f7723d5d18d9085f4a8a039729b031e3e62
ayumi.jin <ayumi.jin@ivis.co.jp>
2023-06-02 11:36:41 +0900

    利用規約が閉じない問題 追加

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
@@ -94,29 +94,32 @@ require([
     if (typeof communityId !== 'undefined' && communityId !== "") {
       post_uri = post_uri + "?community=" + communityId;
     }
+    var deferred = new $.Deferred();
     $.ajax({
       url: post_uri,
       method: 'POST',
       contentType: 'application/json',
       data: JSON.stringify(post_data),
-      success: function (data) {
-        if (0 === data.code) {
-          let activity_url = data.data.redirect.split('/').slice(-1)[0];
-          let activity_id = activity_url.split('?')[0];
-          init_permission(recordId, fileName, activity_id);
-          document.location.href = data.data.redirect;
-        } else if(1 === data.code && data.data.is_download){
-          const url = new URL(data.data.redirect , document.location.origin);
-          url.searchParams.append('terms_of_use_only',true);
-          document.location.href = url;
-        } else {
-          alert(data.msg);
-        }
-      },
-      error: function (jqXHE, status) {
+    }).done(function (data) {
+      if (0 === data.code) {
+        let activity_url = data.data.redirect.split('/').slice(-1)[0];
+        let activity_id = activity_url.split('?')[0];
+        init_permission(recordId, fileName, activity_id);
+        document.location.href = data.data.redirect;
+      } else if(1 === data.code && data.data.is_download){
+        const url = new URL(data.data.redirect , document.location.origin);
+        url.searchParams.append('terms_of_use_only',true);
+        document.location.href = url;
+      } else {
+        alert(data.msg);
       }
-    });
-  }
+    }).fail(function (jqXHE, status) {
+      console.log('fail:{}', jqXHE.message);
+    }).always(function() {
+      deferred.resolve();
+    })
+    return deferred;
+  };
 
   function init_permission(record_id, file_name, activity_id) {
     let init_permission_uri = '/records/permission/';
