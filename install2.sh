1dbb81141091adc32cc1f31b3b0c458ea0639f35
Masaharu Hayashi <mhaya@nii.ac.jp>
2023-08-16 22:51:34 +0900

    small fix

--- install2.sh
+++ install2.sh
@@ -8,7 +8,7 @@ done
 DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker-compose -f docker-compose2.yml build --no-cache --force-rm
 
 # Initialize resources
-docker-compose run --rm web ./scripts/populate-instance.sh
+docker-compose -f docker-compose2.yml run --rm web ./scripts/populate-instance.sh
 docker cp scripts/demo/item_type3.sql $(docker-compose -f docker-compose2.yml ps -q postgresql):/tmp/item_type.sql
 docker-compose -f docker-compose2.yml exec postgresql psql -U invenio -d invenio -f /tmp/item_type.sql
 docker-compose -f docker-compose2.yml run --rm web invenio workflow init action_status,Action
