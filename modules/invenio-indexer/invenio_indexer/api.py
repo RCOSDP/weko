a25515936551680ea40c129bb912e0da15599d5f
Masaharu Hayashi <mhaya@nii.ac.jp>
2023-09-23 12:10:00 +0900

    fix body size error

--- modules/invenio-indexer/invenio_indexer/api.py
+++ modules/invenio-indexer/invenio_indexer/api.py
@@ -25,7 +25,7 @@ from kombu import Producer as KombuProducer
 from kombu.compat import Consumer
 from sqlalchemy.orm.exc import NoResultFound
 from elasticsearch.helpers import BulkIndexError
-from elasticsearch.exceptions import ConnectionTimeout
+from elasticsearch.exceptions import ConnectionTimeout,ConnectionError
 
 from .proxies import current_record_to_index
 from .signals import before_record_index
@@ -205,12 +205,7 @@ class RecordIndexer(object):
                     exchange=self.mq_exchange.name,
                     routing_key=self.mq_routing_key,
                 )
-                
-                
                 es_bulk_kwargs = es_bulk_kwargs or {}
-                
-                
-                
                 with consumer:
                     try:
                         _success,_fail  = bulk(
@@ -249,6 +244,24 @@ class RecordIndexer(object):
                             fail = fail + len(be2.errors)
                             for error in be2.errors:
                                 click.secho("{}, {}".format(error['index']['_id'],error['index']['error']['type']),fg='red')   
+                    except ConnectionError as ce:
+                        with conn.channel() as chan:
+                            name, af_queues_cnt, consumers = chan.queue_declare(queue=current_app.config['INDEXER_MQ_ROUTING_KEY'], passive=True)
+                            current_app.logger.debug("name:{}, queues:{}, consumers:{}".format(name, af_queues_cnt, consumers))
+                            success = success + (b4_queues_cnt-af_queues_cnt-self.count)
+                        error_ids = []
+                        error_ids.append(self.latest_item_id)
+                        _success,_fail = bulk(
+                                self.client,
+                                self._actionsiter2(error_ids),
+                                stats_only=True,
+                                request_timeout=req_timeout,
+                                #raise_on_error=False,
+                                # raise_on_exception=True,
+                                **es_bulk_kwargs
+                        )
+                        success = success + _success
+                        fail = fail + _fail
                     except ConnectionTimeout as ce:
                         click.secho("Error: {}".format(ce),fg='red')
                         click.secho("INDEXER_BULK_REQUEST_TIMEOUT: {} sec".format(req_timeout),fg='red')
@@ -369,8 +382,8 @@ class RecordIndexer(object):
         body = self._prepare_record(record, index, doc_type, arguments)
         if deleteFile:
             if 'content' in body:
-                for f in body['content']:
-                    f['file'] = ""
+                for i in range(len(body['content'])):
+                    body['content'][i]['file'] = ""
         
         action = {
             '_op_type': 'index',
