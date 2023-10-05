52ea74b4b32a27fcb0354b46de9633eb3defd54c
itski-Tanaka <itsuki.takahashi@ivis.co.jp>
2023-10-05 13:38:17 +0900

    W2023-22-5.1

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/detail.js
@@ -267,14 +267,4 @@ require([
       });
     }
   });
-  
-  document.getElementById('print-btn').addEventListener('click', () => {
-    const iframe = document.createElement('iframe');
-    iframe.srcdoc = "<!DOCTYPE html>"
-    document.body.appendChild(iframe);
-    iframe.onload = () => {
-      iframe.contentDocument.body.appendChild(document.getElementById('terms').cloneNode(true));
-      iframe.contentWindow.print();
-      }
-  })
 });
