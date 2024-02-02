33dbbea3cfc126a1f274927bac4ab191d1d8a1f7
MakotoASAOKA <asaoka@nii.ac.jp>
2024-02-02 04:29:36 +0000

    2024/3/26アップデート用ブランチ

--- modules/weko-records-ui/weko_records_ui/config.py
+++ modules/weko-records-ui/weko_records_ui/config.py
@@ -601,7 +601,7 @@ WEKO_RECORDS_UI_DISPLAY_ONLINE_ANALYSIS_FLG = False
 WEKO_RECORDS_UI_ONLINE_ANALYSIS_URL = 'https://binder.cs.rcos.nii.ac.jp/v2/weko3/'
 """ URL for online analysis button. """
 
-WEKO_RECORDS_UI_DISPLAY_SHARE_BOX_FLG = True
+WEKO_RECORDS_UI_DISPLAY_SHARE_BOX_FLG = False
 """ Display Share box on item detail. """
 
 WEKO_RECORDS_UI_DISPLAY_VERSION_BOX_FLG = True
