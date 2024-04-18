aaeab0c422ecf1577e1ca10e197226089cc5a635
Masaharu Hayashi <mhaya@nii.ac.jp>
2024-04-18 15:06:11 +0900

    add mapping tool

---
+++ tools/update/addjpcoar_v2_mapping.py
@@ -0,0 +1,25 @@
+from weko_records.models import ItemTypeMapping
+from weko_records.api import Mapping
+from sqlalchemy.sql import func
+from invenio_db import db
+import pickle
+
+def main():
+    try:
+        res = db.session.query(ItemTypeMapping.id).all()
+        for _id in list(res):
+            with db.session.begin_nested():
+                item_type = ItemTypeMapping.query.filter(ItemTypeMapping.id==_id).first()
+                mapping =  pickle.loads(pickle.dumps(item_type.mapping,-1))
+                for key in list(mapping.keys()):
+                    if 'jpcoar_v1_mapping' in mapping[key]:
+                        mapping[key]['jpcoar_mapping']=mapping[key]['jpcoar_v1_mapping']
+                item_type.mapping = mapping
+        db.session.commit()
+    except Exception as ex:
+        print(ex)
+        db.session.rollback()
+
+if __name__ == '__main__':
+    """Main context."""
+    main()
