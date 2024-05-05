08970cea1f6b33a2e2f07a18e52e30fa63bcb733
Masaharu Hayashi <mhaya@nii.ac.jp>
2024-05-05 10:39:17 +0900

    fix tool

--- tools/add_root_file_id.py
+++ tools/add_root_file_id.py
@@ -14,7 +14,7 @@ es = Elasticsearch(
 def add_root_file_id(index):
     errors = []
     updated = []
-    # _query = '{"query":{"bool":{"should":[{"bool":{"must_not":{"exists":{"field":"root_file_id"}}}},{"term":{"root_file_id":""}}]}}}'
+    # root_file_id が null または "" となっている文書を探す
     _query = '{"query":{"bool":{"should":[{"bool":{"must_not":{"exists":{"field":"root_file_id"}}}},{"bool":{"must":[{"exists":{"field":"root_file_id"}}],"must_not":[{"wildcard":{"root_file_id":"*"}}]}}]}}}'
     results = helpers.scan(
             es,
@@ -31,17 +31,26 @@ def add_root_file_id(index):
         _body = {"root_file_id": source['file_id']}
         with db.session.begin_nested():
             file = None
+            # file_idが存在する場合はそこからroot_file_idの取得を試みる
             if "file_id" in source and source['file_id'] is not None and source['file_id'] is not "":
                 file = ObjectVersion.query.filter_by(file_id=source["file_id"]).order_by(ObjectVersion.updated.desc()).first() 
+            # file_idがなく、file_keyが存在する場合はそこからroot_file_idの取得を試みる
             elif "file_key" in source and source['file_key'] is not None and source['file_key'] is not "":
                 file = ObjectVersion.query.filter_by(key=source["file_key"],bucket_id=source["bucket_id"]).order_by(ObjectVersion.updated.desc()).first() 
+            # file_idもfile_keyも存在ない場合はbucket_idからroot_file_idの取得を試みる
             else:
                 file = ObjectVersion.query.filter_by(bucket_id=source["bucket_id"]).order_by(ObjectVersion.updated.desc()).first() 
-            if file:
+            
+            # fileオブジェクトが取得でき、root_file_idが取得できた場合
+            if file and file.root_file_id is not None:
                 _body = {"file_keys":file.key,"root_file_id":file.root_file_id,"file_id":file.file_id}
                 _bulk.append({'_op_type': 'update',"_index":_index,"_type":_type,"_id":id,"doc":_body,"doc_as_upsert" : True})
                 updated.append(id)
-            elif _body is not None:
+            elif file and file.file_id is not None:
+                _body = {"file_keys":file.key,"root_file_id":file.file_id,"file_id":file.file_id}
+                _bulk.append({'_op_type': 'update',"_index":_index,"_type":_type,"_id":id,"doc":_body,"doc_as_upsert" : True})
+                updated.append(id)
+            elif _body is not None and _body['root_file_id'] is not None:
                 _bulk.append({'_op_type': 'update',"_index":_index,"_type":_type,"_id":id,"doc":_body,"doc_as_upsert" : True})
                 updated.append(id)
             else:
