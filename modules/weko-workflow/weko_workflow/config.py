596eb83bbbaa8503910fea91256761b2e65f1ca9
ivis-kondo <hiroaki.kondo@ivis.co.jp>
2024-07-02 14:52:04 +0900

    fix query and merge bugs

--- modules/weko-workflow/weko_workflow/config.py
+++ modules/weko-workflow/weko_workflow/config.py
@@ -428,8 +428,8 @@ WEKO_WORKFLOW_APPROVE_OUTPUT_REGISTRATION = ""
 WEKO_WORKFLOW_ACCESS_ACTIVITY_URL = "1"
 """Template access the URL"""
 
-WEKO_WORKFLOW_USAGE_REPORT_ACTIVITY_URL = "6"
-"""Template access the URL"""
+WEKO_WORKFLOW_USAGE_REPORT_REMINDER_MAIL_TEMPLATE_ID = "6"
+"""Usage Repost reminder mail template id"""
 
 WEKO_WORKFLOW_DATE_FORMAT = "%Y-%m-%d"
 """Date format string."""
