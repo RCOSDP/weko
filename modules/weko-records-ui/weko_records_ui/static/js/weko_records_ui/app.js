7e067a168d9312d2c1937aa456654ffd68efa311
kanon-yoneji <kanon.yoneji@ivis.co.jp>
2024-07-25 17:46:58 +0900

    fix highlight display

--- modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/app.js
+++ modules/weko-records-ui/weko_records_ui/static/js/weko_records_ui/app.js
@@ -328,11 +328,6 @@ $(function() {
     handleConfirmButton();
     handleChargeBillingFile();
 });
-if ($('#highlight').val() == 'True') {
-    priceDisplay = document.getElementById('price_display');
-    priceDisplay.style.backgroundColor ='#ADD8E6';
-    priceDisplay.style.fontWeight = 'bolder'; 
-}
 
 function handleDownloadBillingFile(isVersionTable) {
     if (isVersionTable === undefined) {
