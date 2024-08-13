9b65ee05f5c26690d3eb765b5bde4e91f6e8a194
ivis-akaike <nozomu.akaike@ivis.co.jp>
2024-08-13 18:02:34 +0900

    weko_index_treeの対応

---
+++ modules/weko-index-tree/weko_index_tree/webpack.py
@@ -0,0 +1,18 @@
+from invenio_assets.webpack import WebpackThemeBundle
+
+weko_record_ui = WebpackThemeBundle(
+    __name__,
+    "assets",
+    default="bootstrap3",
+    themes={
+        "bootstrap3": dict(
+            entry={
+                "index_tree_css_style": "css/weko_index_tree/styles.bundle.css",
+                "index_tree_js_treeview": "js/weko_index_tree/js_treeview.js",
+                "index_tree_js_js": "js/weko_index_tree/app.js",
+            },
+            dependencies={
+            }
+        )
+    }
+)
