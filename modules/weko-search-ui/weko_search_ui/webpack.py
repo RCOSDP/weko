e2c5f5d5f0d9a1cbe7be6debbdde5b0aa3611ca7
ivis-akaike <nozomu.akaike@ivis.co.jp>
2024-08-16 14:20:44 +0900

    コーディング規約に合わせて修正(空白削除)

--- modules/weko-search-ui/weko_search_ui/webpack.py
+++ modules/weko-search-ui/weko_search_ui/webpack.py
@@ -4,20 +4,20 @@ weko_search_ui = WebpackThemeBundle(
     __name__,
     "assets",
     default="bootstrap3",
-    themes = {
+    themes={
         "bootstrap3": dict(
-            entry = {
-                "theme-weko-search-ui": "css/weko_search_ui/dot_leaders.css",
-                "weko-search-ui": "js/weko_search_ui/app.js",
-                "import": "js/weko_search_ui/import.js",
-                "export": "js/weko_search_ui/export.js",
-                "moment": "js/weko_search_ui/moment.min.js",
-                "theme-import": "css/weko_search_ui/import.less",
-                "theme-export": "css/weko_search_ui/export.less",
-                "facet-search":"js/weko_search_ui/facet_search_bundle.js",
-                "theme-facet-search":"css/weko_search_ui/facet_search_bundle.less"
+            entry={
+                'theme-weko-search-ui': 'css/weko_search_ui/dot_leaders.css',
+                'weko-search-ui': 'js/weko_search_ui/app.js',
+                'import': 'js/weko_search_ui/import.js',
+                'export': 'js/weko_search_ui/export.js',
+                'moment': 'js/weko_search_ui/moment.min.js',
+                'theme-import': 'css/weko_search_ui/import.less',
+                'theme-export': 'css/weko_search_ui/export.less',
+                'facet-search': 'js/weko_search_ui/facet_search_bundle.js',
+                'theme-facet-search': 'css/weko_search_ui/facet_search_bundle.less'
             },
-            dependencies = {
+            dependencies={
                 "bootstrap-datepicker": "~1.7.1"
             }
         )
