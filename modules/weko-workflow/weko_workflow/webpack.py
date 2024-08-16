f05ab60ce5e253608d2de70cc8276ade90f4c1e6
ivis-akaike <nozomu.akaike@ivis.co.jp>
2024-08-16 17:11:34 +0900

    コーディング規約に合わせて修正(不要な空白を削除)

--- modules/weko-workflow/weko_workflow/webpack.py
+++ modules/weko-workflow/weko_workflow/webpack.py
@@ -4,20 +4,20 @@ weko_workflow = WebpackThemeBundle(
     __name__,
     "assets",
     default="bootstrap3",
-    themes = {
+    themes={
         "bootstrap3": dict(
-            entry = {
+            entry={
                 'workflow-detail': 'js/weko_workflow/workflow_detail.js',
                 'workflow-item-link': 'js/weko_workflow/workflow_item_link.js',
                 'activity-list': 'js/weko_workflow/activity_list.js',
                 'bootstrap-datepicker': 'js/weko_workflow/bootstrap-datepicker.min.js',
-                'iframe-pop':'js/weko_workflow/iframe_pop.js',
-                'workflow-oa-policy':'js/weko_workflow/workflow_oa_policy.js',
-                'workflow-identifier-grant':'js/weko_workflow/workflow_identifier_grant.js',
-                'quit-confirmation':'js/weko_workflow/quit_confirmation.js',
-                'lock-activity':'js/weko_workflow/lock_activity.js',
-                'theme-flow-detail':'js/weko_workflow/admin/flow_detail.js',
-                'theme-workflow':'css/weko_workflow/style.css',
+                'iframe-pop': 'js/weko_workflow/iframe_pop.js',
+                'workflow-oa-policy': 'js/weko_workflow/workflow_oa_policy.js',
+                'workflow-identifier-grant': 'js/weko_workflow/workflow_identifier_grant.js',
+                'quit-confirmation': 'js/weko_workflow/quit_confirmation.js',
+                'lock-activity': 'js/weko_workflow/lock_activity.js',
+                'theme-flow-detail': 'js/weko_workflow/admin/flow_detail.js',
+                'theme-workflow': 'css/weko_workflow/style.css',
                 'theme-datepicker': 'css/weko_workflow/bootstrap-datepicker3.standalone.min.css',
             },
             dependencies={
