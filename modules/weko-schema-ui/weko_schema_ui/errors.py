2e2786a32532492e52200bd1f8db039cf6dc2fc6
tomohiro.kuroda <tomohiro.kuroda@ivis.co.jp>
2024-08-19 13:12:15 +0900

    add error

--- modules/weko-schema-ui/weko_schema_ui/errors.py
+++ modules/weko-schema-ui/weko_schema_ui/errors.py
@@ -13,34 +13,41 @@ class WekoSchemaError(Exception):
         if ex is not None:
             self.exception = ex
         if msg is None:
-            msg = "Some error has occurred in weko_schema_ui"
+            msg = "Some error has occurred in weko_schema_ui."
         super().__init__(msg, *args)
 
 
 class WekoSchemaSettingError(WekoSchemaError):
     def __init__(self, ex=None, msg=None, *args):
         if msg is None:
-            msg = "Some setting error has occurred in weko_schema_ui"
+            msg = "Some setting error has occurred in weko_schema_ui."
         super().__init__(ex, msg, *args)
 
 
 class WekoSchemaConversionError(WekoSchemaError):
     def __init__(self, ex=None, msg=None, *args):
         if msg is None:
-            msg = "Some schema conversion error has occurred in weko_schema_ui"
+            msg = "Some schema conversion error has occurred in weko_schema_ui."
         super().__init__(ex, msg, *args)
 
 
 class WekoOAISchemaError(WekoSchemaError):
     def __init__(self, ex=None, msg=None, *args):
         if msg is None:
-            msg = "Some OAI schema error has occurred in weko_schema_ui"
+            msg = "Some OAI schema error has occurred in weko_schema_ui."
+        super().__init__(ex, msg, *args)
+
+
+class WekoItemtypeSchemaTreeError(WekoSchemaError):
+    def __init__(self, ex=None, msg=None, *args):
+        if not msg:
+            msg = "Some itemtype schema error has occurred in weko_schema_ui."
         super().__init__(ex, msg, *args)
 
 
 class WekoSchemaTreeError(WekoSchemaError):
     def __init__(self, ex=None, msg=None, *args):
         if msg is None:
-            msg = "Some schema tree error has occurred in weko_schema_ui"
+            msg = "Some schema tree error has occurred in weko_schema_ui."
         super().__init__(ex, msg, *args)
 
