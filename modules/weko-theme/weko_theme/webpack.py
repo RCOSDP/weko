eb7ea5e8c47419cfbd34f2e790d55c827c5fb497
ivis-akaike <nozomu.akaike@ivis.co.jp>
2024-08-20 10:53:19 +0900

    WekoTheme修正

--- modules/weko-theme/weko_theme/webpack.py
+++ modules/weko-theme/weko_theme/webpack.py
@@ -4,14 +4,14 @@ weko_theme = WebpackThemeBundle(
     __name__,
     "assets",
     default="bootstrap3",
-    themes = {
+    themes={
         "bootstrap3": dict(
-            entry = {
-                'theme-scss-css-bootstrap':'css/weko_theme/styles.scss',
-                'theme-css-styling':'css/weko_theme/styling.css',
-                'theme-css-weko-theme-widget':'css/weko_theme/weko_theme_widget.css',
-                'theme-scss-weko-theme':'css/weko_theme/theme.scss',
-                'theme-js-weko-theme-tree-view':'js/weko_theme/weko_theme_tree_view.js',
+            entry={
+                'theme-scss-css-bootstrap': 'css/weko_theme/styles.scss',
+                'theme-scss-weko-theme': 'css/weko_theme/theme.scss',
+                'theme-css-buttons': 'css/weko_theme/styling.css',
+                'theme-css-widget': 'css/weko_theme/weko_theme_widget.css',
+                'theme-js-weko-theme-tree-view': 'js/weko_theme/weko_theme_tree_view.js',
                 'theme-js-base': 'js/weko_theme/base.js',
                 'theme-js-top-page': 'js/weko_theme/top_page.js',
                 'theme-js-search-detail': 'js/weko_theme/search_detail.js',
