5a2fb29c91d71cd42887a341b1bf15924a1a1ab0
tomohiro.kuroda <tomohiro.kuroda@ivis.co.jp>
2024-09-05 13:42:58 +0900

    add TestGetRecordIdentifier

--- modules/weko-deposit/tests/test_pidstore.py
+++ modules/weko-deposit/tests/test_pidstore.py
@@ -1,9 +1,14 @@
 
+import unittest
+from unittest.mock import patch
 import uuid
+from sqlalchemy.exc import SQLAlchemyError
 
 from invenio_pidrelations.models import PIDRelation
 from invenio_pidstore.models import PersistentIdentifier,RecordIdentifier,PIDStatus
+
 from weko_deposit.pidstore import weko_deposit_minter,weko_deposit_fetcher,get_latest_version_id,get_record_identifier,get_record_identifier,get_record_without_version
+from weko_deposit.logger import weko_logger
 
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
 
@@ -19,7 +24,7 @@ def test_weko_deposit_minter(app,db):
     }
     result = weko_deposit_minter(rec_id,data)
     assert result.object_uuid == rec_id
-    
+
     # exist recid, recid is int
     rec_id = uuid.uuid4()
     data = {
@@ -28,7 +33,7 @@ def test_weko_deposit_minter(app,db):
     }
     result = weko_deposit_minter(rec_id,data,recid=2)
     assert result.object_uuid == rec_id
-    
+
     # exist recid, recid is not int
     rec_id = uuid.uuid4()
     data = {
@@ -37,7 +42,7 @@ def test_weko_deposit_minter(app,db):
     }
     result = weko_deposit_minter(rec_id,data,recid="3")
     assert result.object_uuid == rec_id
-    
+
 # def weko_deposit_fetcher(record_uuid, data):
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py::test_weko_deposit_fetcher -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
 def test_weko_deposit_fetcher(app):
@@ -63,7 +68,7 @@ def test_weko_deposit_fetcher(app):
     assert result.provider == None
     assert result.pid_type == "depid"
     assert result.pid_value == "1"
-    
+
     record_uuid = uuid.uuid4()
     data = {
       "recid": "1",
@@ -84,10 +89,10 @@ def test_get_latest_version_id(app,db,records):
     pids.append(PersistentIdentifier.create('recid', "1.2",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
     db.session.add_all(pids)
     db.session.commit()
-    
+
     result = get_latest_version_id("1")
     assert result == 3
-    
+
     result = get_latest_version_id("1000")
     assert result == 1
 
@@ -98,10 +103,66 @@ def test_get_record_identifier(app,db,es_records):
     RecordIdentifier.next()
     result = get_record_identifier("1")
     assert result.recid == 1
-    
+
     result = get_record_identifier("not int")
     assert result == None
 
+# def get_record_identifier(recid):
+# .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py::TestGetRecordIdentifier -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
+class TestGetRecordIdentifier(unittest.TestCase):
+    @patch('weko_deposit.logger.weko_logger')
+    @patch('invenio_pidstore.models.RecordIdentifier.query')
+    def test_get_record_identifier_success(self, mock_query, mock_logger):
+        recid = "1"
+        record_id = RecordIdentifier(recid=1)
+        mock_query.filter_by.return_value.one_or_none.return_value = record_id
+        mock_logger.return_value = None
+
+        result = get_record_identifier(recid)
+
+        mock_query.filter_by.assert_called_once_with(recid=1)
+        mock_logger.assert_called_once_with(key='WEKO_COMMON_RETURN_VALUE', value=record_id)
+        self.assertEqual(result.recid, 1)
+
+    @patch('weko_deposit.logger.weko_logger')
+    @patch('invenio_pidstore.models.RecordIdentifier.query')
+    def test_get_record_identifier_not_found(self, mock_query, mock_logger):
+        recid = "2"
+        mock_query.filter_by.return_value.one_or_none.return_value = None
+        mock_logger.return_value = None
+
+        result = get_record_identifier(recid)
+
+        mock_query.filter_by.assert_called_once_with(recid=2)
+        mock_logger.assert_called_once_with(key='WEKO_COMMON_RETURN_VALUE', value=None)
+        self.assertIsNone(result)
+
+    @patch('weko_deposit.logger.weko_logger')
+    @patch('invenio_pidstore.models.RecordIdentifier.query')
+    def test_get_record_identifier_not_int(self, mock_query, mock_logger):
+        recid = "not int"
+        mock_query.filter_by.return_value.one_or_none.return_value = None
+        mock_logger.return_value = None
+
+        result = get_record_identifier(recid)
+
+        self.assertEqual(mock_query.filter_by.call_count, 0)
+        mock_logger.assert_called_once_with(key='WEKO_COMMON_ERROR_UNEXPECTED')
+        self.assertEqual(result.recid, 1)
+
+    @patch('weko_deposit.logger.weko_logger')
+    @patch('invenio_pidstore.models.RecordIdentifier.query')
+    def test_get_record_identifier_sqlalchemyerror(self, mock_query, mock_logger):
+        recid = "1"
+        ex = SQLAlchemyError('test error')
+        mock_query.filter_by.side_effect = ex
+        mock_logger.return_value = None
+
+        result = get_record_identifier(recid)
+
+        mock_query.filter_by.assert_called_once_with(recid=1)
+        mock_logger.assert_called_once_with(key='WEKO_COMMON_DB_SOME_ERROR', ex=ex)
+        self.assertIsNone(result)
 
 # def get_record_without_version(pid):
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py::test_get_record_without_version -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
@@ -115,12 +176,12 @@ def test_get_record_without_version(app, db):
     db.session.add(rel2)
     other_obj = PersistentIdentifier.create('recid', "2",object_type='rec', object_uuid=uuid.uuid4(),status=PIDStatus.REGISTERED)
     db.session.commit()
-    
+
     # not exist parent_relations
     result = get_record_without_version(other_obj)
     assert result == None
-    
+
     result = get_record_without_version(obj_c1)
     assert result == obj_without_ver
-    
-    
+
+
