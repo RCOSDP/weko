1f2bfab60733b0e49d2012543db0e1a723cb5582
asuka.hirabayashi <asuka.hirabayashi@ivis.co.jp>
2024-09-06 09:00:25 +0900

    update test_pidstore.py

--- modules/weko-deposit/tests/test_pidstore.py
+++ modules/weko-deposit/tests/test_pidstore.py
@@ -1,5 +1,5 @@
 
-import unittest
+from unittest import mock
 from unittest.mock import patch
 import uuid
 
@@ -9,7 +9,6 @@ from invenio_pidrelations.models import PIDRelation
 from invenio_pidstore.models import PersistentIdentifier,RecordIdentifier,PIDStatus
 
 from weko_deposit.pidstore import weko_deposit_minter,weko_deposit_fetcher,get_latest_version_id,get_record_identifier,get_record_identifier,get_record_without_version
-from weko_deposit.logger import weko_logger
 
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
 
@@ -17,68 +16,94 @@ from weko_deposit.logger import weko_logger
 # def weko_deposit_minter(record_uuid, data, recid=None):
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py::test_weko_deposit_minter -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
 def test_weko_deposit_minter(app,db):
-    # not recid
-    rec_id = uuid.uuid4()
-    data = {
-      "recid": "1",
-      "title": ["test item1"],
-    }
-    result = weko_deposit_minter(rec_id,data)
-    assert result.object_uuid == rec_id
 
-    # exist recid, recid is int
-    rec_id = uuid.uuid4()
-    data = {
-      "recid": "2",
-      "title": ["test item2"],
-    }
-    result = weko_deposit_minter(rec_id,data,recid=2)
-    assert result.object_uuid == rec_id
+    with patch('weko_deposit.pidstore.weko_logger') as mock_logger:
 
-    # exist recid, recid is not int
-    rec_id = uuid.uuid4()
-    data = {
-      "recid": "3",
-      "title": ["test item3"],
-    }
-    result = weko_deposit_minter(rec_id,data,recid="3")
-    assert result.object_uuid == rec_id
+      # not recid
+      rec_id = uuid.uuid4()
+      data = {
+        "recid": "1",
+        "title": ["test item1"],
+      }
+      result = weko_deposit_minter(rec_id,data)
+      assert result.object_uuid == rec_id
+
+      assert mock_logger.call_count == 2
+      mock_logger.assert_any_call(key='WEKO_COMMON_IF_ENTER', branch='recid is None')
+      mock_logger.assert_any_call(key='WEKO_COMMON_RETURN_VALUE', value=mock.ANY)
+      mock_logger.reset_mock()
+
+      # exist recid, recid is int
+      rec_id = uuid.uuid4()
+      data = {
+        "recid": "2",
+        "title": ["test item2"],
+      }
+      result = weko_deposit_minter(rec_id,data,recid=2)
+      assert result.object_uuid == rec_id
+
+      assert mock_logger.call_count == 3
+      mock_logger.assert_any_call(key='WEKO_COMMON_IF_ENTER', branch='recid is not None')
+      mock_logger.assert_any_call(key='WEKO_COMMON_IF_ENTER', branch='recid is int')
+      mock_logger.assert_any_call(key='WEKO_COMMON_RETURN_VALUE', value=mock.ANY)
+      mock_logger.reset_mock()
+
+      # exist recid, recid is not int
+      rec_id = uuid.uuid4()
+      data = {
+        "recid": "3",
+        "title": ["test item3"],
+      }
+      result = weko_deposit_minter(rec_id,data,recid="3")
+      assert result.object_uuid == rec_id
+
+      assert mock_logger.call_count == 2
+      mock_logger.assert_any_call(key='WEKO_COMMON_IF_ENTER', branch='recid is not None')
+      mock_logger.assert_any_call(key='WEKO_COMMON_RETURN_VALUE', value=mock.ANY)
 
 # def weko_deposit_fetcher(record_uuid, data):
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py::test_weko_deposit_fetcher -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
 def test_weko_deposit_fetcher(app):
-    record_uuid = uuid.uuid4()
-    data = {
-      "recid": "1",
-      "title": ["test item1"],
-      "_deposit": {
-        "id": "1",
-        "pid": { "type": "depid", "value": "1", "revision_id": 0 },
-        "owner": "1",
-        "owners": [1],
-        "status": "published",
-        "created_by": 1,
-        "owners_ext": {
-          "email": "wekosoftware@nii.ac.jp",
-          "username": "",
-          "displayname": ""
+
+    with patch('weko_deposit.pidstore.weko_logger') as mock_logger:
+
+      # exist pid_value
+      record_uuid = uuid.uuid4()
+      data = {
+        "recid": "1",
+        "title": ["test item1"],
+        "_deposit": {
+          "id": "1",
+          "pid": { "type": "depid", "value": "1", "revision_id": 0 },
+          "owner": "1",
+          "owners": [1],
+          "status": "published",
+          "created_by": 1,
+          "owners_ext": {
+            "email": "wekosoftware@nii.ac.jp",
+            "username": "",
+            "displayname": ""
+          }
         }
       }
-    }
-    result = weko_deposit_fetcher(record_uuid, data)
-    assert result.provider == None
-    assert result.pid_type == "depid"
-    assert result.pid_value == "1"
+      result = weko_deposit_fetcher(record_uuid, data)
+      assert result.provider == None
+      assert result.pid_type == "depid"
+      assert result.pid_value == "1"
+      mock_logger.assert_called_once_with(key='WEKO_COMMON_RETURN_VALUE', value=result)
+      mock_logger.reset_mock()
 
-    record_uuid = uuid.uuid4()
-    data = {
-      "recid": "1",
-      "title": ["test item1"],
-      "_deposit": {
+      # not exist pid_value
+      record_uuid = uuid.uuid4()
+      data = {
+        "recid": "1",
+        "title": ["test item1"],
+        "_deposit": {
+        }
       }
-    }
-    result = weko_deposit_fetcher(record_uuid, data)
-    assert result == None
+      result = weko_deposit_fetcher(record_uuid, data)
+      assert result == None
+      mock_logger.assert_called_once_with(key='WEKO_COMMON_RETURN_VALUE', value=None)
 
 
 # def get_latest_version_id(recid):
@@ -98,72 +123,81 @@ def test_get_latest_version_id(app,db,records):
     assert result == 1
 
 
-# def get_record_identifier(recid):
-# .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py::test_get_record_identifier -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
-def test_get_record_identifier(app,db,es_records):
-    RecordIdentifier.next()
-    result = get_record_identifier("1")
-    assert result.recid == 1
+# # def get_record_identifier(recid):
+# # .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py::test_get_record_identifier -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
+# def test_get_record_identifier(app,db,es_records):
+#     RecordIdentifier.next()
+#     result = get_record_identifier("1")
+#     assert result.recid == 1
 
-    result = get_record_identifier("not int")
-    assert result == None
+#     result = get_record_identifier("not int")
+#     assert result == None
 
 # def get_record_identifier(recid):
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py::TestGetRecordIdentifier -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
-class TestGetRecordIdentifier(unittest.TestCase):
-    @patch('weko_deposit.logger.weko_logger')
-    @patch('invenio_pidstore.models.RecordIdentifier.query')
-    def test_get_record_identifier_success(self, mock_query, mock_logger):
-        recid = "1"
-        record_id = RecordIdentifier(recid=1)
+def test_get_record_identifier(app, db, es_records):
+    
+    # exsit record
+    recid = "1"
+    record_id = RecordIdentifier(recid=1)
+
+    with patch('weko_deposit.pidstore.RecordIdentifier.query') as mock_query:
         mock_query.filter_by.return_value.one_or_none.return_value = record_id
-        mock_logger.return_value = None
+        with patch('weko_deposit.pidstore.weko_logger') as mock_logger:
+            mock_logger.return_value = None
 
-        result = get_record_identifier(recid)
+            result = get_record_identifier(recid)
 
-        mock_query.filter_by.assert_called_once_with(recid=1)
-        mock_logger.assert_called_once_with(key='WEKO_COMMON_RETURN_VALUE', value=record_id)
-        self.assertEqual(result.recid, 1)
+            mock_query.filter_by.assert_called_once_with(recid=1)
+            mock_logger.assert_called_once_with(key='WEKO_COMMON_RETURN_VALUE', value=record_id)
+            assert result.recid == 1
 
-    @patch('weko_deposit.logger.weko_logger')
-    @patch('invenio_pidstore.models.RecordIdentifier.query')
-    def test_get_record_identifier_not_found(self, mock_query, mock_logger):
-        recid = "2"
+    # not exist record
+    recid = "2"
+
+    with patch('weko_deposit.pidstore.RecordIdentifier.query') as mock_query:
         mock_query.filter_by.return_value.one_or_none.return_value = None
-        mock_logger.return_value = None
+        with patch('weko_deposit.pidstore.weko_logger') as mock_logger:
+            mock_logger.return_value = None
 
-        result = get_record_identifier(recid)
+            result = get_record_identifier(recid)
 
-        mock_query.filter_by.assert_called_once_with(recid=2)
-        mock_logger.assert_called_once_with(key='WEKO_COMMON_RETURN_VALUE', value=None)
-        self.assertIsNone(result)
+            mock_query.filter_by.assert_called_once_with(recid=2)
+            mock_logger.assert_called_once_with(key='WEKO_COMMON_RETURN_VALUE', value=None)
+            assert result == None
 
-    @patch('weko_deposit.logger.weko_logger')
-    @patch('invenio_pidstore.models.RecordIdentifier.query')
-    def test_get_record_identifier_not_int(self, mock_query, mock_logger):
-        recid = "not int"
+    # recid not int(raise ValueError)
+    recid = "not int"
+
+    with patch('weko_deposit.pidstore.RecordIdentifier.query') as mock_query:
         mock_query.filter_by.return_value.one_or_none.return_value = None
-        mock_logger.return_value = None
+        with patch('weko_deposit.pidstore.weko_logger') as mock_logger:
+            mock_logger.return_value = None
 
-        result = get_record_identifier(recid)
+            result = get_record_identifier(recid)
 
-        self.assertEqual(mock_query.filter_by.call_count, 0)
-        mock_logger.assert_called_once_with(key='WEKO_COMMON_ERROR_UNEXPECTED')
-        self.assertEqual(result.recid, 1)
+            mock_query.filter_by.assert_not_called()
+            assert mock_logger.call_count == 2
+            mock_logger.assert_any_call(key='WEKO_COMMON_ERROR_UNEXPECTED', ex=mock.ANY)
+            mock_logger.assert_any_call(key='WEKO_COMMON_RETURN_VALUE', value=None)
+            assert result == None
 
-    @patch('weko_deposit.logger.weko_logger')
-    @patch('invenio_pidstore.models.RecordIdentifier.query')
-    def test_get_record_identifier_sqlalchemyerror(self, mock_query, mock_logger):
-        recid = "1"
-        ex = SQLAlchemyError('test error')
-        mock_query.filter_by.side_effect = ex
-        mock_logger.return_value = None
+    # raise SQLAlchemyError
+    recid = "1"
+    ex = SQLAlchemyError('test error')
 
-        result = get_record_identifier(recid)
+    with patch('weko_deposit.pidstore.RecordIdentifier.query') as mock_query:
+        mock_query.filter_by.return_value.one_or_none.side_effect = ex
+        with patch('weko_deposit.pidstore.weko_logger') as mock_logger:
+            mock_logger.return_value = None
 
-        mock_query.filter_by.assert_called_once_with(recid=1)
-        mock_logger.assert_called_once_with(key='WEKO_COMMON_DB_SOME_ERROR', ex=ex)
-        self.assertIsNone(result)
+            result = get_record_identifier(recid)
+
+            mock_query.filter_by.assert_called_once_with(recid=1)
+            assert mock_logger.call_count == 2
+            mock_logger.assert_any_call(key='WEKO_COMMON_DB_SOME_ERROR', ex=ex)
+            mock_logger.assert_any_call(key='WEKO_COMMON_RETURN_VALUE', value=None)
+            assert result == None
 
 # def get_record_without_version(pid):
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_pidstore.py::test_get_record_without_version -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
@@ -171,7 +205,6 @@ def test_get_record_without_version(app, db):
     obj_p1 = PersistentIdentifier.create('parent', "parent:1",object_type='rec', object_uuid=uuid.uuid4(),status=PIDStatus.REGISTERED)
     obj_c1 = PersistentIdentifier.create('recid', "1.1",object_type='rec', object_uuid=uuid.uuid4(),status=PIDStatus.REGISTERED)
     obj_without_ver = PersistentIdentifier.create('recid', "1",object_type='rec', object_uuid=uuid.uuid4(),status=PIDStatus.REGISTERED)
-    # obj_without_pid = PersistentIdentifier.create('recid', None,object_type='rec', object_uuid=uuid.uuid4(),status=PIDStatus.REGISTERED)
     rel1 = PIDRelation.create(obj_p1,obj_c1,2,0)
     rel2 = PIDRelation.create(obj_p1,obj_without_ver,2,0)
     db.session.add(rel1)
@@ -179,12 +212,29 @@ def test_get_record_without_version(app, db):
     other_obj = PersistentIdentifier.create('recid', "2",object_type='rec', object_uuid=uuid.uuid4(),status=PIDStatus.REGISTERED)
     db.session.commit()
 
-    # not exist parent_relations
-    result = get_record_without_version(other_obj)
-    assert result == None
+    with patch('weko_deposit.pidstore.weko_logger') as mock_logger:
+      
+      # not exist parent_relations
+      result = get_record_without_version(other_obj)
+      assert result == None
+      mock_logger.assert_called_once_with(key='WEKO_COMMON_RETURN_VALUE', value=None)
+      mock_logger.reset_mock()
 
-    result = get_record_without_version(obj_c1)
-    assert result == obj_without_ver
+      # parent_pid is not None
+      result = get_record_without_version(obj_c1)
+      assert result == obj_without_ver
+      mock_logger.call_count == 3
+      mock_logger.assert_any_call(key='WEKO_COMMON_IF_ENTER', branch='parent_relations is not None')
+      mock_logger.assert_any_call(key='WEKO_COMMON_IF_ENTER', branch='parent_pid is not None')
+      mock_logger.assert_any_call(key='WEKO_COMMON_RETURN_VALUE', value=obj_without_ver)
+      mock_logger.reset_mock()
 
-    # result = get_record_without_version(obj_without_pid)
-    # assert result == obj_without_pid
+      # parent_pid is None
+      with patch('weko_deposit.pidstore.PersistentIdentifier.query') as mock_query:
+        mock_query.filter_by.return_value.one_or_none.return_value = None
+        result = get_record_without_version(obj_c1)
+        assert result == None
+        mock_logger.call_count == 2
+        mock_logger.assert_any_call(key='WEKO_COMMON_IF_ENTER', branch='parent_relations is not None')
+        mock_logger.assert_any_call(key='WEKO_COMMON_RETURN_VALUE', value=None)
+
