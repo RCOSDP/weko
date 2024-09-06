2dc36fcda0d18ec67de2a904d70098a65902856b
asuka.hirabayashi <asuka.hirabayashi@ivis.co.jp>
2024-09-06 10:34:49 +0900

    fix errors.py

--- modules/weko-records/weko_records/errors.py
+++ modules/weko-records/weko_records/errors.py
@@ -20,14 +20,14 @@ class WekoRecordsError(Exception):
         if ex is not None:
             self.exception = ex
         if msg is None:
-            msg = "Some registration error has occurred in weko_records."
+            msg = "Some error has occurred in weko_records."
         super().__init__(msg, *args)
 
 
 class WekoRecordsRegistrationError(WekoRecordsError):
     def __init__(self, ex=None, msg=None, *args):
         if msg is None:
-            msg = "Some error has occurred in weko_records."
+            msg = "Some registration error has occurred in weko_records."
         super().__init__(ex, msg, *args)
 
 
@@ -45,10 +45,10 @@ class WekoRecordsMetadataError(WekoRecordsError):
         super().__init__(ex, msg, *args)
 
 
-class WekoRecordsSiteError(WekoRecordsError):
+class WekoRecordsSiteLicenseError(WekoRecordsError):
     def __init__(self, ex=None, msg=None, *args):
         if msg is None:
-            msg = "Some  site error has occurred in weko_records."
+            msg = "Some site lisence error has occurred in weko_records."
         super().__init__(ex, msg, *args)
 
 
