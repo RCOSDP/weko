2db21edb178eb33689463204e6c9b65523ff8120
asuka.hirabayashi <asuka.hirabayashi@ivis.co.jp>
2024-09-12 16:29:51 +0900

    update test of links
    test_links.py

--- modules/weko-deposit/tests/test_links.py
+++ modules/weko-deposit/tests/test_links.py
@@ -1,4 +1,6 @@
 
+from unittest import mock
+from unittest.mock import patch
 import uuid
 
 from invenio_pidstore.models import PersistentIdentifier
@@ -10,48 +12,51 @@ from weko_deposit.links import links_factory,base_factory
 # def links_factory(pid, **kwargs):
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_links.py::test_links_factory -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
 def test_links_factory(app, db,mocker):
-    pids = list()
-    pids.append(PersistentIdentifier.create('recid', "1.0",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
-    pids.append(PersistentIdentifier.create('recid', "1.1",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
-    pids.append(PersistentIdentifier.create('recid', "1.2",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
-    db.session.add_all(pids)
-    db.session.commit()
-    base = {
-        "self":"/records/1.1"
-    }
-    new_url = {
-        "index":"/api/deposits/redirect/1.1",
-        "r":"/items/index/1.1",
-        "iframe_tree":"/items/iframe/index/1.1",
-        "iframe_tree_upgrade":"/items/iframe/index/1.3"
-    }
-    mocker.patch("weko_deposit.links.deposit_links_factory",return_value=base)
-    mocker.patch("weko_deposit.links.base_factory",return_value=new_url)
-    test = {
-        "self":"/records/1.1",
-        "index":"/api/deposits/redirect/1.1",
-        "r":"/items/index/1.1",
-        "iframe_tree":"/items/iframe/index/1.1",
-        "iframe_tree_upgrade":"/items/iframe/index/1.3"
-    }
-    result = links_factory(pids[1])
-    assert result == test
-
+    with patch("weko_deposit.links.weko_logger") as mock_logger:
+        pids = list()
+        pids.append(PersistentIdentifier.create('recid', "1.0",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
+        pids.append(PersistentIdentifier.create('recid', "1.1",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
+        pids.append(PersistentIdentifier.create('recid', "1.2",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
+        db.session.add_all(pids)
+        db.session.commit()
+        base = {
+            "self":"/records/1.1"
+        }
+        new_url = {
+            "index":"/api/deposits/redirect/1.1",
+            "r":"/items/index/1.1",
+            "iframe_tree":"/items/iframe/index/1.1",
+            "iframe_tree_upgrade":"/items/iframe/index/1.3"
+        }
+        mocker.patch("weko_deposit.links.deposit_links_factory",return_value=base)
+        mocker.patch("weko_deposit.links.base_factory",return_value=new_url)
+        test = {
+            "self":"/records/1.1",
+            "index":"/api/deposits/redirect/1.1",
+            "r":"/items/index/1.1",
+            "iframe_tree":"/items/iframe/index/1.1",
+            "iframe_tree_upgrade":"/items/iframe/index/1.3"
+        }
+        result = links_factory(pids[1])
+        assert result == test
+        mock_logger.assert_called_with(key='WEKO_COMMON_RETURN_VALUE', value=mock.ANY)
 
 # def base_factory(pid, **kwargs):
 # .tox/c1/bin/pytest --cov=weko_deposit tests/test_links.py -v -s -vv --cov-branch --cov-report=term --cov-config=tox.ini --basetemp=/code/modules/weko-deposit/.tox/c1/tmp
 def test_base_factory(app,db):
-    pids = list()
-    pids.append(PersistentIdentifier.create('recid', "1.0",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
-    pids.append(PersistentIdentifier.create('recid', "1.1",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
-    pids.append(PersistentIdentifier.create('recid', "1.2",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
-    db.session.add_all(pids)
-    db.session.commit()
-    test = {
-        "index":"/api/deposits/redirect/1.1",
-        "r":"/items/index/1.1",
-        "iframe_tree":"/items/iframe/index/1.1",
-        "iframe_tree_upgrade":"/items/iframe/index/1.3"
-    }
-    result = base_factory(pids[1])
-    assert result == test
+    with patch("weko_deposit.links.weko_logger") as mock_logger:
+        pids = list()
+        pids.append(PersistentIdentifier.create('recid', "1.0",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
+        pids.append(PersistentIdentifier.create('recid', "1.1",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
+        pids.append(PersistentIdentifier.create('recid', "1.2",object_type='rec', object_uuid=uuid.uuid4(),status="R"))
+        db.session.add_all(pids)
+        db.session.commit()
+        test = {
+            "index":"/api/deposits/redirect/1.1",
+            "r":"/items/index/1.1",
+            "iframe_tree":"/items/iframe/index/1.1",
+            "iframe_tree_upgrade":"/items/iframe/index/1.3"
+        }
+        result = base_factory(pids[1])
+        assert result == test
+        mock_logger.assert_called_with(key='WEKO_COMMON_RETURN_VALUE', value=mock.ANY)
