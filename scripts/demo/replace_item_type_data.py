9d2d5b4d0e5ec42392aafd57fd7b885996d5bb34
wei-kuochen <wei_kuochen@co-graph.co.jp>
2024-09-30 16:52:29 +0900

    weko#45417 added fix item type tool

--- scripts/demo/replace_item_type_data.py
+++ scripts/demo/replace_item_type_data.py
@@ -1,23 +1,23 @@
 from weko_records.models import ItemType
+from weko_itemtypes_ui.utils import fix_json_schema, update_required_schema_not_exist_in_form
 from sqlalchemy import desc
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm.attributes import flag_modified
 from invenio_db import db
 from flask import current_app
 
 from properties import checkbox, radiobutton, listbox, property_func
 
-UPDATE_DATE = '2024/9/14'
+UPDATE_DATE = '2024/9/28'
 
 
 def get_option(item_type_data, item_key):
     return item_type_data.render.get("meta_list", {}).get(item_key, {}).get("option")
 
-def replace_schema(schema_old, schema_new, item_key, sub_key, prop, option_old):
-    multi_flag = prop.multiple_flag
+def replace_schema(schema_old, schema_new, item_key, sub_key, multi_flag):
     _required = []
 
-    if "multiple" in option_old:
-        multi_flag = option_old["multiple"]
-    if schema_old.get("type") == "array":
+    if schema_old.get("properties", {}).get(item_key, {}).get("type") == "array":
         _enum = schema_old.get("properties", {}) \
             .get(item_key, {}).get("items", {}) \
             .get("properties", {}).get(sub_key, {}) \
@@ -35,7 +35,7 @@ def replace_schema(schema_old, schema_new, item_key, sub_key, prop, option_old):
             and "properties" in schema_new \
             and item_key in schema_new["properties"]:
         schema = schema_new["properties"][item_key]
-        if schema.get("type") == "option":
+        if schema.get("type") == "object":
             if "properties" in schema \
                     and sub_key in schema["properties"] \
                     and "items" in schema["properties"][sub_key]:
@@ -51,7 +51,7 @@ def replace_schema(schema_old, schema_new, item_key, sub_key, prop, option_old):
                 if _required:
                     schema["items"]["required"] = _required
 
-        if schema.get("type") == "option" and multi_flag:
+        if schema.get("type") == "object" and multi_flag:
             _title = schema.pop("title")
             schema_new["properties"][item_key] = {
                 "type": "array",
@@ -65,25 +65,40 @@ def replace_schema(schema_old, schema_new, item_key, sub_key, prop, option_old):
             _schema = schema.get("items", {})
             _schema["title"] = _title
             schema_new["properties"][item_key] = _schema
+        
 
-def replace_form(form_old, form_new, item_key, sub_key, prop, option_old):
-    multi_flag = prop.multiple_flag
-
-    if "multiple" in option_old:
-        multi_flag = option_old["multiple"]
+def replace_form(form_old, form_new, item_key, sub_key, multi_flag):
     for i in form_old.get("items"):
         if sub_key in i.get("key"):
             _titleMap = i.get("titleMap")
             _value = i.get("title_i18n_temp")
+            _v_option = {
+                "required": i.get("required", False),
+                "isShowList": i.get("isShowList", False),
+                "isSpecifyNewline": i.get("isSpecifyNewline", False),
+                "isHide": i.get("isHide", False),
+                "isNonDisplay": i.get("isNonDisplay", False),
+            }
         else:
             _lang = i.get("title_i18n_temp")
+            _l_option = {
+                "required": i.get("required", False),
+                "isShowList": i.get("isShowList", False),
+                "isSpecifyNewline": i.get("isSpecifyNewline", False),
+                "isHide": i.get("isHide", False),
+                "isNonDisplay": i.get("isNonDisplay", False),
+            }
 
     for i in form_new.get("items"):
         if sub_key in i.get("key"):
             i["titleMap"] = _titleMap
             i["title_i18n_temp"] = _value
+            for k, v in _v_option.items():
+                i[k] = v
         else:
             i["title_i18n_temp"] = _lang
+            for k, v in _l_option.items():
+                i[k] = v
 
     if form_new.get("add", None) == None and multi_flag:
         form_new["add"] = "New"
@@ -97,7 +112,34 @@ def replace_form(form_old, form_new, item_key, sub_key, prop, option_old):
         for i in form_new.get("items"):
             i.get("key").replace("{}[]".format(item_key), item_key)
 
+def replace_item_type_data(render_old, render_new, item_key, sub_key, prop, option_old):
+    if "meta_list" in render_new and \
+            item_key in render_new["meta_list"]:
+        render_new["meta_list"][item_key]["option"] = option_old
+    multi_flag = prop.multiple_flag
+    if "multiple" in option_old:
+        multi_flag = option_old["multiple"]
+
+    _table_row_schema_old = render_old['table_row_map']['schema']
+    _table_row_schema_new = render_new['table_row_map']['schema']
+    _schema_old = render_old['schemaeditor']['schema']
+    _schema_new = render_new['schemaeditor']['schema']
+    _form_old = render_old['table_row_map']['form']
+    _form_new = render_new['table_row_map']['form']
+    replace_schema(_table_row_schema_old, _table_row_schema_new, item_key, sub_key, multi_flag)
+    #replace_schema(_schema_old, _schema_new, item_key, sub_key, False)
+    for f in _form_old:
+        if f.get("key") == item_key:
+            _form_prop_old = f
+            break
+    for f in _form_new:
+        if f.get("key") == item_key:
+            _form_prop_new = f
+            break
+    replace_form(_form_prop_old, _form_prop_new, item_key, sub_key, multi_flag)
+
 def main():
+    current_app.logger.info('=============== replace_item_type_data ===============')
     current_app.logger.info('start')
     try:
         sql = """
@@ -106,82 +148,55 @@ def main():
                    render
             FROM item_type_version
             WHERE id = :item_type_id
-              AND  updated < :update_date
+              AND  updated <= :update_date
             ORDER BY version_id DESC;
             """.strip()
-        res = db.session.query(ItemType.id).all()
+        res = db.session.query(ItemType.id).filter(ItemType.id==40002).all()
         for _id in res:
             params = {
                 "item_type_id": _id,
                 "update_date": UPDATE_DATE
             }
-            _checkbox_key = None
-            _radiobtn_key = None
-            _listbox_key = None
-            with db.session.begin_nested():
-                item_type_new = ItemType.query.filter(ItemType.id==_id).order_by(desc(ItemType.created)).first()
-                item_type_old = db.session.execute(sql, params).first()
+            try:
+                with db.session.begin_nested():
+                    item_type_new = ItemType.query.filter(ItemType.id==_id).order_by(desc(ItemType.created)).first()
+                    item_type_old = db.session.execute(sql, params).first()
 
-                if item_type_old and "properties" in item_type_old.schema:
-                    for _k1, _v1 in item_type_old.schema["properties"].items():
-                        if _v1.get("type") == "object":
-                            _prop_keys = _v1.get("properties", {}).keys()
-                        elif _v1.get("type") == "array":
-                            _prop_keys = _v1.get("items", {}).get("properties", {}).keys()
-                        
+                    for _k1, _v1 in item_type_old.render["schemaeditor"]["schema"].items():
+                        _prop_keys = _v1.get("properties", {}).keys()
+
                         if "subitem_checkbox_item" in _prop_keys:
-                            _checkbox_key = _k1
-                            _option = get_option(item_type_old, _checkbox_key)
+                            _option = get_option(item_type_old, _k1)
+                            replace_item_type_data(item_type_old.render, item_type_new.render, _k1, "subitem_checkbox_item", checkbox, _option)
                         if "subitem_radio_item" in _prop_keys:
-                            _radiobtn_key = _k1
-                            _option = get_option(item_type_old, _radiobtn_key)
+                            _option = get_option(item_type_old, _k1)
+                            replace_item_type_data(item_type_old.render, item_type_new.render, _k1, "subitem_radio_language", radiobutton, _option)
                         if "subitem_select_item" in _prop_keys:
-                            _listbox_key = _k1
-                            _option = get_option(item_type_old, _listbox_key)
-
-                if _option:
-                    # schema
-                    if _checkbox_key:
-                        replace_schema(item_type_old.schema, item_type_new.schema, _checkbox_key, "subitem_checkbox_item", checkbox, _option)
-                    if _radiobtn_key:
-                        replace_schema(item_type_old.schema, item_type_new.schema, _radiobtn_key, "subitem_radio_item", radiobutton, _option)
-                    if _listbox_key:
-                        replace_schema(item_type_old.schema, item_type_new.schema, _listbox_key, "subitem_select_item", listbox, _option)
-
-                    # form
-                    _form_old = None
-                    _form_new = None
-                    for f in item_type_old.form:
-                        if _checkbox_key and f.get("key") == _checkbox_key:
-                            _form_old = f
-                        if _radiobtn_key and f.get("key") == _radiobtn_key:
-                            _form_old = f
-                        if _listbox_key and f.get("key") == _listbox_key:
-                            _form_old = f
-                    for f in item_type_new.form:
-                        if _checkbox_key and f.get("key") == _checkbox_key:
-                            _form_new = f
-                        if _radiobtn_key and f.get("key") == _radiobtn_key:
-                            _form_new = f
-                        if _listbox_key and f.get("key") == _listbox_key:
-                            _form_new = f
-                    if _form_old and _form_new:
-                        if _checkbox_key:
-                            replace_form(_form_old, _form_new, _checkbox_key, "subitem_checkbox_item", checkbox, _option)
-                        if _radiobtn_key:
-                            replace_form(_form_old, _form_new, _radiobtn_key, "subitem_radio_item", radiobutton, _option)
-                        if _listbox_key:
-                            replace_form(_form_old, _form_new, _listbox_key, "subitem_select_item", listbox, _option)
-
-                current_app.logger.info(_k1)
-                current_app.logger.info(_v1)
-            break
-        db.session.commit()
+                            _option = get_option(item_type_old, _k1)
+                            replace_item_type_data(item_type_old.render, item_type_new.render, _k1, "subitem_select_item", listbox, _option)
+                    json_schema = fix_json_schema(item_type_new.render['table_row_map']['schema'])
+                    json_form = item_type_new.render['table_row_map']['form']
+                    json_schema = update_required_schema_not_exist_in_form(json_schema, json_form)
+                    item_type_new.schema = json_schema
+                    item_type_new.form = json_form
+                    flag_modified(item_type_new, "schema")
+                    flag_modified(item_type_new, "form")
+                    flag_modified(item_type_new, "render")
+                    db.session.merge(item_type_new)
+                db.session.commit()
+                current_app.logger.info('Updated item type (id = {}) successfully.'.format(_id))
+            except SQLAlchemyError as e:
+                current_app.logger.error('Failed to update item type (id = {}).'.format(_id))
+                current_app.logger.error(e)
+                db.session.rollback()
+            except Exception as e:
+                current_app.logger.error('Failed to update item type (id = {}).'.format(_id))
+                current_app.logger.error(e)
+                db.session.rollback()
     except Exception as ex:
         current_app.logger.error(ex)
-        db.session.rollback()
     current_app.logger.info('end')
-    current_app.logger.info('Finished!')
+    current_app.logger.info('=============== Finished! ===============')
 
 if __name__ == '__main__':
     """Main context."""
