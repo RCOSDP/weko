5f0f13ee51d14ac83d4584e985345d9f8da70642
MakotoASAOKA <asaoka@nii.ac.jp>
2024-10-13 12:57:46 +0000

    deleate nginx/ams

--- nginx/ams/weko-frontend/composables/getContentById.ts
+++
@@ -1,9 +0,0 @@
-export default function (obj: any, id: string) {
-  let result: any = {};
-  obj['@graph'].forEach((element: any) => {
-    if (element['@id'] === id) {
-      result = element;
-    }
-  });
-  return result;
-}
