2048f99b2fbc514c8903ce6229d6f7224b171589
ivis-nakagawa <tatsuki.nakagawa@ivis.co.jp>
2024-10-16 09:48:21 +0900

    weko#45809 Remove pageStart in required item of thesis_types

--- modules/weko-workflow/tests/test_utils.py
+++ modules/weko-workflow/tests/test_utils.py
@@ -342,7 +342,7 @@ def test_item_metadata_validation(db_records,item_type):
     with patch("weko_workflow.utils.MappingData.get_first_data_by_mapping",\
         side_effect=[("item_1617258105262.resourcetype", ['thesis']),(None,["report"])]):
         result = item_metadata_validation(None,"5",without_ver_id=without_ver.object_uuid,record=record,file_path="test_file_path")
-        assert result == 'Cannot register selected DOI for current Item Type of this item.'
+        assert result['other'] == 'Cannot register selected DOI for current Item Type of this item.'
 
 
 #* THIS IS FOR JPCOAR2.0 DOI VALIDATION TEST
@@ -397,6 +397,8 @@ def test_item_metadata_validation_2(db_records_for_doi_validation_test, item_typ
             if result_1_check_item_1 in result_1.get(result_1_key):
                 result_1_check_list_2.append(result_1_check_item_1)
     assert len(result_1_check_list_2) == 1
+    # issue 45809
+    assert not "jpcoar:pageStart" in result_1.get("either_key")
 
     #* 別表2-3 JaLC DOI
     recid2, depid2, record2, item2, parent2, doi2, deposit2 = db_records_for_doi_validation_test[2]
@@ -510,7 +512,7 @@ def test_item_metadata_validation_2(db_records_for_doi_validation_test, item_typ
         for result_6_key in result_6_keys:
             if result_6_check_item_1 in result_6.get(result_6_key):
                 result_6_check_list_2.append(result_6_check_item_1)
-    assert len(result_6_check_list_2) == 3
+    assert len(set(result_6_check_list_2)) == 3
 
     #* 別表3-2 Crossref DOI
     recid7, depid7, record7, item7, parent7, doi7, deposit7 = db_records_for_doi_validation_test[7]
