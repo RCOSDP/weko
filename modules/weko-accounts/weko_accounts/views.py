898c1e4aa508877846f8bd3c0c4e1ccbffb84c47
wei-kuochen <wei_kuochen@co-graph.co.jp>
2024-10-18 16:49:45 +0900

    weko#45013 fix redirect url issue

--- modules/weko-accounts/weko_accounts/views.py
+++ modules/weko-accounts/weko_accounts/views.py
@@ -26,6 +26,7 @@ Blueprint.
 
 import json
 import sys
+from urllib.parse import quote_plus
 
 import redis
 from redis import sentinel
@@ -358,6 +359,12 @@ def shib_stub_login():
             sp_handlerURL = sp_handlerURL,
             return_url = return_url,
             module_name=_('WEKO-Accounts'))
+
+
+@blueprint.app_template_filter('urlencode')
+def urlencode(value):
+    """Encode url value."""
+    return quote_plus(value)
 
 
 @blueprint.route('/shib/logout')
