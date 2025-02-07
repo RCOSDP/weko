b1e4cbbb9f04d4f816479193b5b6feb27c0462f4
guan.shuang <guan.shuang@za.ivis.co.jp>
2025-02-07 15:30:08 +0900

    アイテム一覧画面モック追加

--- modules/weko-workspace/weko_workspace/views.py
+++ modules/weko-workspace/weko_workspace/views.py
@@ -34,11 +34,6 @@ from typing import List
 
 import redis
 from redis import sentinel
-# from weko_workflow.schema.marshmallow import ActionSchema, \
-#     ActivitySchema, ResponseMessageSchema, CancelSchema, PasswdSchema, LockSchema,\
-#     ResponseLockSchema, LockedValueSchema, GetFeedbackMailListSchema, SaveActivityResponseSchema,\
-#     SaveActivitySchema, CheckApprovalSchema,ResponseUnlockSchema
-# from weko_workflow.schema.utils import get_schema_action, type_null_check
 from marshmallow.exceptions import ValidationError
 
 from flask import Response, Blueprint, abort, current_app, has_request_context, \
@@ -69,48 +64,25 @@ from weko_deposit.links import base_factory
 from weko_deposit.pidstore import get_record_identifier, \
     get_record_without_version
 from weko_deposit.signals import item_created
-# from weko_items_ui.api import item_login
 from weko_records.api import FeedbackMailList, ItemLink
 from weko_records.models import ItemMetadata
 from weko_records.serializers.utils import get_item_type_name
-# from weko_records_ui.models import FilePermission
-# from weko_search_ui.utils import check_import_items, import_items_to_system
 from weko_user_profiles.config import \
     WEKO_USERPROFILES_INSTITUTE_POSITION_LIST, \
     WEKO_USERPROFILES_POSITION_LIST
-
-# from .api import Action, Flow, GetCommunity, WorkActivity, \
-#     WorkActivityHistory, WorkFlow
 from .config import IDENTIFIER_GRANT_LIST, IDENTIFIER_GRANT_SELECT_DICT, \
     IDENTIFIER_GRANT_SUFFIX_METHOD, WEKO_WORKFLOW_TODO_TAB
 from .errors import ActivityBaseRESTError, ActivityNotFoundRESTError, \
     DeleteActivityFailedRESTError, InvalidInputRESTError, \
     RegisteredActivityNotFoundRESTError
-# from .models import ActionStatusPolicy, Activity, ActivityAction, \
-#     ActivityStatusPolicy, FlowAction, GuestActivity
+
 from .romeo import search_romeo_issn, search_romeo_jtitles
 from .scopes import activity_scope
-# from .utils import IdentifierHandle, auto_fill_title, \
-#     check_authority_by_admin, check_continue, check_doi_validation_not_pass, \
-#     check_existed_doi, is_terms_of_use_only, \
-#     delete_cache_data, delete_guest_activity, filter_all_condition, \
-#     get_account_info, get_actionid, get_activity_display_info, \
-#     get_activity_id_of_record_without_version, \
-#     get_application_and_approved_date, get_approval_keys, get_cache_data, \
-#     get_files_and_thumbnail, get_identifier_setting, get_main_record_detail, \
-#     get_pid_and_record, get_pid_value_by_activity_detail, \
-#     get_record_by_root_ver, get_thumbnails, get_usage_data, \
-#     get_workflow_item_type_names, grant_access_rights_to_all_open_restricted_files, handle_finish_workflow, \
-#     init_activity_for_guest_user, is_enable_item_name_link, \
-#     is_hidden_pubdate, is_show_autofill_metadata, \
-#     is_usage_application_item_type, prepare_data_for_guest_activity, \
-#     prepare_doi_link_workflow, process_send_approval_mails, \
-#     process_send_notification_mail, process_send_reminder_mail, register_hdl, \
-#     save_activity_data, saving_doi_pidstore, \
-#     send_usage_application_mail_for_guest_user, set_files_display_type, \
-#     update_approval_date, update_cache_data, validate_guest_activity_expired, \
-#     validate_guest_activity_token, make_activitylog_tsv, \
-#     delete_lock_activity_cache, delete_user_lock_activity_cache
+
+# =============本物=================
+from flask_menu import register_menu
+from flask_breadcrumbs import register_breadcrumb
+
 
 workspace_blueprint = Blueprint(
     'weko_workspace',
@@ -122,10 +94,14 @@ workspace_blueprint = Blueprint(
 
 @workspace_blueprint.route('/workspace')
 @login_required
+# @register_menu(
+#     workspace_blueprint, 'settings.Workspace',
+#     _('%(icon)s Workspace', icon='<i class="fa fa-list-alt" aria-hidden="true" style="margin-right: 8px;"></i>'),
+#     order=20)
+# @register_breadcrumb(workspace_blueprint, 'breadcrumbs.settings.Workspace', _('Workspace'))
 def workspace():
         print("==========guan.shuang workspace =========")
         # return None
         return render_template(
-        # 'weko_workflow/workspaceItemList.html'
         'weko_workspace/workspace_base.html'
     )
