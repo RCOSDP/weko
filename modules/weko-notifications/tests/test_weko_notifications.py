86f84934463f11d24e84f89f0b2edd59c59e3a71
ivis-kuroda <tomohiro.kuroda@ivis.co.jp>
2025-02-18 19:45:47 +0900

    add: for test

--- modules/weko-notifications/tests/test_weko_notifications.py
+++ modules/weko-notifications/tests/test_weko_notifications.py
@@ -31,10 +31,3 @@ def test_init():
     assert 'weko-notifications' not in app.extensions
     ext.init_app(app)
     assert 'weko-notifications' in app.extensions
-
-
-def test_view(base_client):
-    """Test view."""
-    res = base_client.get("/")
-    assert res.status_code == 200
-    assert 'Welcome to WEKO-Notifications' in str(res.data)
