masahiro-oji/weko3

create: weko-notifications by cookiecutter

modules/weko-notifications/tests/conftest.py
@@ -1,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 National Institute of Informatics.
+#
+# WEKO-Notification is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pytest configuration.
+
+See https://pytest-invenio.readthedocs.io/ for documentation on which test
+fixtures are available.
+"""
+
+from __future__ import absolute_import, print_function
+
+import shutil
+import tempfile
+
+import pytest
+from flask import Flask
+from flask_babelex import Babel
+
+from weko_notification import WekoNotification
+from weko_notification.views import blueprint
+
+
+@pytest.fixture(scope='module')
+def celery_config():
+    """Override pytest-invenio fixture.
+
+    TODO: Remove this fixture if you add Celery support.
+    """
+    return {}
+
+
+@pytest.fixture(scope='module')
+def create_app(instance_path):
+    """Application factory fixture."""
+    def factory(**config):
+        app = Flask('testapp', instance_path=instance_path)
+        app.config.update(**config)
+        Babel(app)
+        WekoNotification(app)
+        app.register_blueprint(blueprint)
+        return app
+    return factory

modules/weko-notifications/tests/test_examples_app.py
@@ -1,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 National Institute of Informatics.
+#
+# WEKO-Notification is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Test example app."""
+
+import os
+import signal
+import subprocess
+import time
+from os.path import abspath, dirname, join
+
+import pytest
+
+
+@pytest.yield_fixture
+def example_app():
+    """Example app fixture."""
+    current_dir = os.getcwd()
+
+    # Go to example directory
+    project_dir = dirname(dirname(abspath(__file__)))
+    exampleapp_dir = join(project_dir, 'examples')
+    os.chdir(exampleapp_dir)
+
+    # Setup application
+    assert subprocess.call('./app-setup.sh', shell=True) == 0
+
+    # Setup fixtures
+    assert subprocess.call('./app-fixtures.sh', shell=True) == 0
+
+    # Start example app
+    webapp = subprocess.Popen(
+        'FLASK_APP=app.py flask run --debugger -p 5000',
+        stdout=subprocess.PIPE, preexec_fn=os.setsid, shell=True)
+    time.sleep(10)
+    yield webapp
+
+    # Stop server
+    os.killpg(webapp.pid, signal.SIGTERM)
+
+    # Tear down example app
+    subprocess.call('./app-teardown.sh', shell=True)
+
+    # Return to the original directory
+    os.chdir(current_dir)
+
+
+def test_example_app_role_admin(example_app):
+    """Test example app."""
+    cmd = 'curl http://0.0.0.0:5000/'
+    output = subprocess.check_output(cmd, shell=True)
+    assert b'Welcome to WEKO-Notification' in output

RCOSDP/weko

modules/weko-notifications/tests/conftest.py
@@ -1,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 National Institute of Informatics.
+#
+# WEKO-Notifications is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pytest configuration.
+
+See https://pytest-invenio.readthedocs.io/ for documentation on which test
+fixtures are available.
+"""
+
+from __future__ import absolute_import, print_function
+
+import shutil
+import tempfile
+
+import pytest
+from flask import Flask
+from flask_babelex import Babel
+
+from weko_notifications import WekoNotifications
+from weko_notifications.views import blueprint
+
+
+@pytest.fixture(scope='module')
+def celery_config():
+    """Override pytest-invenio fixture.
+
+    TODO: Remove this fixture if you add Celery support.
+    """
+    return {}
+
+
+@pytest.fixture(scope='module')
+def create_app(instance_path):
+    """Application factory fixture."""
+    def factory(**config):
+        app = Flask('testapp', instance_path=instance_path)
+        app.config.update(**config)
+        Babel(app)
+        WekoNotifications(app)
+        app.register_blueprint(blueprint)
+        return app
+    return factory

modules/weko-notifications/tests/test_examples_app.py
@@ -1,0 +1,56 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 National Institute of Informatics.
+#
+# WEKO-Notifications is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Test example app."""
+
+import os
+import signal
+import subprocess
+import time
+from os.path import abspath, dirname, join
+
+import pytest
+
+
+@pytest.yield_fixture
+def example_app():
+    """Example app fixture."""
+    current_dir = os.getcwd()
+
+    # Go to example directory
+    project_dir = dirname(dirname(abspath(__file__)))
+    exampleapp_dir = join(project_dir, 'examples')
+    os.chdir(exampleapp_dir)
+
+    # Setup application
+    assert subprocess.call('./app-setup.sh', shell=True) == 0
+
+    # Setup fixtures
+    assert subprocess.call('./app-fixtures.sh', shell=True) == 0
+
+    # Start example app
+    webapp = subprocess.Popen(
+        'FLASK_APP=app.py flask run --debugger -p 5000',
+        stdout=subprocess.PIPE, preexec_fn=os.setsid, shell=True)
+    time.sleep(10)
+    yield webapp
+
+    # Stop server
+    os.killpg(webapp.pid, signal.SIGTERM)
+
+    # Tear down example app
+    subprocess.call('./app-teardown.sh', shell=True)
+
+    # Return to the original directory
+    os.chdir(current_dir)
+
+
+def test_example_app_role_admin(example_app):
+    """Test example app."""
+    cmd = 'curl http://0.0.0.0:5000/'
+    output = subprocess.check_output(cmd, shell=True)
+    assert b'Welcome to WEKO-Notifications' in output
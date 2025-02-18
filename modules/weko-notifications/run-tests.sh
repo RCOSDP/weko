035abf6696f3df2917c157f32c762ea18f94d6aa
ivis-kuroda <tomohiro.kuroda@ivis.co.jp>
2025-02-18 11:31:01 +0900

    create: weko-notifications by cookiecutter

---
+++ modules/weko-notifications/run-tests.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 National Institute of Informatics.
+#
+# WEKO-Notifications is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+pydocstyle weko_notifications tests docs && \
+isort -rc -c -df && \
+check-manifest --ignore ".travis-*" && \
+sphinx-build -qnNW docs docs/_build/html && \
+python setup.py test
---
+++ modules/weko-notifications/weko_notifications/config.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2025 National Institute of Informatics.
+#
+# WEKO-Notifications is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Module of weko-notifications."""
+
+# TODO: This is an example file. Remove it if your package does not use any
+# extra configuration variables.
+
+weko_notifications_DEFAULT_VALUE = 'foobar'
+"""Default value for the application."""
+
+weko_notifications_BASE_TEMPLATE = 'weko_notifications/base.html'
+"""Default base template for the demo page."""
