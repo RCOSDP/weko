d0444ecafe64427d3caa29f3443dd7fed7e30d38
ayumi-nishida <ayumi.nishida@za.ivis.co.jp>
2025-02-26 14:02:33 +0900

    ダブルクォーテ、シングルクォーテの混在を整理

--- modules/weko-accounts/weko_accounts/config.py
+++ modules/weko-accounts/weko_accounts/config.py
@@ -63,11 +63,11 @@ WEKO_ACCOUNTS_SHIB_IDP_LOGIN_URL = '{}secure/login.php'
 
 WEKO_ACCOUNTS_SSO_ATTRIBUTE_MAP = {
     'SHIB_ATTR_EPPN': (False, 'shib_eppn'),
-    # "SHIB_ATTR_LOGIN_ID": (False, 'shib_uid'),
-    # "SHIB_ATTR_HANDLE": (False, 'shib_handle'),
+    # 'SHIB_ATTR_LOGIN_ID': (False, 'shib_uid'),
+    # 'SHIB_ATTR_HANDLE': (False, 'shib_handle'),
     'SHIB_ATTR_ROLE_AUTHORITY_NAME': (False, 'shib_role_authority_name'),
-    # "SHIB_ATTR_PAGE_NAME": (False, 'shib_page_name'),
-    # "SHIB_ATTR_ACTIVE_FLAG": (False, 'shib_active_flag'),
+    # 'SHIB_ATTR_PAGE_NAME': (False, 'shib_page_name'),
+    # 'SHIB_ATTR_ACTIVE_FLAG': (False, 'shib_active_flag'),
     'SHIB_ATTR_SITE_USER_WITHIN_IP_RANGE_FLAG': (False, 'shib_ip_range_flag'),
     'SHIB_ATTR_MAIL': (False, 'shib_mail'),
     'SHIB_ATTR_USER_NAME': (False, 'shib_user_name'),
@@ -113,26 +113,26 @@ WEKO_ACCOUNTS_GENERAL_ROLE = 'Contributor'
 """Default role."""
 
 WEKO_ACCOUNTS_GAKUNIN_ROLE = {
-  "defaultRole": "Contributor",
-  "organizationName": []  
+  'defaultRole': 'Contributor',
+  'organizationName': []  
 } 
 """Gakunin Default role."""
 
 WEKO_ACCOUNTS_ORTHROS_INSIDE_ROLE = {
-  "defaultRole": 'Repository Administrator',
-  "organizationName": []  
+  'defaultRole': 'Repository Administrator',
+  'organizationName': []  
 } 
 """Orthros (Inside) Default role."""
 
 WEKO_ACCOUNTS_ORTHROS_OUTSIDE_ROLE = {
-  "defaultRole": 'Community Administrator',
-  "organizationName": []  
+  'defaultRole': 'Community Administrator',
+  'organizationName': []  
 } 
 """Orthros (Outsite) Default role."""
 
 WEKO_ACCOUNTS_EXTRA_ROLE = {
-  "defaultRole": 'None', # ロール無
-  "organizationName": []  
+  'defaultRole': 'None', # ロール無
+  'organizationName': []  
 } 
 """Extra Default role."""
 
@@ -156,17 +156,17 @@ WEKO_ACCOUNTS_SHIB_DP_LOGIN_DIRECTLY_ENABLED = True
 
 WEKO_ACCOUNTS_SHIB_ALLOW_USERNAME_INST_EPPN = True
 """Allow using SHIB_ATTR_USER_NAME instead of SHIB_ATTR_EPPN."""
-WEKO_ACCOUNTS_LOGIN_LABEL = "Log in to account"
+WEKO_ACCOUNTS_LOGIN_LABEL = 'Log in to account'
 """The login label"""
 
-WEKO_ACCOUNTS_REGISTER_LABEL = "Sign up for a %(sitename)s account!"
+WEKO_ACCOUNTS_REGISTER_LABEL = 'Sign up for a %(sitename)s account!'
 """The register label"""
 
 WEKO_ACCOUNTS_REAL_IP = None # X-Real-IP > X-Forwarded-For[0] > remote_addr
-# WEKO_ACCOUNTS_REAL_IP = "remote_add" # remote_addr
-# WEKO_ACCOUNTS_REAL_IP = "x_real_ip" # X-Real-IP > remote_addr
-# WEKO_ACCOUNTS_REAL_IP = "x_forwarded_for" # X-Forwarded-For[first] > remote_addr
-# WEKO_ACCOUNTS_REAL_IP = "x_forwarded_for_rev" # X-Forwarded-For[last] > remote_addr
+# WEKO_ACCOUNTS_REAL_IP = 'remote_add' # remote_addr
+# WEKO_ACCOUNTS_REAL_IP = 'x_real_ip' # X-Real-IP > remote_addr
+# WEKO_ACCOUNTS_REAL_IP = 'x_forwarded_for' # X-Forwarded-For[first] > remote_addr
+# WEKO_ACCOUNTS_REAL_IP = 'x_forwarded_for_rev' # X-Forwarded-For[last] > remote_addr
 
 WEKO_ACCOUNTS_REST_ENDPOINTS = {
     'login': {
@@ -181,5 +181,5 @@ WEKO_ACCOUNTS_REST_ENDPOINTS = {
 
 WEKO_ACCOUNTS_API_LIMIT_RATE_DEFAULT = ['100 per minute']
 
-WEKO_API_LIMIT_RATE_DEFAULT = ["100 per minute"]
+WEKO_API_LIMIT_RATE_DEFAULT = ['100 per minute']
 """Default rate limit per endpoint for one user in the WEKO API."""
