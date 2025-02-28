9b1778e0bcd38ecf8ca7c8cc6c35f139f661c446
ivis-futagami <ryota.futagami@ivis.co.jp>
2025-02-28 21:26:53 +0900

    create ams itemtype

---
+++ scripts/demo/ams/create_ams_itemtype.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo 'create ams itemtype'
+docker cp scripts/demo/ams/item_type_name.sql $(docker-compose -f docker-compose2.yml ps -q postgresql):/tmp/ams_itemtype_name.sql
+docker cp scripts/demo/ams/item_type.sql $(docker-compose -f docker-compose2.yml ps -q postgresql):/tmp/ams_itemtype.sql
+docker cp scripts/demo/ams/item_type_mapping.sql $(docker-compose -f docker-compose2.yml ps -q postgresql):/tmp/ams_itemtype_mapping.sql
+docker cp scripts/demo/ams/rocrate_mapping.sql $(docker-compose -f docker-compose2.yml ps -q postgresql):/tmp/ams_rocrate_mapping.sql
+docker cp scripts/demo/ams/facet_search_setting.sql $(docker-compose -f docker-compose2.yml ps -q postgresql):/tmp/ams_facet_search_setting.sql
+docker-compose -f docker-compose2.yml exec postgresql psql -U invenio -d invenio -f /tmp/ams_itemtype_name.sql
+docker-compose -f docker-compose2.yml exec postgresql psql -U invenio -d invenio -f /tmp/ams_itemtype.sql
+docker-compose -f docker-compose2.yml exec postgresql psql -U invenio -d invenio -f /tmp/ams_itemtype_mapping.sql
+docker-compose -f docker-compose2.yml exec postgresql psql -U invenio -d invenio -f /tmp/ams_rocrate_mapping.sql
+docker-compose -f docker-compose2.yml exec postgresql psql -U invenio -d invenio -f /tmp/ams_facet_search_setting.sql
+
+echo 'add free textarea to file property'
+docker-compose -f docker-compose2.yml exec web invenio shell /code/tools/add_free_textarea.py
+
+echo 'create search settings'
+record_count=$(docker-compose -f docker-compose2.yml exec postgresql psql -U invenio -d invenio -t -c 'SELECT count(*) FROM search_management;')
+if [ $record_count -lt 1 ]; then
+    docker cp scripts/demo/ams/search_management.sql $(docker-compose -f docker-compose2.yml ps -q postgresql):/tmp/ams_search_management.sql
+    docker-compose -f docker-compose2.yml exec postgresql psql -U invenio -d invenio -f /tmp/ams_search_management.sql
+fi
+docker-compose -f docker-compose2.yml exec web invenio shell /code/scripts/demo/ams/update_search_management.py
