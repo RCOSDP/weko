db1cd2f82b9b90845c7e99698e035b7bde8988c5
tomohiro.kuroda <tomohiro.kuroda@ivis.co.jp>
2025-03-22 20:29:38 +0900

    remove: signpostingclient

--- modules/weko-signpostingclient/INSTALL.rst
+++
@@ -1,8 +0,0 @@
-Installation
-============
-
-WEKO-Signpostingclient is on PyPI so all you need is:
-
-.. code-block:: console
-
-   $ pip install weko-signpostingclient
--- modules/weko-signpostingserver/weko_signpostingserver/api.py
+++ modules/weko-signpostingserver/weko_signpostingserver/api.py
@@ -5,12 +5,15 @@
 # WEKO-Signpostingserver is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
-from flask import Blueprint, Response, request, current_app, redirect
 
+"""Module of weko-signpostingserver."""
+
+import traceback
+from flask import Response, current_app
+
+from invenio_db import db
 from invenio_pidstore.models import PersistentIdentifier, PIDStatus
 from invenio_pidstore.errors import PIDDoesNotExistError
-from invenio_pidrelations.contrib.versioning import PIDVersioning
-from invenio_db import db
 from invenio_records.api import Record
 
 
@@ -34,72 +37,67 @@ def requested_signposting(pid, record, template=None, **kwargs):
     In addition, please place it above the one that has
     the same route and uses the GET method.
     """
-    resp = Response()
-    host_url = outside_url(request.host_url)
-    link = list()
+    host_url = current_app.config['THEME_SITEURL']
+
+    links = []
     recid = record['recid']
-    permalink = outside_url(get_record_permalink(recid))
-    if not permalink:
-        permalink = '{hosturl}records/{recid}'.\
-                    format(hosturl=host_url, recid=recid)
-    link.append('<{url}> ; rel="cite-as"'.
-                format(url=permalink))
-    link.append('<{url}> ; rel="describedby" ; type="application/json"'.
-                format(url='{hosturl}records/{recid}/export/json'.
-                           format(hosturl=host_url, recid=recid)))
-    link.append('<{url}> ; rel="describedby" ; type="application/x-bibtex"'.
-                format(url='{hosturl}records/{recid}/export/bibtex'.
-                           format(hosturl=host_url, recid=recid)))
+    record_link = f"{host_url}/records/{recid}"
+    permalink = get_record_permalink(recid)
+
+    if permalink is not None:
+        links.append('<{url}> ; rel="cite-as"'.format(url=permalink))
+
+    links.append(
+        '<{url}> ; rel="describedby" ; type="application/json"'
+        .format(url=f'{record_link}/export/json')
+    )
+    links.append(
+        '<{url}> ; rel="describedby" ; type="application/x-bibtex"'
+        .format(url=f'{record_link}/export/bibtex')
+    )
 
     oad = current_app.config.get('OAISERVER_METADATA_FORMATS', {})
 
     for _format, _object in oad.items():
-        url = '{hosturl}oai?verb=GetRecord&metadataPrefix='\
-              '{formats}&identifier={recid}'.\
-              format(hosturl=host_url,
-                     formats=_format,
-                     recid=record['_oai']['id']
-                     )
-        link.append('<{url}> ; rel="describedby" ; '
-                    'type="application/xml" ; formats="{formats}"'.
-                    format(url=url,
-                           formats=_object['namespace']))
-    resp.headers['Link'] = ','.join(link)
+        url = (
+            f'{host_url}/oai?verb=GetRecord&metadataPrefix={_format}'
+            f'&identifier={record["_oai"]["id"]}'
+        )
+        links.append(
+            f'<{url}> ; rel="describedby" ; '
+            f'type="application/xml" ; formats="{_object["namespace"]}"'
+        )
+
+    resp = Response()
+    resp.headers['Link'] = ','.join(links)
     return resp
 
 
-def outside_url(url):
-    if 'nginx' in url:
-        return url.replace('nginx', current_app.config['WEB_HOST'])
-    else:
-        return url
-
-
-def get_record_permalink(recid_p):
+def get_record_permalink(recid):
     """Get the uuid of the parent element of the record
     and get the uri of the doi associated with it
 
-    :param string recid_p: recid
+    :param string recid: recid
     :return: uri of doi
-    :rtype: string
+    :rtype: string | None
     """
-    uuid_p = \
-        PersistentIdentifier.get('parent',
-                                 'parent:'+str(recid_p)
-                                 ).object_uuid
+    item_uuid =  PersistentIdentifier.get('recid', str(recid)).object_uuid
+
     try:
-        pid = PersistentIdentifier.query.filter_by(
-            pid_type='doi',
-            object_uuid=uuid_p,
-            status=PIDStatus.REGISTERED
-        ).order_by(
-            db.desc(PersistentIdentifier.created)
-        ).first()
-        if pid is None:
-            return '{host_url}records/{recid}'.format(
-                host_url=current_app.config['THEME_SITEURL'] + '/', recid=recid_p)
-        else:
-            return pid.pid_value
-    except PIDDoesNotExistError as e:
-        return '{host_url}records/{recid}'.format(
-            host_url=current_app.config['THEME_SITEURL'] + '/', recid=recid_p)
+        pid = (
+            PersistentIdentifier
+            .query.filter_by(
+                pid_type='doi', object_uuid=item_uuid,
+                status=PIDStatus.REGISTERED
+            )
+            .order_by(db.desc(PersistentIdentifier.created))
+            .first()
+        )
+        if pid is not None:
+            return  pid.pid_value
+
+    except PIDDoesNotExistError as ex:
+        current_app.logger.error(f"Error getting doi for recid: {recid}")
+        traceback.print_exc()
+
+    return None
