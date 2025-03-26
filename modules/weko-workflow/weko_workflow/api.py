566197f55af1abd55d717cbb601fe529f102671a
QinZhe <qin.zhe@za.ivis.co.jp>
2025-03-26 15:17:39 +0900

    code 1st commit

--- modules/weko-workflow/weko_workflow/api.py
+++ modules/weko-workflow/weko_workflow/api.py
@@ -121,6 +121,8 @@ class Flow(object):
         """
         try:
             flow_name = flow.get('flow_name')
+            for_delete = flow.get('for_delete', False)
+            flow_type = 2 if for_delete else 1
             if not flow_name:
                 raise ValueError('Flow name cannot be empty.')
 
@@ -138,6 +140,7 @@ class Flow(object):
                     flow_id=flow_id).one_or_none()
                 if _flow:
                     _flow.flow_name = flow_name
+                    _flow.flow_type = flow_type
                     _flow.flow_user = current_user.get_id()
                     db.session.merge(_flow)
             db.session.commit()
@@ -408,6 +411,7 @@ class WorkFlow(object):
                     _workflow.flows_name = workflow.get('flows_name')
                     _workflow.itemtype_id = workflow.get('itemtype_id')
                     _workflow.flow_id = workflow.get('flow_id')
+                    _workflow.delete_flow_id = workflow.get('delete_flow_id')
                     _workflow.index_tree_id = workflow.get('index_tree_id')
                     _workflow.open_restricted = workflow.get('open_restricted')
                     _workflow.location_id = workflow.get('location_id')
