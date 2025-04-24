7508c981294e3840201cb874e556e5544cd048ea
ivis-okano <140027365+ivis-okano@users.noreply.github.com>
2025-04-24 16:57:59 +0900

    Update utils.py

--- modules/weko-items-ui/weko_items_ui/utils.py
+++ modules/weko-items-ui/weko_items_ui/utils.py
@@ -4190,7 +4190,7 @@ def check_duplicate(data, is_item=True):
                 matched_recids.add(recid)
                 break
 
-    matched_recids.discard(int(data["metainfo"].get("id") or 0))
+    matched_recids.discard(int(data.get("metainfo", {}).get("id") or 0))
 
     if not matched_recids:
         return False, [], []
@@ -4205,7 +4205,7 @@ def check_duplicate(data, is_item=True):
     db.session.commit()
     recids_resource = {r[0] for r in result}
     matched_recids &= recids_resource
-    matched_recids.discard(int(data["metainfo"].get("id") or 0))
+    matched_recids.discard(int(data.get("metainfo", {}).get("id") or 0))
     
     if not matched_recids:
         return False, [], []
