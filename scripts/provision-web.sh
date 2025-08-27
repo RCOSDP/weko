c9eac107a571d01750cad17e9ecf070b4f4d6ee3
Masaharu Hayashi <mhaya@nii.ac.jp>
2025-08-27 15:46:26 +0000

    Fix build error

--- scripts/provision-web.sh
+++ scripts/provision-web.sh
@@ -51,6 +51,12 @@ provision_web_common_ubuntu14 () {
     $sudo sed -i 's|http://deb.debian.org/debian|http://archive.debian.org/debian|g' /etc/apt/sources.list
     $sudo sed -i 's|http://security.debian.org/debian-security|http://archive.debian.org/debian-security|g' /etc/apt/sources.list
 
+    # Point the main repository to the archive
+    sed -i 's/deb.debian.org/archive.debian.org/g' /etc/apt/sources.list
+
+    # Point the security repository to the archive
+    sed -i 's/security.debian.org/archive.debian.org/g' /etc/apt/sources.list
+
     # sphinxdoc-install-useful-system-tools-ubuntu14-begin
     # update list of available packages:
     $sudo apt-get -y update -o Acquire::Check-Valid-Until=false --allow-releaseinfo-change
