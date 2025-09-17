2d0f773900e9f22f18e2d7d93843d1160c0cada6
ryoya-hayase <ryoya.hayase@ivis.co.jp>
2025-09-17 21:48:56 +0900

    fix update tool

--- scripts/demo/update_W2025-29.py
+++ scripts/demo/update_W2025-29.py
@@ -1,18 +1,26 @@
 import sys
+import traceback
 
+from flask import current_app
 from invenio_db import db
 from properties import property_config
 from register_properties import del_properties, get_properties_id, register_properties_from_folder
 from tools import updateRestrictedRecords
-import renew_all_item_types
+
+from weko_records.api import ItemTypes
+
 
 def main(restricted_item_type_id):
-    print("run updateRestrictedRecords")
-    updateRestrictedRecords.main(restricted_item_type_id)
-    print("run register_properties_only_specified")
-    register_properties_only_specified()
-    print("run renew_all_item_types")
-    renew_all_item_types.main()
+    try:
+        current_app.logger.info("run updateRestrictedRecords")
+        updateRestrictedRecords.main(restricted_item_type_id)
+        current_app.logger.info("run register_properties_only_specified")
+        register_properties_only_specified()
+        current_app.logger.info("run renew_all_item_types")
+        renew_all_item_types()
+    except Exception as ex:
+        current_app.logger.error(ex)
+        db.session.rollback()
 
 
 def register_properties_only_specified():
@@ -23,8 +31,21 @@ def register_properties_only_specified():
         exclusion_list += get_properties_id()
         register_properties_from_folder(exclusion_list, specified_list)
         db.session.commit()
-    except Exception as ex:
-        print(ex)
+    except:
+        current_app.logger.error(traceback.format_exc())
+        db.session.rollback()
+
+
+def renew_all_item_types():
+    try:
+        itemtypes = ItemTypes.get_all()
+        for itemtype in itemtypes:
+            ret = ItemTypes.reload(itemtype.id)
+            current_app.logger.info("itemtype id:{}, itemtype name:{}".format(itemtype.id,itemtype.item_type_name.name))
+            current_app.logger.info(ret['msg'])
+        db.session.commit()
+    except:
+        current_app.logger.error(traceback.format_exc())
         db.session.rollback()
 
 
@@ -32,7 +53,7 @@ if __name__ == "__main__":
     args = sys.argv
     if len(args) > 1:
         restricted_item_type_id = int(args[1])
-        main()
+        main(restricted_item_type_id)
     else:
         print("Please provide restricted_item_type_id as an argument.")
         sys.exit(1)
